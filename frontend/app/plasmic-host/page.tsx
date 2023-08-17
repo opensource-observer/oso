f5bb1c9b818ddd462e39ec734afc7fcc6855f142
Raymond Cheng <me@raymondcheng.net>
2023-08-17 08:42:55 -0700

    feat: switch Next.js to app router (#128)
    * Turn off static export mode
    * Build now includes support for Plasmic dynamic pages
    (e.g. /project/slug)

---
+++ frontend/app/plasmic-host/page.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import { PlasmicCanvasHost } from "@plasmicapp/loader-nextjs";
+import "plasmic-init-client";
+
+export default function PlasmicHost() {
+  return <PlasmicCanvasHost />;
+}
