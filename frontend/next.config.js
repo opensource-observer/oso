675cf56f799d53a031228393d3c39c67bbedcdd1
Raymond Cheng <me@raymondcheng.net>
2023-08-24 20:03:37 -0700

    fix: using @prisma/nextjs-monorepo-workaround-plugin
    * Fixing the Vercel error: `PrismaClientInitializationError: Your schema.prisma could not be
    found`
    * For reference see:
      https://www.prisma.io/docs/guides/other/troubleshooting-orm/help-articles/nextjs-prisma-client-monorepo
    * Also update .env.local.example for frontend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,19 @@
 // @ts-check
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
 
 /**
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
   //output: 'export',
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()];
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
