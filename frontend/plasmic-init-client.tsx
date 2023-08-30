1c11b30a3c5980941dd336b7e95101521147ca4d
Raymond Cheng <me@raymondcheng.net>
2023-08-29 18:39:33 -0700

    fix: remove custom formik-wrapped components
    * In the future, let's try to just use the generic FormField with
      a design system

--- frontend/plasmic-init-client.tsx
+++ frontend/plasmic-init-client.tsx
@@ -6,16 +6,7 @@ import { PLASMIC } from "./plasmic-init";
 import { ProjectsClientProvider } from "./components/project-browser/project-client-provider";
 import { ProjectBrowser } from "./components/project-browser/project-browser";
 import { SupabaseQuery } from "./components/dataprovider/supabase-query";
-import {
-  FormField,
-  FormError,
-  FormTextField,
-  FormSelect,
-  FormSlider,
-  FormDatePicker,
-  FormDropZone,
-  FormCheckbox,
-} from "./components/forms/form-elements";
+import { FormField, FormError } from "./components/forms/form-elements";
 import { VisualizationContext } from "./components/forms/visualization-context";
 
 /**
@@ -158,127 +149,6 @@ PLASMIC.registerComponent(FormField, {
 PLASMIC.registerComponent(FormError, {
   name: "FormError",
   description: "Displays the error associated with fieldName",
-  props: {
-    fieldName: {
-      type: "string",
-      helpText: "Formik field name",
-    },
-  },
-  importPath: "./components/forms/form-elements",
-});
-
-PLASMIC.registerComponent(FormTextField, {
-  name: "FormTextField",
-  description: "Textfield for forms",
-  props: {
-    fieldName: {
-      type: "string",
-      helpText: "Formik field name",
-    },
-    disabled: "boolean",
-    label: "string",
-    placeholder: "string",
-    multiline: {
-      type: "boolean",
-      helpText: "Support multiple lines. Automatically resizes the box",
-    },
-    minRows: {
-      type: "number",
-      helpText: "If multiline, minimum rows to show",
-    },
-    maxRows: {
-      type: "number",
-      helpText: "If multiline, maximum rows to show. Scrolls if more than that",
-    },
-    rows: {
-      type: "number",
-      helpText: "If multiline, fixes the number of rows to show",
-    },
-  },
-  importPath: "./components/forms/form-elements",
-});
-
-PLASMIC.registerComponent(FormSelect, {
-  name: "FormSelect",
-  description: "Select box for forms",
-  props: {
-    fieldName: {
-      type: "string",
-      helpText: "Formik field name",
-    },
-    label: "string",
-    optionValues: {
-      type: "object",
-      defaultValue: ["a", "b"],
-    },
-    multiple: {
-      type: "boolean",
-      helpText: "Let the user choose multiple values",
-    },
-    disabled: "boolean",
-  },
-  importPath: "./components/forms/form-elements",
-});
-
-PLASMIC.registerComponent(FormSlider, {
-  name: "FormSlider",
-  description: "Slider for forms",
-  props: {
-    fieldName: {
-      type: "string",
-      helpText: "Formik field name",
-    },
-    disabled: "boolean",
-    defaultValue: {
-      type: "number",
-      helpText: "Starting value",
-    },
-    min: "number",
-    max: "number",
-    step: "number",
-  },
-  importPath: "./components/forms/form-elements",
-});
-
-PLASMIC.registerComponent(FormDatePicker, {
-  name: "FormDatePicker",
-  description: "Date picker for forms",
-  props: {
-    fieldName: {
-      type: "string",
-      helpText: "Formik field name",
-    },
-    label: "string",
-    showUndefined: {
-      type: "boolean",
-      helpText: "Show checkbox to allow setting to undefined",
-    },
-    defaultUndefined: "boolean",
-    disabled: "boolean",
-  },
-  importPath: "./components/forms/form-elements",
-});
-
-PLASMIC.registerComponent(FormDropZone, {
-  name: "FormDropZone",
-  description: "DropZone for forms",
-  props: {
-    fieldName: {
-      type: "string",
-      helpText: "Formik field name",
-    },
-    children: "slot",
-    accept: {
-      type: "string",
-      helpText: "Types of files to accept",
-    },
-  },
-  importPath: "./components/forms/form-elements",
-});
-
-PLASMIC.registerComponent(FormCheckbox, {
-  name: "FormCheckbox",
-  description: "Checkbox for forms",
   props: {
     fieldName: {
       type: "string",
