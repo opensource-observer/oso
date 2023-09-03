857ddaad9aa3107436bbc5165a89d2e1179eaa6c
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2023-09-02 21:41:38 -0500

    initial project queries (#165)
    * feat: query all project events
    
    * update gitignore
    
    * feat: get blockchain users

---
+++ indexer/supabase/queries/get_project_users.sql
@@ -0,0 +1,30 @@
+-- This query identifies active blockchain users of a project
+WITH UserInteractions AS (
+    SELECT
+        e."contributorId" AS contributor_id,
+        COUNT(*) AS interaction_count
+    FROM
+        "Event" e
+    JOIN
+        "Artifact" a ON e."artifactId" = a.id
+    JOIN
+        "ProjectsOnArtifacts" poa ON a.id = poa."artifactId"
+    JOIN
+        "Project" p ON poa."projectId" = p.id
+    WHERE
+        e."eventTime" >= CURRENT_DATE - INTERVAL '30 days' AND
+        e."eventTime" < CURRENT_DATE AND
+        e."eventType" = 'CONTRACT_INVOKED' AND
+        p.name = 'Uniswap' -- Specify your project name here
+    GROUP BY
+        e."contributorId"
+)
+SELECT
+    c.id AS contributor_id,
+    c.name AS contributor_name,
+    ui.interaction_count
+FROM
+    "Contributor" c
+JOIN
+    UserInteractions ui ON c.id = ui.contributor_id
+ORDER BY 3 DESC;
