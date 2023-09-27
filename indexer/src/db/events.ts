8926cbf3676b2ca1aa5b1648033d6aed35bd1898
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2023-09-27 07:22:25 +0000

    small fixes

--- indexer/src/db/events.ts
+++ indexer/src/db/events.ts
@@ -1,9 +1,7 @@
-import _ from "lodash";
-import { Readable } from "stream";
-import { assert, normalizeToObject } from "../utils/common.js";
+import { normalizeToObject } from "../utils/common.js";
 import { AppDataSource } from "./data-source.js";
 import type { Brand } from "utility-types";
-import { EventType, EventPointer, Event } from "./orm-entities.js";
+import { EventPointer, Event } from "./orm-entities.js";
 
 export const EventPointerRepository = AppDataSource.getRepository(
   EventPointer,
