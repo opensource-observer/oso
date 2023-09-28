92900e89a0cb0f84d98bc3b4c31e1e4278c73135
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2023-09-28 14:01:44 -0400

    feat: scripts for bulk project updates (#264)
    * refactor: move address mapper module
    
    * feat: map slugs to repos and names
    
    * fix: convert addresses to lowercase
    
    * refactor: mapping functions
    
    * feat: scripts for mapping  OP GovFund grants
    
    * fix: reset addresses for new project
    
    * fix: improved CLI prompts
    
    * refactor: reader and parser scripts
    
    * feat: generate collections
    
    * fix: sorted and dedupe lists
    
    * fix: updates to main branch (#274)
    
    * fix: update environment variables to be format expected by TypeORM (#261)
    
    * Various Build Fixes (#262)
    
    * fix all indexer build errors and tests
    
    * small fixes
    
    * Allows running non-db tests (#263)
    
    * fix: linting errors (#265)
    
    * Fixes Funding Collector (#266)
    
    * current wip
    
    * Fix relations in importOssDirectory
    
    * Fix funding collector
    
    * Clean up (don't rewrite for now)
    
    * fix luxon import
    
    * remove enum reverser
    
    * lint fixes
    
    * lint fixes
    
    * Fix commit loading (#269)
    
    * Loading commits works for X period of time
    
    * lint fixes
    
    * Fixes issues with the cache writing (#270)
    
    * fix cache writing
    
    * put the arg in the right place
    
    * Fixes pull-request collector (#271)
    
    * Fixes pull-request collector
    
    * build fixes
    
    * skip errors for groups in the collectors
    
    * Fix all repo-followers (#272)
    
    * fix build error (#273)
    
    ---------
    
    Co-authored-by: Raymond Cheng <me@raymondcheng.net>
    Co-authored-by: Reuven Gonzales <reuven.v.gonzales@gmail.com>
    
    ---------
    
    Co-authored-by: Raymond Cheng <me@raymondcheng.net>
    Co-authored-by: Reuven Gonzales <reuven.v.gonzales@gmail.com>

--- indexer/utilities/address-indexers/src/ossd.py
+++ indexer/utilities/address-indexers/src/ossd.py
@@ -52,10 +52,114 @@ def update_yaml_data(yaml_data):
         dump(data, path)
 
 
-def main():
-    # test the script
+def map_addresses_to_slugs(yaml_data, chain, lowercase=True):
+    """
+    Returns a mapping of addresses to slugs.
+    Example:
+    {
+        "0x1234...": "project-slug"
+    }
+    """
+    addresses = {}
+    for data in yaml_data:
+        if not data:
+            continue
+        slug = data['slug']
+        blockchain_entries = data.get('blockchain', [])
+        if not blockchain_entries:
+            continue
+        for entry in blockchain_entries:
+            if chain not in entry.get('networks', []):
+                continue
+            address = entry.get('address', None)
+            if address:
+                if lowercase:
+                    address = address.lower()
+                addresses[address] = slug
+    return addresses
+
+
+def map_repos_to_slugs(yaml_data):
+    """
+    Returns a mapping of github repo urls to slugs.
+    Example:
+    {
+        "https://github.com/my-repo": "project-slug"
+    }
+    """
+    repos = {}
+    for data in yaml_data:
+        if not data:
+            continue
+        slug = data['slug']
+        repo_entries = data.get('github', [])
+        if not repo_entries:
+            continue
+        for entry in repo_entries:
+            url = entry.get('url', None)
+            if url:
+                repos[url] = slug
+    return repos
+
+
+def map_slugs_to_names(yaml_data):
+    """
+    Returns a mapping of slugs to names.
+    Example:
+    {
+        "project-slug": "My Project"
+    }
+    """
+    mapping = {}
+    for data in yaml_data:
+        if not data:
+            continue
+        slug = data['slug']
+        name = data['name']
+        mapping[slug] = name
+    return mapping
+
+
+def map_slugs_to_project_data(yaml_data):
+    """
+    Returns a mapping of slugs to project data.
+    Example:
+    {
+        "project-slug": {
+            "name": "My Project",
+            "github": "https://github.com/my-repo"
+        }
+    }
+    """
+    mapping = {}
+    for data in yaml_data:
+        if not data:
+            continue
+        slug = data['slug']
+        mapping[slug] = data
+    return mapping
+
+
+def make_template(yaml_data):
+    temp = yaml_data[0]
+    template = {
+        k: [] if isinstance(v, list) else None
+        for k,v in temp.items()
+    }
+    template['version'] = temp.get('version', None)
+    return template
+
+
+def test():
     yaml_data = get_yaml_data_from_path()
-    update_yaml_data(yaml_data)
+    #update_yaml_data(yaml_data)
+    addresses = map_addresses_to_slugs(yaml_data, chain='optimism')
+    repos = map_repos_to_slugs(yaml_data)
+    names = map_slugs_to_names(yaml_data)
+    mapping = map_slugs_to_project_data(yaml_data)
+    template = make_template(yaml_data)
+    print("YAML template:")
+    print(template)
 
 
-#main()
+#test()
