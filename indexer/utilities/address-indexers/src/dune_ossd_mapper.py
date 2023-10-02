e306feb43f134221334995aaecb355f8699fe3c4
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2023-10-02 15:49:55 -0400

    fix: cleanup dune contract mapping (#286)

--- indexer/utilities/address-indexers/src/dune_ossd_mapper.py
+++ indexer/utilities/address-indexers/src/dune_ossd_mapper.py
@@ -1,7 +1,7 @@
 import json
 from collections import defaultdict
 
-from ossd import get_yaml_data_from_path, update_yaml_data, assign_slugs_to_addresses
+from ossd import get_yaml_data_from_path, update_yaml_data, map_addresses_to_slugs
 
 DUNE_ADDRESS_DATA_PATH = "data/dune_exports/addresses.json"
 MAPPING_FILE_PATH = "data/dune_exports/dune_ossd_mapping.json"
@@ -36,68 +36,56 @@ def map_dune_names_to_yaml_slugs():
     dune_data = load_dune_data()
     
     # Assign slugs to addresses
-    yaml_addresses = assign_slugs_to_addresses(yaml_data, chain='optimism')
+    yaml_addresses = map_addresses_to_slugs(yaml_data, chain='optimism')
     
     # Parse Dune data
     dune_addresses, dune_duplicates = parse_dune_data(dune_data)
-    
-    # Initialize mappings
-    mapping = {
-        "one_to_one": {},
-        "yaml_to_multiple_dune": defaultdict(list),
-        "dune_to_multiple_yaml": defaultdict(list),
-        "no_yaml": defaultdict(list),
-        "no_dune": defaultdict(list),
-        "dune_duplicates": dune_duplicates
-    }
-    
-    # One to One and Multiple Dune to YAML
+
+    dune_spells_to_slugs = {}
+    unmapped_addresses = {}
+    mapped_projects = {}
+    unmapped_projects = {}
     for address, dune_name in dune_addresses.items():
         yaml_slug = yaml_addresses.get(address, None)
-        if yaml_slug:
-            if dune_name not in mapping["dune_to_multiple_yaml"]:
-                mapping["one_to_one"][dune_name] = yaml_slug
-            mapping["dune_to_multiple_yaml"][dune_name].append(yaml_slug)
-    
-    # Multiple YAML to Dune and No Dune
-    for address, yaml_slug in yaml_addresses.items():
-        dune_names = dune_data.get(address, {}).get("project_list", [])
-        if dune_names:
-            if len(dune_names) > 1:
-                mapping["yaml_to_multiple_dune"][yaml_slug].extend(dune_names)
+        if not yaml_slug:
+            unmapped_addresses[address] = dune_name
+            continue
+
+        # Case 1: Addressed has been mapped to a YAML
+        if dune_name not in dune_spells_to_slugs:
+            dune_spells_to_slugs[dune_name] = yaml_slug
+            mapped_projects[yaml_slug] = {
+                "dune_names": [dune_name],
+                "mapped_addresses": 1,
+                "unmapped_addresses": []
+            }
         else:
-            mapping["no_dune"][yaml_slug].append(address)
-            
-    # No YAML
-    for address, dune_info in dune_duplicates.items():
-        for dune_name in dune_info['project_list']:
-            if dune_name not in mapping["one_to_one"] and dune_name not in mapping["dune_to_multiple_yaml"]:
-                mapping["no_yaml"][dune_name].append(address)
+            if dune_name not in mapped_projects[yaml_slug]["dune_names"]:
+                mapped_projects[yaml_slug]["dune_names"].append(dune_name)
+            mapped_projects[yaml_slug]["mapped_addresses"] += 1
                 
-    # Convert defaultdict to dict for JSON serialization
-    mapping = {key: dict(value) for key, value in mapping.items()}
+    for address, dune_name in unmapped_addresses.items():
+        # Case 2: Address is used by multiple projects; skip it
+        if address in dune_duplicates:
+            continue
+        # Case 3: Address belongs to a mapped project but has been mapped yet
+        if dune_name in dune_spells_to_slugs:
+            mapped_projects[dune_spells_to_slugs[dune_name]]["unmapped_addresses"].append(address)
+            continue
+        # Case 4: Address belongs to a project that has not been mapped to a YAML
+        if dune_name not in unmapped_projects:
+            unmapped_projects[dune_name] = [address]
+        else:
+            unmapped_projects[dune_name].append(address)
 
-    # De-duplicate and sort the lists in mappings
-    for key in ["yaml_to_multiple_dune", "dune_to_multiple_yaml", "no_yaml", "no_dune"]:
-        mapping[key] = {k: sorted(list(set(v))) for k, v in mapping[key].items()}
-    
-    # Identify Dune names that map to a single YAML slug to move them from 'dune_to_multiple_yaml' to 'one_to_one'
-    keys_to_move = []
-    for dune_name, yaml_slugs in mapping['dune_to_multiple_yaml'].items():
-        if len(yaml_slugs) == 1:
-            keys_to_move.append(dune_name)
-            
-    # Perform the move
-    for key in keys_to_move:
-        mapping['one_to_one'][key] = mapping['dune_to_multiple_yaml'][key][0]
-        del mapping['dune_to_multiple_yaml'][key]
-    
-    # Sort the keys in each dictionary
+    mapping = {
+        "dune_spells_to_slugs": dune_spells_to_slugs,
+        "mapped_projects": mapped_projects,
+        "unmapped_projects": unmapped_projects,
+        "dune_duplicates": dune_duplicates
+    }
+
     mapping = {key: dict(sorted(value.items())) for key, value in mapping.items()}
-
-    # Sort the keys in the main mapping dictionary as well
-    mapping = dict(sorted(mapping.items()))
-
     
     with open(MAPPING_FILE_PATH, "w") as f:
         json.dump(mapping, f, indent=4)
