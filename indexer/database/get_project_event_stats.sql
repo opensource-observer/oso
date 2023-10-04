0e50af56538d9819b2741d81074eefeeffff9e05
Carl Cervone <cerv1-air@Carls-Air.lan>
2023-10-04 06:41:50 -0400

    feat: project summary stat query

--- indexer/database/get_project_event_stats.sql
+++ indexer/database/get_project_event_stats.sql
@@ -1,20 +1,41 @@
 SELECT
-    project."name" AS project_name,
-    event."type" AS event_type,
-    COUNT(event."id") AS total_events,
-    COUNT(DISTINCT event."fromId") AS total_contributors,
-    MIN(event."time") AS first_event_time,
-    MAX(event."time") AS last_event_time
+    p."slug" AS project_slug,
+    p."name" AS project_name,
+    e."type" AS event_type,
+    COUNT(e."id") AS total_events,
+    COUNT(DISTINCT e."fromId") AS total_contributors,
+    MIN(e."time") AS first_event_time,
+    MAX(e."time") AS last_event_time
 FROM
-    project
+    project p
 LEFT JOIN
-    project_artifacts_artifact ON project."id" = project_artifacts_artifact."projectId"
+    project_artifacts_artifact ON p."id" = project_artifacts_artifact."projectId"
 LEFT JOIN
     artifact ON project_artifacts_artifact."artifactId" = artifact."id"
 LEFT JOIN
-    event ON artifact."id" = event."toId"
+    event e ON artifact."id" = e."toId"
+LEFT JOIN
+    collection_projects_project cpp ON p."id" = cpp."projectId"
+LEFT JOIN
+    collection c ON cpp."collectionId" = c."id"
+WHERE
+    e."type" IN (
+        'PULL_REQUEST_CREATED',
+        'PULL_REQUEST_MERGED',
+        'COMMIT_CODE',
+        'ISSUE_CLOSED',
+        'PULL_REQUEST_CLOSED',
+        'PULL_REQUEST_APPROVED',
+        'ISSUE_CREATED',
+        'STARRED',
+        'FORKED'
+    )
+    AND e."time" IS NOT NULL
+    --AND e."time" >= '2019-01-01'        
+    AND c."slug" = 'ffdw-grants'    
 GROUP BY
-    project."name",
-    event."type"
+    p."slug",
+    p."name",
+    e."type"
 ORDER BY
-    total_events DESC;
+    project_name; 
