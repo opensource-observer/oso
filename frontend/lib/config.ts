d1f786f137480c91aba222d33e4fb10679b6d6ed
Raymond Cheng <me@raymondcheng.net>
2023-10-10 20:12:33 -0700

    feat: update EventDataProvider to use GraphQL
    * Add queries for continuous aggregates
    * Remove invalid import in config
    * Allow selection of entityType

--- frontend/lib/config.ts
+++ frontend/lib/config.ts
@@ -1,9 +1,6 @@
 import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const envPath = path.resolve(__dirname, "../.env.local");
 dotenv.config();
 dotenv.config({ path: envPath, override: true });
--- frontend/lib/graphql/queries.ts
+++ frontend/lib/graphql/queries.ts
@@ -24,4 +24,49 @@ const GET_PROJECT_BY_SLUG = gql(`
   }
 `);
 
-export { GET_ARTIFACT_BY_NAME, GET_PROJECT_BY_SLUG };
+const GET_EVENTS_DAILY_BY_ARTIFACT = gql(`
+  query EventsDailyByArtifact(
+    $ids: [Int!],
+    $types: [event_type_enum!],
+    $startDate: timestamptz!,
+    $endDate: timestamptz!, 
+  ) {
+    events_daily_by_artifact(where: {
+      toId: {_in: $ids},
+      type: {_in: $types},
+      bucketDaily: {_gte: $startDate, _lte: $endDate}
+    }) {
+      toId
+      amount
+      bucketDaily
+      type
+    }
+  }
+`);
+
+const GET_EVENTS_DAILY_BY_PROJECT = gql(`
+  query EventsDailyByProject(
+    $ids: [Int!],
+    $types: [event_type_enum!],
+    $startDate: timestamptz!,
+    $endDate: timestamptz!, 
+  ) {
+    events_daily_by_project(where: {
+      projectId: {_in: $ids}
+      type: {_in: $types},
+      bucketDaily: {_gte: $startDate, _lte: $endDate}
+    }) {
+      amount
+      bucketDaily
+      projectId
+      type
+    }
+  }
+`);
+
+export {
+  GET_ARTIFACT_BY_NAME,
+  GET_PROJECT_BY_SLUG,
+  GET_EVENTS_DAILY_BY_ARTIFACT,
+  GET_EVENTS_DAILY_BY_PROJECT,
+};
