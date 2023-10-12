8ee56994441af283ce15f63ffdc1c32cffe7a07c
Raymond Cheng <me@raymondcheng.net>
2023-10-11 17:47:58 -0700

    fix: hardcode homepage route (#389)
    * After we added not-found.tsx, that started showing when we would click
      to a relative `/` link. Hoping this makes it clear we should render
      the page instead

---
+++ frontend/app/page.tsx
@@ -0,0 +1,23 @@
+import { PlasmicComponent } from "@plasmicapp/loader-nextjs";
+import { PLASMIC } from "../plasmic-init";
+import { PlasmicClientRootProvider } from "../plasmic-init-client";
+
+// Using incremental static regeneration, will invalidate this page
+// after this (no deploy webhooks needed)
+export const revalidate = 3600;
+const PLASMIC_COMPONENT = "Homepage";
+
+export default async function HomePage() {
+  //console.log(project);
+  const plasmicData = await PLASMIC.fetchComponentData(PLASMIC_COMPONENT);
+  const compMeta = plasmicData.entryCompMetas[0];
+
+  return (
+    <PlasmicClientRootProvider
+      prefetchedData={plasmicData}
+      pageParams={compMeta.params}
+    >
+      <PlasmicComponent component={compMeta.displayName} />
+    </PlasmicClientRootProvider>
+  );
+}
