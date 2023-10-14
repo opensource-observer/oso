0f3024106a826f5795b24d5235539c4abd6be5df
Raymond Cheng <me@raymondcheng.net>
2023-10-14 16:38:42 -0700

    feat: Add aggregate GraphQL queries
    * Adds GraphQL queries for fetching aggregates by artifactId or
      projectId
    * Removes `path` dependency in `config.ts`
    * Removes `kpiCard` option in EventDataProvider. We'll migrate to using
      an AggregateDataProvider in the future

--- frontend/components/dataprovider/table-data-provider.tsx
+++ frontend/components/dataprovider/table-data-provider.tsx
@@ -9,7 +9,8 @@ import type { CommonDataProviderProps } from "./provider-view";
  * - Does not support authentication or RLS. Make sure data is readable by unauthenticated users
  */
 type TableDataProviderProps = CommonDataProviderProps & {
-  ids?: string[];
+  collectionSlugs: string[]; // Collections to include
+  projectSlugs: string[]; // Projects to include
   eventTypes?: string[];
   startDate?: string;
   endDate?: string;
@@ -22,12 +23,18 @@ type TableDataProviderProps = CommonDataProviderProps & {
  */
 function TableDataProvider(props: TableDataProviderProps) {
   /**
-  const variables = createVariables(props);
   const {
     data: rawData,
     error,
     loading,
-  } = useQuery(GET_EVENTS_DAILY_BY_ARTIFACT, { variables });
+  } = useQuery(
+    GET_EVENTS_DAILY_BY_ARTIFACT,
+    { 
+      variables: {
+
+      },
+    }
+  );
   const normalizedData: EventData[] = (
     rawData?.events_daily_by_artifact ?? []
   ).map((x) => ({
@@ -38,15 +45,13 @@ function TableDataProvider(props: TableDataProviderProps) {
   }));
   console.log(props.ids, rawData, formattedData);
   */
-  const loading = false;
   const formattedData = {};
-  const error = undefined;
   return (
     <DataProviderView
       {...props}
       formattedData={formattedData}
-      loading={loading}
-      error={error}
+      loading={false}
+      error={undefined}
     />
   );
 }
