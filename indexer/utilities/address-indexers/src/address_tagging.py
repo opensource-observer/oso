0287455968dcc409de4b7b29d2f04fa4c8b7d5fd
ccerv1 <carl.cervone@gmail.com>
2023-10-23 15:53:34 -0400

    feat: identify new repos/addresses to existing projects

--- indexer/utilities/address-indexers/src/address_tagging.py
+++ indexer/utilities/address-indexers/src/address_tagging.py
@@ -1,3 +1,4 @@
+import csv
 from dotenv import load_dotenv
 import os
 import requests
@@ -73,3 +74,42 @@ def fetch_contract_name(chain, address, sleep=.5):
     except:
         print(f"\n\n** Fatal error looking up a contract at address {address}\n\n")
         return None
+
+
+def parse_csv(csv_path, address_col, label_col, chain):
+
+    with open(csv_path, 'r') as f:
+        reader = csv.DictReader(f)
+        rows = [row for row in reader]
+
+    for row in rows:
+        address = row[address_col]
+        label = row[label_col]
+        if address[:2] != '0x':
+            continue
+
+        address_type = "eoa" if is_eoa(chain, address, sleep=0) else "contract"
+        contract_name = fetch_contract_name(chain, address, sleep=0) if address_type == "contract" else None
+        if isinstance(contract_name, str):
+            contract_name = contract_name.lower()
+
+        if 'wallet' in label:
+            if address_type == 'eoa':
+                row[label_col] = 'eoa wallet'
+            elif isinstance(contract_name, str) and 'safe' in contract_name:
+                row[label_col] = 'safe wallet'
+            else:
+                row[label_col] = 'unknown wallet'
+        elif 'contract' in label:
+            if address_type == 'contract':
+                if isinstance(contract_name,str) and 'factory' in contract_name:
+                    row[label_col] = 'factory contract'
+                else:
+                    row[label_col] = 'contract'
+            elif address_type == 'eoa':
+                row[label_col] = 'eoa'
+    
+    with open(csv_path, 'w') as f:
+        writer = csv.DictWriter(f, fieldnames=reader.fieldnames)
+        writer.writeheader()
+        writer.writerows(rows)
