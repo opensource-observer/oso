d8c96595e63c1e5491aeae72e0690f58d21ee532
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2023-11-01 11:07:31 -0400

    fix: improve handling of projects that didn't list github in their app (#507)
    fix: improve handling of projects that didn't list a github in their application

--- indexer/utilities/rpgf3-attestations/src/canonical.py
+++ indexer/utilities/rpgf3-attestations/src/canonical.py
@@ -5,6 +5,7 @@ from address_lookup import is_eoa
 PROJECT_SUMMARY_CSV = "data/project_attestation_summary.csv"	
 PROJECT_OSSD_MAPPINGS = "data/rpgf3_ossd_mappings.csv"
 CANONICAL_PROJECTS_LIST = "data/canonical_projects.csv"
+PAYOUT_ADDRESS_CSV = "data/payout_addresses.csv"
 
 
 def update_canonical_projects():
@@ -14,7 +15,12 @@ def update_canonical_projects():
     cols = ["applicantType", "contributionLinkCount", "impactMetricCount", "urlTypeCount"]
     df = df1.join(df2[cols], how='outer')
 
-    df['isEOA'] = df['Payout Address'].apply(lambda x: is_eoa(chain='optimism', address=x, sleep=0.2))
+    try:
+        df3 = pd.read_csv(PAYOUT_ADDRESS_CSV, index_col="Payout Address")
+        df = df.join(df3, on='Payout Address', how='outer')
+    except FileNotFoundError:
+        print("Looking up EOA status for payout addresses...")
+        df['isEOA'] = df['Payout Address'].apply(lambda x: is_eoa(chain='optimism', address=x, sleep=0.2))
 
     df.to_csv(CANONICAL_PROJECTS_LIST)
 
