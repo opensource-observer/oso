0f8241b7397c46aa12d5b861225e3f72fcf914da
Raymond Cheng <me@raymondcheng.net>
2023-11-02 14:34:45 -0700

    feat: materialized view for users_monthly_to_project
    * Note that this cannot be a continuous aggregate (doesn't allow set
      operations)
    * This means this materialized view needs to be refreshed periodically
      manually

--- indexer/src/db/orm-entities.ts
+++ indexer/src/db/orm-entities.ts
@@ -406,7 +406,7 @@ export class Log extends Base<"LogId"> {
  ******************************/
 
 /**
- * For each (to, from) pair, get the first contribution event in time.
+ * For each (to, from, type) tuple, get the first contribution event in time.
  */
 @ViewEntity({
   materialized: true,
@@ -444,6 +444,92 @@ export class FirstContribution {
 
   @ViewColumn()
   id: Brand<number, "EventId">;
+
+  @ViewColumn()
+  amount: number;
+}
+
+/**
+ * Get monthly counts of different types of users
+ */
+@ViewEntity({
+  materialized: true,
+  expression: `
+    WITH Devs AS (
+      SELECT 
+        p."id" AS "projectId",
+        e."fromId" AS "fromId",
+        time_bucket(INTERVAL '1 month', e."time") AS "bucketMonthly",
+        CASE 
+          WHEN COUNT(DISTINCT CASE WHEN t."name" = 'COMMIT_CODE' THEN e."time" END) >= 10 THEN 'FULL_TIME_DEV'
+          WHEN COUNT(DISTINCT CASE WHEN t."name" = 'COMMIT_CODE' THEN e."time" END) >= 1 THEN 'PART_TIME_DEV'
+          ELSE 'OTHER_CONTRIBUTOR'
+        END AS "segmentType",
+        1 AS amount
+      FROM event e
+      JOIN project_artifacts_artifact paa ON e."toId" = paa."artifactId"
+      JOIN project p ON paa."projectId" = p.id        
+      JOIN event_type t ON e."typeId" = t.id
+      WHERE
+        t."name" IN (
+          'PULL_REQUEST_CREATED',
+          'PULL_REQUEST_MERGED',
+          'COMMIT_CODE',
+          'ISSUE_CLOSED',
+          'ISSUE_CREATED'
+        )
+      GROUP BY
+        p."id",
+        e."fromId",
+        "bucketMonthly"
+    ),
+    Users AS (
+      SELECT 
+          p."id" AS "projectId",
+          e."fromId" AS "fromId",
+          time_bucket(INTERVAL '1 month', e."time") AS "bucketMonthly",
+          CASE 
+              WHEN SUM(e."amount") >= 1000 THEN 'HIGH_FREQUENCY_USER'
+              WHEN SUM(e."amount") >= 10 THEN 'HIGH_VALUE_USER'
+              ELSE 'LOW_VALUE_USER'
+          END AS "segmentType",
+          1 AS amount
+      FROM event e
+      JOIN project_artifacts_artifact paa ON e."toId" = paa."artifactId"
+      JOIN project p ON paa."projectId" = p.id
+      JOIN event_type t ON e."typeId" = t.id
+      WHERE t."name" = 'CONTRACT_INVOCATION_DAILY_COUNT'
+      GROUP BY
+        p."id",
+        e."fromId",
+        "bucketMonthly"
+    )
+    SELECT
+      "projectId",
+      "segmentType",
+      "bucketMonthly",
+      SUM("amount") AS "amount"
+    FROM 
+      (
+          SELECT * FROM Devs
+          UNION ALL
+          SELECT * FROM Users
+      ) combined_data
+    GROUP BY
+      "projectId",
+      "segmentType",
+      "bucketMonthly";
+  `,
+})
+export class UsersMonthlyToProject {
+  @ViewColumn()
+  project: Project;
+
+  @ViewColumn()
+  segmentType: string;
+
+  @ViewColumn()
+  bucketMonthly: Date;
 
   @ViewColumn()
   amount: number;
