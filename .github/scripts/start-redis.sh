7f20e6c6211e9c0eaa34ef72edcd1d73243dda18
Reuven Gonzales <reuven@karibalabs.co>
2023-11-28 00:04:40 -0800

    Update start-redis.sh
    Remove the rdb file if it's somehow corrupted

--- .github/scripts/start-redis.sh
+++ .github/scripts/start-redis.sh
@@ -9,14 +9,29 @@ mkdir -p "${CACHE_DIR}/redis"
 ls -halt "${CACHE_DIR}/redis"
 
 # Start the redis container
-docker run --name redis -p 6379:6379 -v "${CACHE_DIR}/redis:/data" redis redis-server --save 60 1000 --loglevel warning
+start_redis() {
+    docker run --name redis -d -p 6379:6379 -v "${CACHE_DIR}/redis:/data" redis redis-server --save 60 1000 --loglevel warning
+}
+
+stop_redis() {
+    docker kill redis || true
+}
+
+start_redis
 
 retries=0
-max_retries=120
+max_retries=60
 until [ "$retries" -ge "$max_retries" ]
 do
     redis-cli ping && break && exit 0
     retries=$((retries+1))
+    if [ "$retries" -eq 30 ]; then
+        rm -r "${CACHE_DIR}/redis"
+        mkdir -p "${CACHE_DIR}/redis"
+        
+        stop_redis
+        start_redis
+    fi
     sleep 5 
 done;
 exit 1
