7c2a1de31a5dbe27019a5963cab291900ee50790
Raymond Cheng <me@raymondcheng.net>
2024-01-26 11:28:41 -0800

    fix: switching from Next.js supabase client to the generic JS one (#795)
    * Not sure if this is causing Plasmic to not load, just trying

--- frontend/components/widgets/auth-form.tsx
+++ frontend/components/widgets/auth-form.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { Database } from "../../lib/types/supabase";
+import { supabaseClient } from "../../lib/clients/supabase";
 
 const REDIRECT_URL = "http://localhost:3000/";
 
@@ -12,12 +11,11 @@ type AuthFormProps = {
 
 export function AuthForm(props: AuthFormProps) {
   const { className } = props;
-  const supabase = createClientComponentClient<Database>();
 
   return (
     <div className={className}>
       <Auth
-        supabaseClient={supabase}
+        supabaseClient={supabaseClient}
         appearance={{ theme: ThemeSupa }}
         providers={["google"]}
         queryParams={{
--- frontend/lib/clients/supabase.ts
+++ frontend/lib/clients/supabase.ts
@@ -1,7 +1,9 @@
-import { SupabaseClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
 import { HttpError } from "../types/errors";
+import { Database } from "../types/supabase";
+import { SUPABASE_URL, SUPABASE_ANON_KEY } from "../config";
 
-//const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 type SupabaseQueryArgs = {
   tableName: string; // table to query
@@ -14,12 +16,9 @@ type SupabaseQueryArgs = {
   orderAscending?: boolean; // True if ascending, false if descending
 };
 
-async function supabaseQuery(
-  client: SupabaseClient,
-  args: SupabaseQueryArgs,
-): Promise<any[]> {
+async function supabaseQuery(args: SupabaseQueryArgs): Promise<any[]> {
   const { tableName, columns, filters, limit, orderBy, orderAscending } = args;
-  let query = client.from(tableName).select(columns);
+  let query = supabaseClient.from(tableName).select(columns);
   // Iterate over the filters
   if (Array.isArray(filters)) {
     for (let i = 0; i < filters.length; i++) {
@@ -50,5 +49,5 @@ async function supabaseQuery(
   return data;
 }
 
-export { supabaseQuery };
+export { supabaseClient, supabaseQuery };
 export type { SupabaseQueryArgs };
