b79957c0d061721f96db80222ce261eaa8e6706c
Raymond Cheng <me@raymondcheng.net>
2024-01-26 10:42:06 -0800

    feat: add styling to AuthForm (#793)

--- frontend/components/widgets/auth-form.tsx
+++ frontend/components/widgets/auth-form.tsx
@@ -6,21 +6,28 @@ import { Database } from "../../lib/types/supabase";
 
 const REDIRECT_URL = "http://localhost:3000/";
 
-export function AuthForm() {
+type AuthFormProps = {
+  className?: string; // Plasmic CSS class
+};
+
+export function AuthForm(props: AuthFormProps) {
+  const { className } = props;
   const supabase = createClientComponentClient<Database>();
 
   return (
-    <Auth
-      supabaseClient={supabase}
-      appearance={{ theme: ThemeSupa }}
-      view="sign_in"
-      providers={["google"]}
-      queryParams={{
-        access_type: "offline",
-        prompt: "consent",
-      }}
-      redirectTo={REDIRECT_URL}
-      showLinks={false}
-    />
+    <div className={className}>
+      <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+        providers={["google"]}
+        queryParams={{
+          access_type: "offline",
+          prompt: "consent",
+        }}
+        redirectTo={REDIRECT_URL}
+        showLinks={false}
+        onlyThirdPartyProviders={true}
+      />
+    </div>
   );
 }
