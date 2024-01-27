6a2333a063800af284765dbf145dd5bbe1594066
Raymond Cheng <me@raymondcheng.net>
2024-01-26 21:27:33 -0800

    feat: restore searchParams, dynamically modify force-dynamic/static (#798)
    depending on STATIC_EXPORT

--- frontend/app/project/[...slug]/page.tsx
+++ frontend/app/project/[...slug]/page.tsx
@@ -6,7 +6,9 @@ import { PlasmicClientRootProvider } from "../../../plasmic-init-client";
 import { cachedGetProjectsBySlugs } from "../../../lib/graphql/cached-queries";
 import { logger } from "../../../lib/logger";
 import { catchallPathToString } from "../../../lib/paths";
+import { STATIC_EXPORT } from "../../../lib/config";
 
+export const dynamic = STATIC_EXPORT ? "force-static" : "force-dynamic";
 export const revalidate = false; // 3600 = 1 hour
 const STATIC_EXPORT_SLUGS = ["IGNORE"];
 const PLASMIC_COMPONENT = "ProjectPage";
@@ -31,11 +33,11 @@ type ProjectPageProps = {
   params: {
     slug: string[];
   };
-  //searchParams?: Record<string, string | string[]>;
+  searchParams?: Record<string, string | string[]>;
 };
 
 export default async function ProjectPage(props: ProjectPageProps) {
-  const { params } = props;
+  const { params, searchParams } = props;
   const slugs = [catchallPathToString(params.slug)];
   if (!params.slug || !Array.isArray(params.slug) || params.slug.length < 1) {
     logger.warn("Invalid project page path", params);
@@ -58,6 +60,7 @@ export default async function ProjectPage(props: ProjectPageProps) {
     <PlasmicClientRootProvider
       prefetchedData={plasmicData}
       pageParams={compMeta.params}
+      pageQuery={searchParams}
     >
       <PlasmicComponent
         component={compMeta.displayName}
