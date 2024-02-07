ede82a8b0d2644340f3569e8622921266fab3b3b
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-02-07 08:36:35 +0000

    fixes

--- dbt/models/staging/github/github_events.sql
+++ dbt/models/staging/github/github_events.sql
@@ -8,7 +8,10 @@
       "field": "created_at",
       "data_type": "timestamp",
       "granularity": "day",
-    }
+    },
+    unique_id="id",
+    on_schema_change="append_new_columns",
+    incremental_strategy="insert_overwrite"
   )
 }}
 
