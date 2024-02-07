774ca5533d125639ff6c8f9f612b5cdcd6b22d79
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-02-07 08:01:54 +0000

    Reorganize and fix github events materialization

--- dbt/models/marts/events/all_events.sql
+++ dbt/models/marts/events/all_events.sql
@@ -20,55 +20,18 @@
 
   amount
 #}
-WITH arbitrum_contract_invocation_intermediate AS (
-  SELECT 
-    CAST(cu.date AS Timestamp) AS `time`,
-    TO_BASE64(
-      SHA1(
-        CONCAT(
-          CAST(cu.date AS STRING), 
-          cu.address, 
-          CASE 
-            WHEN cu.user_address IS NULL THEN "" 
-            ELSE cu.user_address
-          END,
-          CASE 
-            WHEN cu.safe_address IS NULL THEN "" 
-            ELSE cu.safe_address
-          END
-        ))
-    ) as `source_id`,
-    cu.address AS `to_name`,
-    "ARBITRUM" AS `to_namespace`,
-    "CONTRACT_ADDRESS" AS `to_type`,
-    cu.address AS `to_source_id`,
-    
-    CASE
-      WHEN cu.safe_address IS NULL THEN cu.user_address
-      WHEN cu.user_address IS NULL THEN cu.safe_address
-      ELSE "unknown"
-    END AS `from_name`,
+{{
+  config(
+    materialized='table',
+    partition_by={
+      "field": "time",
+      "data_type": "timestamp",
+      "granularity": "day",
+    }
+  )
+}}
 
-    "ARBITRUM" AS `from_namespace`,
-
-    CASE
-      WHEN cu.safe_address IS NULL THEN "EOA_ADDRESS"
-      WHEN cu.user_address IS NULL THEN "SAFE_ADDRESS"
-      ELSE "unknown"
-    END AS `from_type`,
-
-    CASE
-      WHEN cu.safe_address IS NULL THEN cu.user_address
-      WHEN cu.user_address IS NULL THEN cu.safe_address
-      ELSE "unknown"
-    END AS `from_source_id`,
-
-    CAST(cu.l1_gas AS FLOAT64) as `l1_gas`,
-    CAST(cu.l2_gas AS FLOAT64) as `l2_gas`,
-    CAST(cu.tx_count AS FLOAT64) AS `tx_count`
-
-  FROM `oso-production.opensource_observer.contract_usage` as cu
-), arbitrum_contract_invocation_daily_count AS (
+WITH arbitrum_contract_invocation_daily_count AS (
   SELECT 
     acii.time,
     "CONTRACT_INVOCATION_DAILY_COUNT" AS `type`,
@@ -82,7 +45,7 @@ WITH arbitrum_contract_invocation_intermediate AS (
     acii.from_type,
     acii.from_source_id,
     acii.tx_count as `amount`
-  FROM arbitrum_contract_invocation_intermediate AS acii
+  FROM {{ ref('arbitrum_contract_invocation') }} AS acii
 ), arbitrum_contract_invocation_daily_l2_gas_used AS (
   SELECT 
     acii.time,
@@ -97,7 +60,7 @@ WITH arbitrum_contract_invocation_intermediate AS (
     acii.from_type,
     acii.from_source_id,
     acii.l2_gas as `amount`
-  FROM arbitrum_contract_invocation_intermediate AS acii
+  FROM {{ ref('arbitrum_contract_invocation') }} AS acii
 ), arbitrum_contract_invocation_daily_l1_gas_used AS (
   SELECT 
     acii.time,
@@ -112,7 +75,7 @@ WITH arbitrum_contract_invocation_intermediate AS (
     acii.from_type,
     acii.from_source_id,
     acii.l1_gas as `amount`
-  FROM arbitrum_contract_invocation_intermediate AS acii
+  FROM {{ ref('arbitrum_contract_invocation') }} AS acii
 ), github_commits AS (
   SELECT
     gc.created_at as `time`,
---
+++ dbt/models/staging/dune/arbitrum_contract_invocation.sql
@@ -0,0 +1,47 @@
+SELECT 
+  CAST(cu.date AS Timestamp) AS `time`,
+  TO_BASE64(
+    SHA1(
+      CONCAT(
+        CAST(cu.date AS STRING), 
+        cu.address, 
+        CASE 
+          WHEN cu.user_address IS NULL THEN "" 
+          ELSE cu.user_address
+        END,
+        CASE 
+          WHEN cu.safe_address IS NULL THEN "" 
+          ELSE cu.safe_address
+        END
+      ))
+  ) as `source_id`,
+  cu.address AS `to_name`,
+  "ARBITRUM" AS `to_namespace`,
+  "CONTRACT_ADDRESS" AS `to_type`,
+  cu.address AS `to_source_id`,
+  
+  CASE
+    WHEN cu.safe_address IS NULL THEN cu.user_address
+    WHEN cu.user_address IS NULL THEN cu.safe_address
+    ELSE "unknown"
+  END AS `from_name`,
+
+  "ARBITRUM" AS `from_namespace`,
+
+  CASE
+    WHEN cu.safe_address IS NULL THEN "EOA_ADDRESS"
+    WHEN cu.user_address IS NULL THEN "SAFE_ADDRESS"
+    ELSE "unknown"
+  END AS `from_type`,
+
+  CASE
+    WHEN cu.safe_address IS NULL THEN cu.user_address
+    WHEN cu.user_address IS NULL THEN cu.safe_address
+    ELSE "unknown"
+  END AS `from_source_id`,
+
+  CAST(cu.l1_gas AS FLOAT64) as `l1_gas`,
+  CAST(cu.l2_gas AS FLOAT64) as `l2_gas`,
+  CAST(cu.tx_count AS FLOAT64) AS `tx_count`
+
+FROM `oso-production.opensource_observer.contract_usage` as cu
