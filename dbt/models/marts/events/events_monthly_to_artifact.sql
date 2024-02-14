043d050e59da44cf48d8f2e55ddd84840592ee0a
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-02-14 05:59:13 +0000

    more fixes

--- dbt/models/marts/events/events_monthly_to_artifact.sql
+++ dbt/models/marts/events/events_monthly_to_artifact.sql
@@ -3,9 +3,9 @@
 #}
 
 SELECT
-  e.to_namespace as artifact_namespace,
-  e.to_type as artifact_type,
-  e.to_source_id as artifact_source_id,
+  e.artifact_namespace as artifact_namespace,
+  e.artifact_type as artifact_type,
+  e.artifact_source_id as artifact_source_id,
   TIMESTAMP_TRUNC(e.bucket_day, MONTH) as bucket_month,
   e.event_type,
   SUM(e.amount) AS amount
