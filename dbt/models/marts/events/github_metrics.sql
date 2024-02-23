9419112d619503001687a1581d4d6afb098d3683
Carl Cervone <carl.cervone@gmail.com>
2024-02-22 22:35:38 -0800

    fix: active developers calc

--- dbt/models/marts/events/github_metrics.sql
+++ dbt/models/marts/events/github_metrics.sql
@@ -1,17 +1,19 @@
 {# 
   Summary GitHub metrics for a project:
-    - First commit date
-    - Total repositories
-    - Total stars
-    - Total forks
-    - Total contributors
-    - Total contributors in the last 6 months
-    - Average active developers over the last 6 months
-    - Total commits over the last 6 months
-    - Total issues opened over the last 6 months
-    - Total issues closed over the last 6 months
-    - Total pull requests opened over the last 6 months
-    - Total pull requests merged over the last 6 months
+    - first_commit_date: The date of the first commit to the project
+    - last_commit_date: The date of the last commit to the project
+    - repos: The number of repositories in the project
+    - stars: The number of stars the project has
+    - forks: The number of forks the project has
+    - contributors: The number of contributors to the project
+    - contributors_6_months: The number of contributors to the project in the last 6 months
+    - new_contributors_6_months: The number of new contributors to the project in the last 6 months    
+    - avg_active_devs_6_months: The average number of active developers in the last 6 months
+    - commits_6_months: The number of commits to the project in the last 6 months
+    - issues_opened_6_months: The number of issues opened in the project in the last 6 months
+    - issues_closed_6_months: The number of issues closed in the project in the last 6 months
+    - pull_requests_opened_6_months: The number of pull requests opened in the project in the last 6 months
+    - pull_requests_merged_6_months: The number of pull requests merged in the project in the last 6 months
 #}
 
 -- CTE for calculating the first and last commit date for each project, ignoring forked repos
@@ -43,7 +45,7 @@ contributors_cte AS (
         project_id,
         COUNT(DISTINCT from_id) AS contributors,
         COUNT(DISTINCT CASE WHEN DATE(bucket_month) >= DATE_ADD(CURRENT_DATE(), INTERVAL -6 MONTH) THEN from_id END) AS contributors_6_months,
-        COUNT(DISTINCT CASE WHEN DATE(bucket_month) >= DATE_ADD(CURRENT_DATE(), INTERVAL -6 MONTH) AND user_segment_type IN ('FULL_TIME_DEV', 'PART_TIME_DEV') THEN from_id END) / 6 AS avg_active_devs_6_months,
+        COUNT(DISTINCT CASE WHEN DATE(bucket_month) >= DATE_ADD(CURRENT_DATE(), INTERVAL -6 MONTH) AND user_segment_type IN ('FULL_TIME_DEV', 'PART_TIME_DEV') THEN CONCAT(from_id, '_', bucket_month) END) / 6 AS avg_active_devs_6_months,
         COUNT(DISTINCT CASE WHEN first_contribution_date >= DATE_ADD(CURRENT_DATE(), INTERVAL -6 MONTH) THEN from_id END) AS new_contributors_6_months
     FROM (
         SELECT
@@ -53,18 +55,19 @@ contributors_cte AS (
             user_segment_type,
             MIN(DATE(bucket_month)) OVER (PARTITION BY from_id, project_id) AS first_contribution_date
         FROM {{ ref('int_devs') }}
-    ) devs
+    )
     GROUP BY project_id
-),  
+),
+
 -- CTE for summarizing project activity metrics over the past 6 months
 activity_cte AS (
     SELECT
         project_id,
-        SUM(CASE WHEN event_type = 'COMMIT_CODE' THEN amount END) AS commits,
-        SUM(CASE WHEN event_type = 'ISSUE_OPENED' THEN amount END) AS issues_opened,
-        SUM(CASE WHEN event_type = 'ISSUE_CLOSED' THEN amount END) AS issues_closed,
-        SUM(CASE WHEN event_type = 'PULL_REQUEST_OPENED' THEN amount END) AS pull_requests_opened,
-        SUM(CASE WHEN event_type = 'PULL_REQUEST_MERGED' THEN amount END) AS pull_requests_merged
+        SUM(CASE WHEN event_type = 'COMMIT_CODE' THEN amount END) AS commits_6_months,
+        SUM(CASE WHEN event_type = 'ISSUE_OPENED' THEN amount END) AS issues_opened_6_months,
+        SUM(CASE WHEN event_type = 'ISSUE_CLOSED' THEN amount END) AS issues_closed_6_months,
+        SUM(CASE WHEN event_type = 'PULL_REQUEST_OPENED' THEN amount END) AS pull_requests_opened_6_months,
+        SUM(CASE WHEN event_type = 'PULL_REQUEST_MERGED' THEN amount END) AS pull_requests_merged_6_months
     FROM {{ ref('int_events_to_project') }}
     WHERE DATE(time) >= DATE_ADD(CURRENT_DATE(), INTERVAL -180 DAY)
     GROUP BY project_id
@@ -81,13 +84,13 @@ SELECT
     sfr.forks,
     c.contributors,
     c.contributors_6_months,
-    c.avg_active_devs_6_months,
     c.new_contributors_6_months,
-    act.commits,
-    act.issues_opened,
-    act.issues_closed,
-    act.pull_requests_opened,
-    act.pull_requests_merged    
+    c.avg_active_devs_6_months,    
+    act.commits_6_months,
+    act.issues_opened_6_months,
+    act.issues_closed_6_months,
+    act.pull_requests_opened_6_months,
+    act.pull_requests_merged_6_months    
 FROM {{ ref('projects') }} AS p
 JOIN project_commit_dates AS pcd ON p.project_id = pcd.project_id
 JOIN stars_forks_repos AS sfr ON p.project_id = sfr.project_id
