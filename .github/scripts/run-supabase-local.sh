ca250d421531d99efbd8a277efd11602c13faf3e
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-03-15 08:56:14 +0000

    fix

--- .github/scripts/run-supabase-local.sh
+++ .github/scripts/run-supabase-local.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 cd "$1"
 
 pnpm supabase start
-export $(supabase status -o env | xargs)
+export $(pnpm supabase status -o env | xargs)
 echo "SUPABASE_SERVICE_KEY=$SERVICE_ROLE_KEY" >> $GITHUB_ENV
 echo "SUPABASE_JWT_SECRET=$JWT_SECRET" >> $GITHUB_ENV 
 echo "NEXT_PUBLIC_SUPABASE_URL=$API_URL" >> $GITHUB_ENV
