4624fe82c29bdcc835f727663b8267d7eef9ed26
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-03-15 06:24:38 +0000

    Initial testing setup for external prs

---
+++ ops/external-prs/src/index.ts
@@ -0,0 +1,39 @@
+import { App } from "octokit";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+async function main() {
+  const APP_TO_CHECK = process.env.APP_TO_CHECK;
+  const SHA_TO_CHECK = process.env.SHA_TO_CHECK;
+
+  const app = new App({
+    appId: process.env.APP_ID!,
+    privateKey: process.env.PRIVATE_KEY!,
+  });
+
+  const { data } = await app.octokit.request("/app");
+  console.log(`Authenticated as ${data.name}`);
+
+  for await (const { installation } of app.eachInstallation.iterator()) {
+    for await (const { octokit, repository } of app.eachRepository.iterator({
+      installationId: installation.id,
+    })) {
+      if (repository.name !== APP_TO_CHECK) {
+        continue;
+      }
+      await octokit.request("POST /repos/{owner}/{repo}/check-runs", {
+        owner: repository.owner.login,
+        repo: repository.name,
+        body: {
+          name: "test-deployment",
+          head_sha: SHA_TO_CHECK,
+        },
+      });
+    }
+  }
+}
+
+main().catch((e) => {
+  console.log(e);
+});
