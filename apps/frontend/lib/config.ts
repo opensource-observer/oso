07ca14cddafaebae006b62f20adf334db7ee233a
Raymond Cheng <me@raymondcheng.net>
2024-03-15 11:29:04 -0700

    fix: don't make OSO_API_KEY required
    * Unbreaks the client-side bundle

--- apps/frontend/lib/config.ts
+++ apps/frontend/lib/config.ts
@@ -1,3 +1,9 @@
+/**
+ * Mark an environment as required for the build.
+ * This is mostly used for convenience of debugging, so that things break early
+ * Note: this should only be used for environment variables prefixed with "NEXT_PUBLIC_".
+ *  Client-side bundles will be missing other env variables by definition
+ */
 export const requireEnv = (value: string | undefined, identifier: string) => {
   if (!value) {
     throw new Error(`Required env var ${identifier} does not exist`);
