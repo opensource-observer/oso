539d7a8ff20fd6c8a6e5003c552b3ec0c7cdce33
Raymond Cheng <me@raymondcheng.net>
2024-03-15 11:14:57 -0700

    feat: use OSO API keys in our own app (#1065)
    * We currently use the OSO API in 2 places: (1) to generate types for
      the GraphQL API via codegen and (2) to make server-side queries to the
      database.
    * Added OSO API keys in various GraphQL initializers so we don't get
      a broken build if we exhaust the anon rate limit

--- apps/frontend/lib/clients/apollo.ts
+++ apps/frontend/lib/clients/apollo.ts
@@ -1,12 +1,15 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
-import { DB_GRAPHQL_URL } from "../config";
+import { DB_GRAPHQL_URL, OSO_API_KEY } from "../config";
 
 const { getClient: getApolloClient } = registerApolloClient(() => {
   return new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
       uri: DB_GRAPHQL_URL,
+      headers: {
+        Authorization: `Bearer ${OSO_API_KEY}`,
+      },
     }),
   });
 });
