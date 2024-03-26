cb2842d152ece1285fbdb7f7eafc71b861b4385b
Reuven Gonzales <reuven@karibalabs.co>
2024-03-26 09:02:06 -0700

    Adds deployer tables (#1130)
    * Adds deployer tables
    
    * fix playground
    
    * Add discovered deployers
    
    * lint fixes
    
    * more lint fixes
    
    * lint fixes
    
    * fix
    
    * fix
    
    * fix
    
    * lint fixes

---
+++ warehouse/dbt/models/staging/ethereum/stg_ethereum__deployers.sql
@@ -0,0 +1,34 @@
+{{
+  config(
+    materialized='incremental',
+    partition_by={
+      "field": "block_timestamp",
+      "data_type": "timestamp",
+      "granularity": "day",
+    },
+    unique_id="transaction_hash",
+    on_schema_change="append_new_columns",
+    incremental_strategy="insert_overwrite"
+  ) if target.name == 'production' else config(
+    materialized='table',
+  )
+}}
+
+SELECT
+  block_timestamp,
+  `hash` AS transaction_hash,
+  from_address AS deployer_address,
+  receipt_contract_address AS contract_address
+FROM {{ source("ethereum", "transactions") }}
+WHERE
+  to_address IS NULL
+  AND receipt_status = 1
+  AND receipt_contract_address IS NOT NULL
+{% if is_incremental() %}
+  WHERE 
+    TIMESTAMP_TRUNC(block_timestamp, DAY) >= (
+      SELECT TIMESTAMP_TRUNC(MAX(block_timestamp), DAY)
+      FROM {{ this }}
+    )
+    AND TIMESTAMP_TRUNC(block_timestamp, DAY) < CURRENT_TIMESTAMP()
+  {% endif %}
