aa943604a8c9b3e2295cec2191770b64317c24f5
Carl Cervone <carl.cervone@gmail.com>
2024-04-01 22:01:22 -0400

    add: user int models

---
+++ warehouse/dbt/models/intermediate/users/int_addresses_to_project.sql
@@ -0,0 +1,14 @@
+{# 
+  Address stats by project and network
+#}
+
+SELECT
+  from_id,
+  from_namespace AS network,
+  project_id,
+  MIN(time) AS date_first_txn,
+  MAX(time) AS date_last_txn,
+  SUM(amount) AS total_amount
+FROM {{ ref('int_events_to_project') }}
+WHERE event_type = 'CONTRACT_INVOCATION_DAILY_COUNT'
+GROUP BY 1, 2, 3
