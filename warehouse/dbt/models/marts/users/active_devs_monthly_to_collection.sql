47094d9bf004faaf99089d72ce589e0c978f4f5c
Carl Cervone <carl.cervone@gmail.com>
2024-04-01 22:09:35 -0400

    add: initial user marts

---
+++ warehouse/dbt/models/marts/users/active_devs_monthly_to_collection.sql
@@ -0,0 +1,35 @@
+{#
+  This model segments developers based on monthly activity
+  using the same taxonomy as in the Electric Capital
+  Developer Report by collection.
+
+  The taxonomy is as follows:
+  - Full-time developer: A developer who has made at least 10 commits
+  - Part-time developer: A developer who has made less than 10 commits
+  - Other contributor: A user who has not made any commits
+#}
+
+SELECT
+  collection_id,
+  from_id,
+  from_namespace AS repository_source,
+  bucket_month,
+  1 AS amount,
+  CASE
+    WHEN
+      event_type = 'COMMIT_CODE' AND count_days >= 10
+      THEN 'FULL_TIME_DEV'
+    WHEN
+      event_type = 'COMMIT_CODE' AND count_days < 10
+      THEN 'PART_TIME_DEV'
+    ELSE 'OTHER_CONTRIBUTOR'
+  END AS user_segment_type
+FROM {{ ref('int_user_events_monthly_to_collection') }}
+WHERE
+  event_type IN (
+    'COMMIT_CODE',
+    'PULL_REQUEST_OPENED',
+    'PULL_REQUEST_MERGED',
+    'ISSUE_OPENED',
+    'ISSUE_CLOSED'
+  )
---
+++ warehouse/dbt/models/marts/users/address_rfm_segments_by_project.sql
@@ -0,0 +1,114 @@
+{# 
+  Onchain address segementation using the RFM model.
+  
+  RFM stands for Recency, Frequency, and Monetary Value.
+  In this version, we adapt the M to represent the 
+  number of projects a user has interacted with.
+  
+  We will consider the following metrics:
+  - Recency: Days since last activity
+  - Frequency: Total activity
+  - Monetary Value: Project count
+  
+  We will then assign a segment to each user based on these
+  metrics.
+#}
+
+WITH user_data AS (
+  SELECT
+    from_id,
+    network,
+    project_id,
+    total_amount AS total_activity,
+    DATE_DIFF(CURRENT_TIMESTAMP(), date_last_txn, DAY)
+      AS days_since_last_activity
+  FROM {{ ref('int_addresses_to_project') }}
+),
+
+user_project_count AS (
+  SELECT
+    from_id,
+    network,
+    COUNT(DISTINCT project_id) AS project_count
+  FROM user_data
+  GROUP BY 1, 2
+),
+
+user_stats AS (
+  SELECT
+    ud.*,
+    upc.project_count
+  FROM user_data AS ud
+  LEFT JOIN user_project_count AS upc
+    ON
+      ud.from_id = upc.from_id
+      AND ud.network = upc.network
+),
+
+rfm_components AS (
+  SELECT
+    from_id,
+    network,
+    project_id,
+    CASE
+      WHEN days_since_last_activity < 7 THEN 5
+      WHEN days_since_last_activity < 30 THEN 4
+      WHEN days_since_last_activity < 90 THEN 3
+      WHEN days_since_last_activity < 180 THEN 2
+      ELSE 1
+    END AS rfm_recency,
+    CASE
+      WHEN total_activity = 1 THEN 1
+      WHEN total_activity < 10 THEN 2
+      WHEN total_activity < 100 THEN 3
+      WHEN total_activity < 1000 THEN 4
+      ELSE 5
+    END AS rfm_frequency,
+    CASE
+      WHEN project_count = 1 THEN 1
+      WHEN project_count <= 3 THEN 2
+      WHEN project_count <= 10 THEN 3
+      WHEN project_count <= 30 THEN 4
+      ELSE 5
+    END AS rfm_ecosystem
+  FROM user_stats
+)
+
+SELECT
+  *,
+  CASE
+    WHEN rfm_frequency = 5
+      THEN
+        CASE
+          WHEN rfm_recency = 5 THEN 'Power'
+          WHEN rfm_recency = 4 THEN 'Loyal'
+          WHEN rfm_recency = 3 THEN 'At risk'
+          ELSE 'Churned'
+        END
+    WHEN rfm_frequency = 4
+      THEN
+        CASE
+          WHEN rfm_recency >= 4 THEN 'Loyal'
+          WHEN rfm_recency = 3 THEN 'At risk'
+          ELSE 'Churned'
+        END
+    WHEN rfm_frequency = 3
+      THEN
+        CASE
+          WHEN rfm_recency >= 4 THEN 'Promising'
+          WHEN rfm_recency = 3 THEN 'Needs attention'
+          ELSE 'Tourist'
+        END
+    WHEN rfm_frequency = 2
+      THEN
+        CASE
+          WHEN rfm_recency >= 4 THEN 'Noob'
+          ELSE 'Tourist'
+        END
+    WHEN rfm_frequency = 1 THEN
+      CASE
+        WHEN rfm_recency >= 3 THEN 'Noob'
+        ELSE 'One and done'
+      END
+  END AS user_segment
+FROM rfm_components
