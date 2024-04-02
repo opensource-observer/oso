bd4569dfda4411d30ccb3c8876d397b444a6aab6
Carl Cervone <carl.cervone@gmail.com>
2024-04-02 14:50:07 -0400

    add artifact and project mini-marts

--- warehouse/dbt/models/marts/directory/artifacts_by_project.sql
+++ warehouse/dbt/models/marts/directory/artifacts_by_project.sql
@@ -1,4 +1,10 @@
 SELECT
   atp.project_id AS project_id,
-  atp.artifact_id AS artifact_id
+  p.project_slug AS project_slug,
+  atp.artifact_id AS artifact_id,
+  atp.artifact_namespace AS artifact_namespace,
+  atp.artifact_type AS artifact_type,
+  atp.artifact_name AS artifact_name
 FROM {{ ref('stg_ossd__artifacts_by_project') }} AS atp
+LEFT JOIN {{ ref('projects') }} AS p
+  ON atp.project_id = p.project_id
---
+++ warehouse/dbt/models/marts/directory/github_repos_by_project.sql
@@ -0,0 +1,32 @@
+{#
+  This model is a directory of all GitHub repositories that are associated with a project.
+  It includes metadata about the repository, as well as the first and last commit dates.
+#}
+
+WITH github_stats AS (
+  SELECT
+    artifact_id,
+    MIN(bucket_day) AS first_commit,
+    MAX(bucket_day) AS last_commit
+  FROM {{ ref('events_daily_to_artifact') }}
+  WHERE event_type = 'COMMIT_CODE'
+  GROUP BY artifact_id
+)
+
+SELECT
+  p.project_id,
+  p.project_slug,
+  p.project_name,
+  r.artifact_id,
+  r.is_fork AS repo_is_fork,
+  r.fork_count AS repo_fork_count,
+  r.star_count AS repo_star_count,
+  s.first_commit,
+  s.last_commit,
+  LOWER(r.name_with_owner) AS repo_name_with_owner
+FROM {{ ref('stg_ossd__repositories_by_project') }} AS r
+LEFT JOIN {{ ref('projects') }} AS p
+  ON r.project_id = p.project_id
+LEFT JOIN github_stats AS s
+  ON r.artifact_id = s.artifact_id
+WHERE r.repository_source = 'GITHUB'
