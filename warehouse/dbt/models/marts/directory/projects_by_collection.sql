8d27dc7c85f35845aa580b61734683837c286b20
Carl Cervone <carl.cervone@gmail.com>
2024-04-05 14:11:03 -0400

    rebase projects by collection

--- warehouse/dbt/models/marts/directory/projects_by_collection.sql
+++ warehouse/dbt/models/marts/directory/projects_by_collection.sql
@@ -4,5 +4,13 @@
   }) 
 }}
 
-SELECT *
-FROM {{ ref('int_projects_by_collection') }}
+SELECT
+  pbc.project_id,
+  pbc.project_slug,
+  pbc.project_name,
+  c.collection_id,
+  c.collection_slug,
+  c.collection_name
+FROM {{ ref('int_projects_by_collection') }} AS pbc
+LEFT JOIN {{ ref('collections') }} AS c
+  ON pbc.collection_id = c.collection_id
