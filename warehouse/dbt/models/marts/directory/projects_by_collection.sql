f36112fd1e9c173faa5100c528a342f66bb84554
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2024-04-05 15:56:28 -0400

    Dbt/update pbc marts (#1183)
    * rebase projects by collection
    
    * update projects by collection mart
    
    * update projects by collection mart
    
    * fix: users monthly to project needs segments
    
    * fix: unqualified reference in table

--- warehouse/dbt/models/marts/directory/projects_by_collection.sql
+++ warehouse/dbt/models/marts/directory/projects_by_collection.sql
@@ -3,6 +3,13 @@
     'sync_to_cloudsql': True
   }) 
 }}
-
-SELECT *
-FROM {{ ref('int_projects_by_collection') }}
+SELECT
+  pbc.project_id,
+  pbc.project_slug,
+  pbc.project_name,
+  c.collection_id,
+  c.collection_slug,
+  c.collection_name
+FROM {{ ref('int_projects_by_collection') }} AS pbc
+LEFT JOIN {{ ref('collections') }} AS c
+  ON pbc.collection_id = c.collection_id
--- warehouse/dbt/models/marts/events/users_monthly_to_project.sql
+++ warehouse/dbt/models/marts/events/users_monthly_to_project.sql
@@ -4,11 +4,29 @@
   }) 
 }}
 
+WITH users AS (
+  SELECT
+    project_id,
+    from_namespace,
+    event_type,
+    bucket_month,
+    COUNT(DISTINCT from_id) AS amount
+  FROM {{ ref('int_user_events_monthly_to_project') }}
+  GROUP BY 1, 2, 3, 4
+)
+
 SELECT
   project_id,
-  from_namespace,
-  event_type,
+  'Developers' AS user_segment_type,
   bucket_month,
-  COUNT(DISTINCT from_id) AS amount
-FROM {{ ref('int_user_events_monthly_to_project') }}
-GROUP BY 1, 2, 3, 4
+  amount
+FROM users
+WHERE event_type = 'COMMIT_CODE'
+UNION ALL
+SELECT
+  project_id,
+  'Active Addresses' AS user_segment_type,
+  bucket_month,
+  amount
+FROM users
+WHERE event_type = 'CONTRACT_INVOCATION_DAILY_COUNT'
