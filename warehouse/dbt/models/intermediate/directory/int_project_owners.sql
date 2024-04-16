39f4cbdb77735a84267f30b6145f8f8b4832ae60
Carl Cervone <carl.cervone@gmail.com>
2024-04-16 14:46:14 -0700

    attempt to fix linting error

--- warehouse/dbt/models/intermediate/directory/int_project_owners.sql
+++ warehouse/dbt/models/intermediate/directory/int_project_owners.sql
@@ -6,13 +6,14 @@ WITH ranked_repos AS (
     ROW_NUMBER() OVER (
       PARTITION BY project_id ORDER BY star_count DESC
     ) AS row_number,
-    COUNT(DISTINCT owner) OVER (PARTITION BY project_id) AS num_github_owners
+    COUNT(DISTINCT owner) OVER (PARTITION BY project_id) AS count_github_owners
   FROM {{ ref('stg_ossd__repositories_by_project') }}
 )
 
 SELECT
   project_id,
-  num_github_owners,
+  count_github_owners,
   LOWER(owner) AS primary_github_owner
+  --TODO: is_git_organization
 FROM ranked_repos
 WHERE row_number = 1
--- warehouse/dbt/models/marts/directory/projects.sql
+++ warehouse/dbt/models/marts/directory/projects.sql
@@ -5,12 +5,17 @@
 }}
 
 SELECT
-  id AS project_id,
-  slug AS project_slug,
+  projects.id AS project_id,
+  projects.slug AS project_slug,
   -- description AS project_description,
-  name AS project_name,
-  namespace AS user_namespace,
-  ARRAY_LENGTH(JSON_EXTRACT_ARRAY(github)) AS count_github_artifacts,
-  ARRAY_LENGTH(JSON_EXTRACT_ARRAY(blockchain)) AS count_blockchain_artifacts,
-  ARRAY_LENGTH(JSON_EXTRACT_ARRAY(npm)) AS count_npm_artifacts
-FROM {{ ref('stg_ossd__current_projects') }}
+  projects.name AS project_name,
+  projects.namespace AS user_namespace,
+  project_owners.primary_github_owner,
+  project_owners.count_github_owners,
+  ARRAY_LENGTH(JSON_EXTRACT_ARRAY(projects.github)) AS count_github_artifacts,
+  ARRAY_LENGTH(JSON_EXTRACT_ARRAY(projects.blockchain))
+    AS count_blockchain_artifacts,
+  ARRAY_LENGTH(JSON_EXTRACT_ARRAY(projects.npm)) AS count_npm_artifacts
+FROM {{ ref('stg_ossd__current_projects') }} AS projects
+INNER JOIN {{ ref('int_project_owners') }} AS project_owners
+  ON projects.id = project_owners.project_id
