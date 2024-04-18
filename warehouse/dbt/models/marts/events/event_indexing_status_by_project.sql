3f9fc9d555ede50ec69fce868cad36724a7de992
Carl Cervone <carl.cervone@gmail.com>
2024-04-18 15:58:04 -0400

    dbt: monitor project indexing status

---
+++ warehouse/dbt/models/marts/events/event_indexing_status_by_project.sql
@@ -0,0 +1,17 @@
+{{ 
+  config(meta = {
+    'sync_to_cloudsql': True
+  }) 
+}}
+
+SELECT
+  e.project_id,
+  p.project_slug,
+  e.from_namespace,
+  e.event_type,
+  MIN(e.bucket_day) AS date_first_event,
+  MAX(e.bucket_day) AS date_last_event,
+  COUNT(DISTINCT e.bucket_day) AS count_days_with_event
+FROM {{ ref('events_daily_to_project_by_source') }} AS e
+INNER JOIN {{ ref('projects') }} AS p
+GROUP BY 1, 2, 3, 4
