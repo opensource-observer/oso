b21513ad5c5f31c8fe2703fe532c425b484fd4f3
ccerv1 <carl.cervone@gmail.com>
2024-04-19 18:58:19 -0400

    add: barebones user model

---
+++ warehouse/dbt/models/marts/users/users.sql
@@ -0,0 +1,18 @@
+{# 
+  This model is a WIP and is not yet ready for production use.
+#}
+
+WITH user_data AS (
+  SELECT
+    from_id,
+    MAX(rfm_recency) AS r,
+    MAX(rfm_frequency) AS f,
+    MAX(rfm_ecosystem) AS e
+  FROM {{ ref('address_rfm_segments_by_project') }}
+  GROUP BY 1
+)
+
+SELECT
+  from_id AS user_id,
+  (r > 2 AND f > 2 AND e > 2) AS is_trusted
+FROM user_data
