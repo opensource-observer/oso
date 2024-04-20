14e78662f4e30df712d47d427bf6de290d314832
ccerv1 <carl.cervone@gmail.com>
2024-04-19 20:18:26 -0400

    update projects mart

--- warehouse/dbt/models/marts/directory/projects.sql
+++ warehouse/dbt/models/marts/directory/projects.sql
@@ -5,12 +5,11 @@
 }}
 
 SELECT
-  id AS project_id,
-  slug AS project_slug,
-  -- description AS project_description,
-  name AS project_name,
-  namespace AS user_namespace,
-  ARRAY_LENGTH(JSON_EXTRACT_ARRAY(github)) AS count_github_artifacts,
-  ARRAY_LENGTH(JSON_EXTRACT_ARRAY(blockchain)) AS count_blockchain_artifacts,
-  ARRAY_LENGTH(JSON_EXTRACT_ARRAY(npm)) AS count_npm_artifacts
-FROM {{ ref('stg_ossd__current_projects') }}
+  project_id,
+  project_slug,
+  project_name,
+  count_github_owners,
+  count_github_artifacts,
+  count_blockchain_artifacts,
+  count_npm_artifacts
+FROM {{ ref('int_projects') }}
