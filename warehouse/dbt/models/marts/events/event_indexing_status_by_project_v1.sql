993867ecfd14c0950394251c6039dc950463e7a1
Raymond Cheng <me@raymondcheng.net>
2024-05-02 18:31:06 -0700

    fix: rename metrics mart columns (#1355)
    - 3m/6m => 3_months, 6_months: Why? `m` is ambigous, could mean months or minutes or something else
    - med => medium, pr => pull_request (no abbreviations)
    - I'm sticking to <adjective>-<kind>-<noun>-<context>.  This means min_commit_date, not commit_min_date. This means opened_issue_count, not issues_opened_count. I understand why you'd want `commit` or `issues` to be first, but I don't think that is  canonical in dbt styling
    - for the <kind>, I'm sticking to singular, not plural (e.g. project_id, address_count, transaction_date). Not addresses_count or contributors_count

--- warehouse/dbt/models/marts/events/event_indexing_status_by_project_v1.sql
+++ warehouse/dbt/models/marts/events/event_indexing_status_by_project_v1.sql
@@ -13,7 +13,7 @@ SELECT
   events.event_type,
   MIN(events.bucket_day) AS first_event_date,
   MAX(events.bucket_day) AS last_event_date,
-  COUNT(DISTINCT events.bucket_day) AS count_days_with_event
+  COUNT(DISTINCT events.bucket_day) AS eventful_day_count
 FROM {{ ref('events_daily_to_project_by_source') }} AS events
 INNER JOIN {{ ref('projects_v1') }} AS projects
   ON projects.project_id = events.project_id
