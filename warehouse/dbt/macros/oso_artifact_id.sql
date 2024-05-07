3f375ee73e42c8f9df804734feb76457d6525ed9
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-05-07 21:09:25 +0000

    Remove type from artifact_id

--- warehouse/dbt/macros/oso_artifact_id.sql
+++ warehouse/dbt/macros/oso_artifact_id.sql
@@ -1,3 +1,11 @@
+{# 
+  oso_artifact_id generates an id for an artifact by hashing namespace and the
+  source_id of the artifact. 
+
+  This macro generates the hash input by using a prefix to discover the
+  namespace and source_id columns. If, for example, the prefix is "artifact"
+  then the columns used are `artifact_namespace` and `artifact_source_id`.
+#}
 {% macro oso_artifact_id(prefix, table_alias="") -%}
     {%- set _prefix = prefix -%}
     {%- if not prefix.endswith('_') and prefix != "" %}
@@ -8,6 +16,5 @@
     {% endif -%}
     {%- set namespace = "%s%s" % (_prefix, 'namespace') -%}
     {%- set source_id = "%s%s" % (_prefix, 'source_id') -%}
-    {%- set type = "%s%s" % (_prefix, 'type') -%}
-    {{- oso_id(namespace, type, source_id) -}}
+    {{- oso_id(namespace, source_id) -}}
 {%- endmacro %}
