9490f33fe358b723995591a2077119b93552a742
ccerv1 <carl.cervone@gmail.com>
2024-05-14 12:35:43 -0400

    fix: trusted user model

--- warehouse/dbt/models/marts/superchain/rf4_trusted_users.sql
+++ warehouse/dbt/models/marts/superchain/rf4_trusted_users.sql
@@ -1,12 +1,16 @@
 with user_model as (
   select
-  artifacts_by_user.user_id,
-  artifacts_by_user.user_source,
-  artifacts_by_user.user_source_id,
-  artifacts_by_user.artifact_name,
-  null as eigentrust_verification,
-  passport_scores.evidence_rawScore
-    >= passport_scores.evidence_threshold as passport_verification  
+    artifacts_by_user.user_id,
+    artifacts_by_user.user_source,
+    artifacts_by_user.user_source_id,
+    artifacts_by_user.artifact_name,
+    CAST(
+      true as bool
+    ) as eigentrust_verification,
+    CAST(
+      passport_scores.evidence_rawscore
+      >= passport_scores.evidence_threshold as bool
+    ) as passport_verification
   from {{ ref('int_artifacts_by_user') }} as artifacts_by_user
   left join {{ ref('stg_passport__scores') }} as passport_scores
     on artifacts_by_user.artifact_name = passport_scores.passport_address
@@ -19,5 +23,5 @@ select
   artifact_name
 from user_model
 where
-  passport_verification = true
-  or eigentrust_verification = true
+  passport_verification is true
+  or eigentrust_verification is true
