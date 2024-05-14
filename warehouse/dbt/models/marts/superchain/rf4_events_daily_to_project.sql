d62c70d53d91235e2ee6bd2ac5273e08991a57ad
ccerv1 <carl.cervone@gmail.com>
2024-05-14 13:05:00 -0400

    add trusted transactions table

--- warehouse/dbt/models/marts/superchain/rf4_events_daily_to_project.sql
+++ warehouse/dbt/models/marts/superchain/rf4_events_daily_to_project.sql
@@ -6,9 +6,7 @@ with events as (
   select
     project_id,
     from_artifact_id,
-    from_artifact_name,
     to_artifact_id,
-    to_artifact_name,
     event_source,
     event_type,
     TIMESTAMP_TRUNC(time, day) as bucket_day,
@@ -27,28 +25,31 @@ with events as (
   group by
     project_id,
     from_artifact_id,
-    from_artifact_name,
     to_artifact_id,
-    to_artifact_name,
     event_source,
     event_type,
     TIMESTAMP_TRUNC(time, day)
 )
 
 select
-  bucket_day,
-  project_id,
-  from_artifact_id,
-  from_artifact_name,
-  to_artifact_id,
-  to_artifact_name,
-  event_source,
-  event_type,
-  amount,
+  events.bucket_day,
+  events.project_id,
+  projects_v1.project_name,
+  from_artifacts.artifact_name as from_artifact_name,
+  to_artifacts.artifact_name as to_artifact_name,
+  events.event_source,
+  events.event_type,
+  events.amount,
   (
-    from_artifact_name in (
+    from_artifacts.artifact_name in (
       select artifact_name
       from {{ ref('rf4_trusted_users') }}
     )
   ) as is_from_trusted_user
 from events
+left join {{ ref('artifacts_v1') }} as to_artifacts
+  on events.to_artifact_id = to_artifacts.artifact_id
+left join {{ ref('artifacts_v1') }} as from_artifacts
+  on events.from_artifact_id = from_artifacts.artifact_id
+left join {{ ref('projects_v1') }}
+  on events.project_id = projects_v1.project_id
