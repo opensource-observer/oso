3835222a6ed475f29933d11b2995ea539e4702a0
ccerv1 <carl.cervone@gmail.com>
2024-05-19 16:10:14 -0400

    add: code metric commits/prs/issue counts

---
+++ warehouse/dbt/models/intermediate/metrics/code/int_code_metric__commits_prs_issues.sql
@@ -0,0 +1,22 @@
+select
+  events.project_id,
+  events.event_source as network,
+  time_intervals.time_interval,
+  CONCAT(LOWER(events.event_type), '_count') as metric,
+  SUM(events.amount) as amount
+from {{ ref('int_events_daily_to_project') }} as events
+cross join {{ ref('int_time_intervals') }} as time_intervals
+where
+  events.bucket_day >= time_intervals.start_date
+  and events.event_type in (
+    'COMMIT_CODE',
+    'PULL_REQUEST_OPENED',
+    'PULL_REQUEST_MERGED',
+    'ISSUE_OPENED',
+    'ISSUE_CLOSED'
+  )
+group by
+  events.project_id,
+  events.event_source,
+  time_intervals.time_interval,
+  events.event_type
