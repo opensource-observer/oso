87c14cd0493ca77ebb67eb6dcd410bd244b6008b
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-05-22 21:51:01 +0000

    Fixes deployer assocations

--- warehouse/dbt/macros/models/factory_deployments.sql
+++ warehouse/dbt/macros/models/factory_deployments.sql
@@ -1,12 +1,56 @@
-{% macro factory_deployments(network_name, traces="traces") %}
-SELECT block_timestamp, transaction_hash, from_address as factory_address, to_address as contract_address,  
-FROM {{ source(network_name, traces) }}
+{% macro factory_deployments(
+    network_name, 
+    traces="traces", 
+    transactions_source=None, 
+    transactions_table_transaction_hash_column="hash", 
+    traces_table_transaction_hash_column="transaction_hash", 
+    block_timestamp_column="block_timestamp") 
+%}
+
+{% if not transactions_source %}
+{% set transactions_source = source(network_name, "transactions") %}
+{% endif %}
+{% set transactions_table_hash_value = "`txs`.`%s`" % (
+    transactions_table_transaction_hash_column,
+  )
+%}
+{% set traces_table_hash_value = "`traces`.`%s`" % (
+    traces_table_transaction_hash_column,
+  )
+%}
+
+WITH transactions AS (
+  SELECT *
+  FROM {{ transactions_source }}
+  {% if is_incremental() %}
+  WHERE {{ block_timestamp_column }} > TIMESTAMP_SUB(_dbt_max_partition, INTERVAL 1 DAY)
+  {% else %}
+  {{ playground_filter(block_timestamp_column, is_start=False) }}
+  {% endif %}
+)
+
+select 
+  traces.block_timestamp, 
+  traces.transaction_hash, 
+  {# this is the eoa that triggered the request #}
+  txs.from_address as originating_address, 
+  {# 
+    It is possible for originating_contract and factory address to be the same.
+    In the case of something like a safe, the originating_contract would be the
+    safe.
+  #}
+  txs.to_address as originating_contract,
+  traces.from_address as factory_address, 
+  traces.to_address as contract_address
+from {{ source(network_name, traces) }} as traces
+inner join transactions as txs
+  on {{ transactions_table_hash_value }} = {{ traces_table_hash_value }}
 
 WHERE LOWER(trace_type) in ("create", "create2") and status = 1
 {% if is_incremental() %}
-  and block_timestamp > TIMESTAMP_SUB(_dbt_max_partition, INTERVAL 1 DAY)
+  and {{ block_timestamp_column }} > TIMESTAMP_SUB(_dbt_max_partition, INTERVAL 1 DAY)
 {% else %}
-{{ playground_filter("block_timestamp", is_start=False) }}
+{{ playground_filter(block_timestamp_column, is_start=False) }}
 {% endif %}
 
 {% endmacro %}
