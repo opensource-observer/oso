6c2eb7f7bd5dddfcffa521928aed4cf48429913b
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2024-05-22 17:50:56 -0400

    v0 implementation of eigentrust for trusted user model (#1495)
    * adds staging model and quick trusted user implementation
    
    * fix: date to time

---
+++ warehouse/dbt/models/staging/karma3/stg_karma3__globaltrust.sql
@@ -0,0 +1,11 @@
+{#
+  Get all Karma3 EigenTrust scores
+  for Farcaster IDs
+#}
+
+select
+  strategy_id,
+  CAST(i as string) as farcaster_id,
+  CAST(v as numeric) as eigentrust_rank,
+  CAST(date as timestamp) as snapshot_time
+from {{ source("karma3", "globaltrust") }}
