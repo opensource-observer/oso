e1f317cffe78d8444509e927cdd0ccafc3690064
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2024-05-27 07:20:02 -0400

    feat: additional rf4 repo eligibility checks (#1535)
    * feat: additional rf4 repo eligibility checks
    
    * chore: add repo checks to overall project checks
    
    * fix: clean up todos
    
    * fix: handling empty arrays

--- warehouse/dbt/models/marts/superchain/rf4_project_verification.sql
+++ warehouse/dbt/models/marts/superchain/rf4_project_verification.sql
@@ -5,10 +5,23 @@
   - Review thresholds for unique_addresses, date_first_transaction, and
     days_with_onchain_activity_in_range.
   - Filter on contracts that are linked to Retro Funding applications (from Agora data)
-  - Integrate with repo_stats_by_project to check licensing and other repo requirements.
 #}
 
-with project_stats as (
+with repo_stats as (
+  select
+    project_id,
+    ARRAY_AGG(repo_name) as eligible_repos
+  from (
+    select
+      project_id,
+      CONCAT(artifact_namespace, '/', artifact_name) as repo_name
+    from {{ ref('rf4_repo_stats_by_project') }}
+    where approval_status = 'approved'
+  )
+  group by project_id
+),
+
+onchain_stats as (
   select
     project_id,
     project_name,
@@ -23,23 +36,38 @@ with project_stats as (
   from {{ ref('rf4_events_daily_to_project') }}
   where
     event_type = 'CONTRACT_INVOCATION_SUCCESS_DAILY_COUNT'
-    and bucket_day >= '2024-01-01'
+    and bucket_day >= '2023-10-01'
   group by
     project_id,
     project_name
 ),
 
-tests as (
+project_stats as (
+  select
+    onchain_stats.*,
+    COALESCE(repo_stats.eligible_repos, array<STRING>[]) as eligible_repos
+  from onchain_stats
+  left join repo_stats
+    on onchain_stats.project_id = repo_stats.project_id
+  left join {{ ref('projects_by_collection_v1') }}
+    on onchain_stats.project_id = projects_by_collection_v1.project_id
+  where
+    projects_by_collection_v1.collection_name = 'op-onchain'
+),
+
+checks as (
   select
     project_id,
     project_name,
+    eligible_repos,
     unique_addresses,
     date_first_transaction,
     days_with_onchain_activity_in_range,
-    unique_addresses >= 420 as test_unique_addresses,
-    date_first_transaction < '2024-03-01' as test_date_first_transaction,
+    ARRAY_LENGTH(eligible_repos) >= 1 as check_eligible_repos,
+    unique_addresses >= 420 as check_unique_addresses,
+    date_first_transaction < '2024-03-01' as check_date_first_transaction,
     days_with_onchain_activity_in_range >= 10
-      as test_days_with_onchain_activity_in_range
+      as check_days_with_onchain_activity_in_range
   from project_stats
 )
 
@@ -49,9 +77,14 @@ select
   unique_addresses,
   date_first_transaction,
   days_with_onchain_activity_in_range,
-  test_unique_addresses,
-  test_date_first_transaction,
-  test_days_with_onchain_activity_in_range,
-  test_unique_addresses and test_date_first_transaction
-  and test_days_with_onchain_activity_in_range as test_all
-from tests
+  check_eligible_repos,
+  check_unique_addresses,
+  check_date_first_transaction,
+  check_days_with_onchain_activity_in_range,
+  (
+    check_eligible_repos
+    and check_unique_addresses
+    and check_date_first_transaction
+    and check_days_with_onchain_activity_in_range
+  ) as meets_all_requirements
+from checks
