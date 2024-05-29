711d56540546dff4ff706be7967f7492b638eb1b
Carl Cervone <carl.cervone@gmail.com>
2024-05-29 16:38:11 -0400

    refactor int_artifacts_by_project

--- warehouse/dbt/models/intermediate/directory/int_artifacts_by_project.sql
+++ warehouse/dbt/models/intermediate/directory/int_artifacts_by_project.sql
@@ -1,9 +1,15 @@
 select distinct
-  project_id,
-  artifact_id,
-  artifact_source_id,
-  artifact_source,
-  artifact_namespace,
-  artifact_name,
-  artifact_url
-from {{ ref('int_all_artifacts') }}
+  artifacts.artifact_id,
+  artifacts.artifact_source_id,
+  artifacts.artifact_source,
+  artifacts.artifact_namespace,
+  artifacts.artifact_name,
+  artifacts.artifact_url,
+  artifacts.project_id,
+  projects.project_source,
+  projects.project_namespace,
+  projects.project_name
+from {{ ref('int_all_artifacts') }} as artifacts
+left join {{ ref('int_projects') }} as projects
+  on artifacts.project_id = projects.project_id
+where artifacts.project_id is not null
