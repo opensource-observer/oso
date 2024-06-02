acdfb46d5a32b53d2bc118a5255c08f2636ea383
Reuven Gonzales <reuven@karibalabs.co>
2024-06-01 20:46:24 -0400

    add metal data sources

--- warehouse/oso_dagster/assets.py
+++ warehouse/oso_dagster/assets.py
@@ -238,6 +238,53 @@ mode_traces = goldsky_asset(
     ),
 )
 
+metal_blocks = goldsky_asset(
+    GoldskyConfig(
+        key_prefix="metal",
+        name="blocks",
+        source_name="metal-blocks",
+        project_id="opensource-observer",
+        destination_table_name="metal_blocks",
+        working_destination_dataset_name="oso_raw_sources",
+        destination_dataset_name="superchain",
+        partition_column_name="timestamp",
+        partition_column_transform=lambda c: f"TIMESTAMP_SECONDS(`{c}`)",
+        checks=[blocks_checks()],
+    ),
+)
+
+metal_transactions = goldsky_asset(
+    GoldskyConfig(
+        key_prefix="metal",
+        name="transactions",
+        source_name="metal-receipt_transactions",
+        project_id="opensource-observer",
+        destination_table_name="metal_transactions",
+        working_destination_dataset_name="oso_raw_sources",
+        destination_dataset_name="superchain",
+        partition_column_name="block_timestamp",
+        partition_column_transform=lambda c: f"TIMESTAMP_SECONDS(`{c}`)",
+        schema_overrides=[SchemaField(name="value", field_type="BYTES")],
+        checks=[transactions_checks()],
+    ),
+)
+
+
+metal_traces = goldsky_asset(
+    GoldskyConfig(
+        key_prefix="metal",
+        name="traces",
+        source_name="metal-traces",
+        project_id="opensource-observer",
+        destination_table_name="metal_traces",
+        working_destination_dataset_name="oso_raw_sources",
+        destination_dataset_name="superchain",
+        partition_column_name="block_timestamp",
+        partition_column_transform=lambda c: f"TIMESTAMP_SECONDS(`{c}`)",
+        checks=[traces_checks()],
+    ),
+)
+
 optimism_traces = goldsky_asset(
     GoldskyConfig(
         key_prefix="optimism",
