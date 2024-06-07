1da6fa44438cec840751ad90e44fe408790a6974
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2024-06-07 09:53:44 -0500

    add: identify proxy interactions (#1597)
    * add: identify proxy interactions
    
    * add: proxy models for base and op
    
    * fix: include trace ids and get staging models to work
    
    * chore: proxies for other networks
    
    * fix: update proxy model to include to_ addresses

---
+++ warehouse/dbt/models/staging/base/stg_base__proxies.sql
@@ -0,0 +1,19 @@
+{{
+  config(
+    materialized='incremental',
+    partition_by={
+      "field": "block_timestamp",
+      "data_type": "timestamp",
+      "granularity": "day",
+    },
+    unique_key="id",
+    on_schema_change="append_new_columns",
+    incremental_strategy="insert_overwrite"
+  )
+}}
+{% if is_incremental() %}
+  {% set start = "TIMESTAMP_SUB(_dbt_max_partition, INTERVAL 1 DAY)" %}
+{% else %}
+  {% set start = "'1970-01-01'" %}
+{% endif %}
+{{ known_proxies("base", start) }}
