c38423941a5c3f97a743f8ee537720f6fd6e34be
ccerv1 <carl.cervone@gmail.com>
2024-06-09 16:44:08 -0400

    refactor: int_all_artifacts from modular int models

---
+++ warehouse/dbt/models/intermediate/directory/int_contracts_by_project.sql
@@ -0,0 +1,50 @@
+{{
+  config(
+    materialized='table'
+  )
+}}
+
+with contracts as (
+  select
+    block_timestamp,
+    transaction_hash,
+    network,
+    deployer_address,
+    factory_deployer_address,
+    contract_address
+  from {{ ref('int_derived_contracts') }}
+),
+
+deployers as (
+  select
+    project_id,
+    artifact_name as deployer_address,
+    artifact_source as network
+  from {{ ref('int_deployers_by_project') }}
+),
+
+discovered_contracts as (
+  select
+    deployers.project_id,
+    contracts.block_timestamp,
+    contracts.transaction_hash,
+    contracts.network,
+    contracts.deployer_address,
+    contracts.factory_deployer_address,
+    contracts.contract_address
+  from contracts
+  left join deployers
+    on
+      contracts.deployer_address = deployers.deployer_address
+      and contracts.network = deployers.network
+  where deployers.deployer_address is not null
+)
+
+select
+  project_id,
+  {{ oso_id("network", "contract_address") }} as artifact_id,
+  network as artifact_source,
+  contract_address as artifact_source_id,
+  LOWER(network) as artifact_namespace,
+  contract_address as artifact_name
+from discovered_contracts
