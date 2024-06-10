2d72b054e4aefa60831738ce85eb8ddff0f63d8d
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2024-06-10 12:25:29 -0400

    feat: rf4 contract discovery part 2 (#1613)
    * refactor: contract discovery for rf4
    
    * feat: discovery 3rd level contracts

--- warehouse/dbt/models/marts/superchain/verification/rf4_contracts_by_app.sql
+++ warehouse/dbt/models/marts/superchain/verification/rf4_contracts_by_app.sql
@@ -1,4 +1,30 @@
-with contracts as (
+{% set networks = ["optimism", "base", "frax", "metal", "mode", "zora"] %}
+
+{% set union_factory_queries = [] %}
+
+{% for network in networks %}
+
+  {% set network_upper = network.upper() %}
+
+  {% set factory_table = "stg_" ~ network ~ "__factories" %}
+  {% set query %}
+  select
+    factory_address,
+    contract_address,
+    '{{ network_upper }}' as network
+  from {{ ref(factory_table) }}
+  {% endset %}
+  {% do union_factory_queries.append(query) %}
+
+{% endfor %}
+
+{% set all_factories = union_factory_queries | join(' union all ') %}
+
+with factories as (
+  {{ all_factories }}
+),
+
+app_contracts as (
   select
     application_id,
     project_name,
@@ -18,6 +44,37 @@ with contracts as (
     discovery_method
   from {{ ref('rf4_agora_contract_discovery') }}
   where artifact_type = 'CONTRACT'
+),
+
+discovered_contracts as (
+  select
+    app_contracts.application_id,
+    app_contracts.project_name,
+    factories.contract_address as address,
+    factories.network,
+    'CONTRACT' as artifact_type,
+    'discovered_contract_from_verified_factory' as discovery_method
+  from factories
+  left join app_contracts
+    on
+      factories.factory_address = app_contracts.address
+      and factories.network = app_contracts.network
+),
+
+contracts as (
+  select
+    application_id,
+    address,
+    network,
+    discovery_method
+  from discovered_contracts
+  union all
+  select
+    application_id,
+    address,
+    network,
+    discovery_method
+  from app_contracts
 )
 
 select distinct
