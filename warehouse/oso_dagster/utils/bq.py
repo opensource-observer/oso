5ae1dba3ed534674157d2899d9de13e74dd05d21
Reuven Gonzales <reuven@karibalabs.co>
2024-06-12 12:13:32 +0100

    Fix GCS loading failures (#1621)
    * Update retry logic for bq dataset update
    
    * ensure updating dataset is reliable

--- warehouse/oso_dagster/utils/bq.py
+++ warehouse/oso_dagster/utils/bq.py
@@ -3,9 +3,8 @@ from dataclasses import dataclass, field
 
 from google.cloud.bigquery import DatasetReference, AccessEntry, Client as BQClient
 from google.cloud.bigquery.enums import EntityTypes
-from google.cloud.exceptions import NotFound
-from dagster_gcp import BigQueryResource
-from dagster import DagsterLogManager
+from google.cloud.exceptions import NotFound, PreconditionFailed
+from .retry import retry
 
 
 @dataclass
@@ -19,21 +18,31 @@ def ensure_dataset(client: BQClient, options: DatasetOptions):
         client.get_dataset(options.dataset_ref)
     except NotFound:
         client.create_dataset(options.dataset_ref)
-    # Manage the public settings for this dataset
-    dataset = client.get_dataset(options.dataset_ref)
-    access_entries = dataset.access_entries
-    new_entries: List[AccessEntry] = []
-    if options.is_public:
-        new_entries.append(
-            AccessEntry(
-                role="READER",
-                entity_type=EntityTypes.SPECIAL_GROUP.value,
-                entity_id="allAuthenticatedUsers",
+
+    def retry_update():
+        # Manage the public settings for this dataset
+        dataset = client.get_dataset(options.dataset_ref)
+        access_entries = dataset.access_entries
+        for entry in access_entries:
+            # Do nothing if the expected entity already has the correct access
+            if entry.entity_id == "allAuthenticatedUsers" and entry.role == "READER":
+                return
+
+        new_entries: List[AccessEntry] = []
+        if options.is_public:
+            new_entries.append(
+                AccessEntry(
+                    role="READER",
+                    entity_type=EntityTypes.SPECIAL_GROUP.value,
+                    entity_id="allAuthenticatedUsers",
+                )
             )
-        )
-    for entry in access_entries:
-        if entry.entity_id == "allAuthenticatedUsers":
-            continue
-        new_entries.append(entry)
-    dataset.access_entries = new_entries
-    client.update_dataset(dataset, ["access_entries"])
+        new_entries.extend(access_entries)
+        dataset.access_entries = new_entries
+        client.update_dataset(dataset, ["access_entries"])
+
+    def error_handler(exc: Exception):
+        if type(exc) != PreconditionFailed:
+            raise exc
+
+    retry(retry_update, error_handler)
