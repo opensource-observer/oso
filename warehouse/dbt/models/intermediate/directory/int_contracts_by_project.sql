520e6dff9f670048719f19b498ac857e18501914
ccerv1 <carl.cervone@gmail.com>
2024-06-19 10:18:23 -0400

    refactor int_contracts_by_project

--- warehouse/dbt/models/intermediate/directory/int_contracts_by_project.sql
+++ warehouse/dbt/models/intermediate/directory/int_contracts_by_project.sql
@@ -1,26 +1,10 @@
 with contracts_in_ossd as (
-  select distinct
+  select
     project_id,
     artifact_source as network,
     artifact_name as contract_address
   from {{ ref('int_artifacts_in_ossd_by_project') }}
-  where artifact_type in ('CONTRACT', 'FACTORY')
-),
-
-deployers_by_project as (
-  select
-    project_id,
-    artifact_source as network,
-    artifact_name as deployer_address
-  from {{ ref('int_deployers_by_project') }}
-),
-
-derived_contracts as (
-  select
-    network,
-    deployer_address,
-    contract_address
-  from {{ ref('int_derived_contracts') }}
+  where artifact_type = 'CONTRACT'
 ),
 
 derived_contracts_by_project as (
@@ -28,12 +12,12 @@ derived_contracts_by_project as (
     deployers_by_project.project_id,
     derived_contracts.network,
     derived_contracts.contract_address
-  from derived_contracts
-  left join deployers_by_project
+  from {{ ref('int_derived_contracts') }} as derived_contracts
+  left join {{ ref('int_deployers_by_project') }} as deployers_by_project
     on
-      derived_contracts.deployer_address = deployers_by_project.deployer_address
-      and derived_contracts.network = deployers_by_project.network
-  where deployers_by_project.deployer_address is not null
+      derived_contracts.deployer_address = deployers_by_project.artifact_name
+      and derived_contracts.network = deployers_by_project.artifact_source
+  where deployers_by_project.artifact_name is not null
 ),
 
 unified_contracts as (
@@ -53,20 +37,12 @@ unified_contracts as (
   )
 ),
 
-factories as (
-  select
-    network,
-    factory_address,
-    contract_address
-  from {{ ref('int_factories') }}
-),
-
 discovered_contracts as (
   select
     unified_contracts.project_id,
     unified_contracts.network,
     factories.contract_address
-  from factories
+  from {{ ref('int_factories') }} as factories
   left join unified_contracts
     on
       factories.factory_address = unified_contracts.contract_address
