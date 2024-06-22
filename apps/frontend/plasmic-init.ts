6a934f3f5bba465e6bab25200334430429861c0f
Raymond Cheng <me@raymondcheng.net>
2024-06-21 18:01:51 -0700

    fix: allow multiple Plasmic projects (#1692)

--- apps/frontend/plasmic-init.ts
+++ apps/frontend/plasmic-init.ts
@@ -1,13 +1,23 @@
 import { initPlasmicLoader } from "@plasmicapp/loader-nextjs/react-server-conditional";
+import _ from "lodash";
+import { assert, ensure } from "@opensource-observer/utils";
 import { PLASMIC_PROJECT_ID, PLASMIC_PROJECT_API_TOKEN } from "./lib/config";
 
+const DELIMITER = ",";
+const ids = _.compact(PLASMIC_PROJECT_ID.split(DELIMITER));
+const tokens = _.compact(PLASMIC_PROJECT_API_TOKEN.split(DELIMITER));
+assert(
+  ids.length === tokens.length,
+  `Plasmic Project ID count (${ids.length}) and API token count (${tokens.length}) mismatch`,
+);
+
 export const PLASMIC = initPlasmicLoader({
-  projects: [
-    {
-      id: PLASMIC_PROJECT_ID,
-      token: PLASMIC_PROJECT_API_TOKEN,
-    },
-  ],
+  projects: _.zip(ids, tokens).map(([id, token]) => {
+    return {
+      id: ensure(id, "Plasmic Project ID is required"),
+      token: ensure(token, "Plasmic Project API token is required"),
+    };
+  }),
   // Fetches the latest revisions, whether or not they were unpublished!
   // Disable for production to ensure you render only published changes.
   preview: false,
