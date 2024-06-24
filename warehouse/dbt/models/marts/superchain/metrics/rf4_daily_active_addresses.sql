8c999e6486b4bde6ad98e2c387f60186c3711420
ccerv1 <carl.cervone@gmail.com>
2024-06-24 13:21:10 -0400

    feat: include AA events in relevant metrics

--- warehouse/dbt/models/marts/superchain/metrics/rf4_daily_active_addresses.sql
+++ warehouse/dbt/models/marts/superchain/metrics/rf4_daily_active_addresses.sql
@@ -1,19 +1,36 @@
-with txns as (
+with transactions_std as (
   select
+    bucket_day,
     project_id,
-    from_artifact_name,
-    bucket_day
+    from_artifact_name as address
   from {{ ref('rf4_events_daily_to_project') }}
   where
     event_type = 'CONTRACT_INVOCATION_SUCCESS_DAILY_COUNT'
     and bucket_day >= '2023-10-01'
 ),
 
+transactions_4337 as (
+  select
+    bucket_day,
+    project_id,
+    to_artifact_name as address
+  from {{ ref('rf4_4337_events') }}
+  where
+    event_type = '4337_INTERACTION'
+    and bucket_day >= '2023-10-01'
+),
+
+txns as (
+  select * from transactions_std
+  union all
+  select * from transactions_4337
+),
+
 daas as (
   select
     project_id,
     bucket_day,
-    COUNT(distinct from_artifact_name) as active_addresses
+    COUNT(distinct address) as active_addresses
   from txns
   group by
     project_id,
