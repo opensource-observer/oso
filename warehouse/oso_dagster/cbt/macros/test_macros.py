9316f594ec1430d0057633af7c38294c0a21d4d2
Reuven Gonzales <reuven@karibalabs.co>
2024-06-25 23:11:32 -0700

    Add pyright to ci (#1715)
    * Setup pyright
    
    * Fix tests
    
    * Parallelize github workflow
    
    * multiple steps require dbt
    
    * Using some 3.12 only things now
    
    * fix
    
    * better setup

--- warehouse/oso_dagster/cbt/macros/test_macros.py
+++ warehouse/oso_dagster/cbt/macros/test_macros.py
@@ -5,7 +5,7 @@ import pandas as pd
 import pytest
 from oso_dagster.cbt.context import context_query_from_str, DataContext
 from oso_dagster.cbt.duckdb import DuckDbConnector
-from oso_dagster.cbt.macros.time_constrain import *
+from oso_dagster.cbt.macros.time_constrain import *  # type: ignore
 from oso_dagster.utils.testing.duckdb import DuckDbFixture
 
 SELECT_NO_CTE = """
@@ -47,6 +47,7 @@ def db():
                 [arrow.get("2024-02-01").datetime, 2, "bravo"],
                 [arrow.get("2024-03-01").datetime, 3, "charlie"],
                 [arrow.get("2024-04-01").datetime, 4, "delta"],
+                [arrow.get("2024-02-01").datetime, 5, "foxtrot"],
             ],
         }
     )
@@ -90,7 +91,7 @@ def test_time_constrain_succeed(
             SELECT_WITH_JOIN,
             "time_id_and_name",
             dict(start=arrow.get("2024-02-01"), end=arrow.get("2024-04-01")),
-            5,
+            2,
         ),
     ],
 )
