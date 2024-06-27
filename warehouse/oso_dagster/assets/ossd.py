95a8a64d1111ce7eb516defcef059502387be737
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-06-27 05:23:46 +0000

    Add date columns to the tables

--- warehouse/oso_dagster/assets/ossd.py
+++ warehouse/oso_dagster/assets/ossd.py
@@ -5,11 +5,16 @@ from dagster import (
     AssetOut,
     AssetExecutionContext,
     JsonMetadataValue,
+    Config,
 )
 from ossdirectory import fetch_data
 
 import polars as pl
 import arrow
+
+
+class OSSDirectoryConfig(Config):
+    force: bool = False
 
 
 @multi_asset(
@@ -19,7 +24,8 @@ import arrow
     },
     can_subset=True,
 )
-def ossdirectory_repo(context: AssetExecutionContext):
+def ossdirectory_repo(context: AssetExecutionContext, config: OSSDirectoryConfig):
+
     data = fetch_data()
 
     if not data.meta:
@@ -30,18 +36,43 @@ def ossdirectory_repo(context: AssetExecutionContext):
         latest_materialization = context.instance.get_latest_materialization_event(
             asset_key=asset_key
         )
-        if latest_materialization and latest_materialization.asset_materialization:
+        if (
+            latest_materialization
+            and latest_materialization.asset_materialization
+            and not config.force
+        ):
             repo_meta = latest_materialization.asset_materialization.metadata.get(
                 "repo_meta", {}
             )
             if repo_meta:
                 repo_meta = cast(JsonMetadataValue, repo_meta)
                 repo_meta_dict = cast(dict, repo_meta.data)
+                context.log.debug(
+                    {
+                        "message": "repo_meta",
+                        "repo_meta": repo_meta_dict,
+                    }
+                )
                 if repo_meta_dict.get("sha", "") == data.meta.sha:
                     context.log.info(f"no changes for {output}")
                     continue
+        context.log.debug(data.meta.committed_datetime)
+        context.log.debug(type(data.meta.committed_datetime))
+        committed_dt = data.meta.committed_datetime
 
         df = pl.from_dicts(getattr(data, output))
+        df = df.with_columns(
+            sha=pl.lit(bytes.fromhex(data.meta.sha)),
+            committed_time=pl.datetime(
+                committed_dt.year,
+                committed_dt.month,
+                committed_dt.day,
+                committed_dt.hour,
+                committed_dt.minute,
+                committed_dt.second,
+            ),
+        )
+
         # Add sync time and commit sha to the dataframe
         yield Output(
             df,
