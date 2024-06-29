d172522f44f4effe06c89d7b78b700bb51e4d726
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-06-29 00:21:39 +0000

    test embedded elt

--- warehouse/oso_dagster/assets/__init__.py
+++ warehouse/oso_dagster/assets/__init__.py
@@ -13,3 +13,5 @@ from .optimism import *
 from .pgn import *
 from .zora import *
 from .ossd import *
+
+# from .ecosystems import *
---
+++ warehouse/oso_dagster/assets/ecosystems.py
@@ -0,0 +1,6 @@
+# from ..dlt_sources import sql_asset
+
+# connection_string = "postgresql://postgres:postgres@127.0.0.1:5432/postgres"
+
+
+# ecosystems = sql_asset("ecosystems", connection_string)
