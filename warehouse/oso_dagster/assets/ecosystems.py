fd7cbce00f6dbfa3bc11f4f66f987078b2318312
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-07-02 15:47:55 +0000

    Working incremental config and secrets

--- warehouse/oso_dagster/assets/ecosystems.py
+++ warehouse/oso_dagster/assets/ecosystems.py
@@ -1,8 +1,9 @@
+from dlt.sources import incremental
 from ..factories.sql import sql_assets
+from ..utils.secrets import SecretReference
 
-# TODO... replace this with a reference to the secret suffix to use in google's
-# secret manager
-connection_string = "postgresql://postgres:postgres@127.0.0.1:5432/postgres"
-
-
-ecosystems = sql_assets("ecosystems", connection_string, [{"table": "fake_timeseries"}])
+ecosystems = sql_assets(
+    "ecosystems",
+    SecretReference(group_name="ecosystems", key="main_database"),
+    [{"table": "fake_timeseries", "incremental": incremental("time")}],
+)
