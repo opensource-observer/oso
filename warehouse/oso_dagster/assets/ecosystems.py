5771a5c3451d41ac5759ea1a9496f000de5fd963
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-07-03 06:40:44 +0000

    configure ecosystems advisories

--- warehouse/oso_dagster/assets/ecosystems.py
+++ warehouse/oso_dagster/assets/ecosystems.py
@@ -1,9 +1,32 @@
 from dlt.sources import incremental
-from ..factories.sql import sql_assets
-from ..utils.secrets import SecretReference
+from oso_dagster.factories import sql_assets
+from oso_dagster.utils import SecretReference
 
-ecosystems = sql_assets(
+# ecosystems_papers = sql_assets(
+#     "ecosystems",
+#     SecretReference(group_name="ecosystems", key="papers_database"),
+#     [
+#         {
+#             "table": "papers",
+#             "incremental": incremental("updated_at"),
+#             "destination_table_name": "papers",
+#         },
+#         {
+#             "table": "projects",
+#             "incremental": incremental("updated_at"),
+#             "destination_table_name": "papers_projects",
+#         },
+#     ],
+# )
+
+ecosystems_advisories = sql_assets(
     "ecosystems",
-    SecretReference(group_name="ecosystems", key="main_database"),
-    [{"table": "fake_timeseries", "incremental": incremental("time")}],
+    SecretReference(group_name="ecosystems", key="advisories_databae"),
+    [
+        {
+            "table": "advisories",
+            "incremental": incremental("updated_at"),
+        }
+    ],
+    group_name="advisories",
 )
