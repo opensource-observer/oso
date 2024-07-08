b379a43d5e23e6436a1a1109330a9a31a0fbd430
chuxinh <chuxin@oplabs.co>
2024-07-08 14:39:39 +1000

    add macro and stg tables

---
+++ warehouse/dbt/macros/models/first_time_addresses.sql
@@ -0,0 +1,35 @@
+{% macro first_time_addresses(network_name, block_timestamp_column="block_timestamp") %}
+-- todo:
+-- 1. add first funded by
+-- 2. most funded by
+select 
+    address
+    ,chain_name
+    ,first_block_timestamp
+    ,date_trunc(first_block_timestamp, day) as first_active_day
+    ,format_timestamp('%Y-%m', date_trunc(first_block_timestamp, month)) as month_cohort
+    ,first_block_number
+    ,first_tx_to
+    ,first_tx_hash
+    ,first_method_id
+from (
+    select 
+        from_address as address
+        ,'{{ network_name }}' as chain_name
+        ,min(block_timestamp) as first_block_timestamp
+        ,min(block_number) as first_block_number
+        ,min_by(to_address, block_number) as first_tx_to
+        ,min_by(`hash`, block_number) as first_tx_hash
+        ,min_by(substring(input, 1, 10), block_number) as first_method_id
+    from {{ source(network_name, "transactions") }}
+    {% if is_incremental() %}
+    where {{ block_timestamp_column }} > TIMESTAMP_SUB(_dbt_max_partition, INTERVAL 1 DAY)
+    {% else %}
+    {{ playground_filter(block_timestamp_column, is_start=True) }}
+    {% endif %}
+    and gas_price > 0
+    and receipt_status = 1
+    and receipt_gas_used > 0
+    group by 1, 2
+)
+{% endmacro %}
