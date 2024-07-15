35d63a26688e4e2954a68878401222c61312f8a6
Raymond Cheng <me@raymondcheng.net>
2024-07-15 07:31:55 -0700

    feat: Update Dagster utils to support bq2clickhouse copy (#1797)
    * BigQuery export to GCS
    * Clickhouse create table, drop table, rename table, import data
    * Adds some more comments to dbt and common
    * GCS batch delete a folder in a bucket

--- warehouse/oso_dagster/utils/bq.py
+++ warehouse/oso_dagster/utils/bq.py
@@ -1,19 +1,43 @@
-from typing import List
+from typing import List, Optional
 from dataclasses import dataclass
 
-from google.cloud.bigquery import DatasetReference, AccessEntry, Client as BQClient
+from google.cloud.bigquery import DatasetReference, AccessEntry, Client as BQClient, ExtractJobConfig
 from google.cloud.bigquery.enums import EntityTypes
 from google.cloud.exceptions import NotFound, PreconditionFailed
 from .retry import retry
 
+# Configuration for a BigQuery Dataset
+@dataclass(kw_only=True)
+class BigQueryDatasetConfig:
+    # GCP project ID
+    project_id: str
+    # BigQuery dataset
+    dataset_name: str
+    # Service account
+    service_account: Optional[str]
+
+@dataclass(kw_only=True)
+class BigQueryTableConfig(BigQueryDatasetConfig):
+    # BigQuery table
+    table_name: str
 
 @dataclass
 class DatasetOptions:
     dataset_ref: DatasetReference
     is_public: bool = False
 
-
 def ensure_dataset(client: BQClient, options: DatasetOptions):
+    """
+    Create a public dataset if missing
+    This will automatically add `allAuthenticatedUsers` with read-only access
+
+    Parameters
+    ----------
+    client: BQClient
+        The Google BigQuery client
+    options: DatasetOptions
+    """
+    
     try:
         client.get_dataset(options.dataset_ref)
     except NotFound:
@@ -46,3 +70,41 @@ def ensure_dataset(client: BQClient, options: DatasetOptions):
             raise exc
 
     retry(retry_update, error_handler)
+
+def export_to_gcs(bq_client: BQClient, bq_table_config: BigQueryTableConfig, gcs_path: str):
+    """
+    Export a BigQuery table to partitioned CSV files in GCS
+
+    Parameters
+    ----------
+    bq_client: BQClient
+        BigQuery client
+    bq_table_config: BigQueryTableConfig
+        BigQuery table configuration
+    gcs_path: str
+        GCS path to export to
+
+    Returns
+    -------
+    str
+        gcs_path
+    """
+    dataset_ref = bq_client.dataset(dataset_id=bq_table_config.dataset_name)
+    table_ref = dataset_ref.table(bq_table_config.table_name)
+    destination_uri = f"{gcs_path}/*.parquet"
+    # Reference:
+    # https://cloud.google.com/python/docs/reference/bigquery/latest/google.cloud.bigquery.job.ExtractJobConfig
+    extract_job = bq_client.extract_table(
+        table_ref,
+        destination_uri,
+        location="US",
+        # When importing into Clickhouse, note the conventions used for compression
+        # https://clickhouse.com/docs/en/sql-reference/table-functions/s3
+        job_config=ExtractJobConfig(
+            print_header=False,
+            destination_format="PARQUET",
+            #compression="ZSTD",
+        ),
+    )
+    extract_job.result()
+    return destination_uri
--- warehouse/oso_dagster/utils/gcs.py
+++ warehouse/oso_dagster/utils/gcs.py
@@ -1,16 +1,67 @@
 from google.cloud.storage import Client
 from typing import List
+from .errors import MalformedUrl
 
+def gcs_to_http_url(gcs_path: str) -> str:
+    """
+    Converts a `gcs://` url to a `https://storage.googleapis.com/` url
+
+    Parameters
+    ----------
+    gcs_path: str
+        URL prefixed with gcs://
+    
+    Returns
+    -------
+    str
+        HTTPS URL to the GCS object
+    """
+    if not gcs_path.startswith("gs://"):
+        raise MalformedUrl(f"Expected gs:// prefix in {gcs_path}")
+    return gcs_path.replace("gs://", "https://storage.googleapis.com/")
 
 def batch_delete_blobs(
     gcs_client: Client, bucket_name: str, blobs: List[str], batch_size: int
 ):
+    """
+    Batch delete blobs
+
+    Parameters
+    ----------
+    gcs_client: Client
+        The Google Cloud Storage client
+    bucket_name: str
+        GCS bucket name
+    blobs: List[str]
+        List of GCS blobs to delete
+    batch_size: int
+        Number of blobs to delete at the same time
+    """
     bucket = gcs_client.bucket(bucket_name)
 
     batch: List[str] = []
     for blob in blobs:
         batch.append(blob)
         if len(batch) == batch_size:
+            # TODO: is this a bug? Should we reset the batch?
             bucket.delete_blobs(blobs=batch)
     if len(batch) > 0:
         bucket.delete_blobs(blobs=batch)
+
+def batch_delete_folder(gcs_client: Client, bucket_name: str, prefix: str):
+    """
+    Enumerates blobs within a bucket and batch delete
+
+    Parameters
+    ----------
+    gcs_client: Client
+        The Google Cloud Storage client
+    bucket_name: str
+        GCS bucket name
+    prefix: str
+        Folder prefix to delete
+    """
+    bucket = gcs_client.get_bucket(bucket_name)
+    blobs = bucket.list_blobs(prefix=prefix)
+    for blob in blobs:
+        blob.delete()
