cd5f36718645bf99a44e879981d024afbd1447a1
ccerv1 <carl.cervone@gmail.com>
2024-07-19 17:29:47 -0400

    fix: dedupe repos in staging

--- warehouse/dbt/models/staging/oss-directory/stg_ossd__current_repositories.sql
+++ warehouse/dbt/models/staging/oss-directory/stg_ossd__current_repositories.sql
@@ -1,26 +1,29 @@
 {# 
   The most recent view of repositories from the github-resolve-repos cloudquery plugin.
 #}
-with most_recent_sync as (
-  select MAX(ingestion_time) as ingestion_time
+with ranked_repositories as (
+  select
+    node_id,
+    id,
+    url,
+    name,
+    name_with_owner,
+    owner,
+    branch,
+    star_count,
+    watcher_count,
+    fork_count,
+    is_fork,
+    license_name,
+    license_spdx_id,
+    language,
+    ingestion_time,
+    ROW_NUMBER()
+      over (partition by node_id order by ingestion_time desc, id asc)
+      as row_num
   from {{ oso_source('ossd', 'repositories') }}
 )
 
-select
-  repositories.node_id,
-  repositories.id,
-  repositories.url,
-  repositories.name,
-  repositories.name_with_owner,
-  repositories.owner,
-  repositories.branch,
-  repositories.star_count,
-  repositories.watcher_count,
-  repositories.fork_count,
-  repositories.is_fork,
-  repositories.license_name,
-  repositories.license_spdx_id,
-  repositories.language,
-  repositories.ingestion_time
-from {{ oso_source('ossd', 'repositories') }} as repositories
-where repositories.ingestion_time = (select * from most_recent_sync)
+select *
+from ranked_repositories
+where row_num = 1
