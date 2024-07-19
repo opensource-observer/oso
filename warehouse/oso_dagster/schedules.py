9b302f300f11aa9ac8b0802a378d5be7014075c3
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-07-19 07:41:39 +0000

    Setup all assets to materialize daily

--- warehouse/oso_dagster/schedules.py
+++ warehouse/oso_dagster/schedules.py
@@ -2,11 +2,13 @@
 To add a daily schedule that materializes your dbt assets, uncomment the following lines.
 """
 
+from dagster import define_asset_job, ScheduleDefinition, AssetSelection
+
+full_sync = define_asset_job("run_everything_job", AssetSelection.all())
+
 schedules = [
-    #     build_schedule_from_dbt_selection(
-    #         [opensource_observer_dbt_assets],
-    #         job_name="materialize_dbt_models",
-    #         cron_schedule="0 0 * * *",
-    #         dbt_select="fqn:*",
-    #     ),
+    ScheduleDefinition(
+        job=full_sync,
+        cron_schedule="@daily",
+    )
 ]
