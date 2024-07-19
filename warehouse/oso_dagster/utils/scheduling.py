c3b76b475d7ed78f89cf412719b23de54831a6da
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-07-19 16:19:07 +0000

    More fixes

--- warehouse/oso_dagster/utils/scheduling.py
+++ warehouse/oso_dagster/utils/scheduling.py
@@ -1,12 +1,15 @@
 from dagster import FreshnessPolicy
 from .types import params_from
-from .. import constants
 
 
 @params_from(FreshnessPolicy)
 def production_freshness_policy(*args, **kwargs) -> FreshnessPolicy | None:
     """Creates a freshness policy that is automatically disabled in a
     non-production mode."""
+
+    # Lazily import this because importing constants in the utils forces the
+    # need to certain environment variables.
+    from .. import constants
 
     if constants.env == "production":
         return FreshnessPolicy(*args, **kwargs)
