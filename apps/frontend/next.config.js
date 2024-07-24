4f2f7516ed9036d801d1a02d742758dab1c31c6a
Raymond Cheng <me@raymondcheng.net>
2024-07-23 19:16:41 -0700

    feat: Apollo server

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -11,6 +11,28 @@ const nextConfig = {
       }
     : {
         // Options for non-static-export
+        async headers() {
+          return [
+            {
+              // matching all API routes
+              //source: "/api/:path*",
+              source: "/api/graphql",
+              headers: [
+                { key: "Access-Control-Allow-Credentials", value: "true" },
+                { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                {
+                  key: "Access-Control-Allow-Methods",
+                  value: "GET,DELETE,PATCH,POST,PUT",
+                },
+                {
+                  key: "Access-Control-Allow-Headers",
+                  value:
+                    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+                },
+              ],
+            },
+          ];
+        },
         async rewrites() {
           return [
             {
