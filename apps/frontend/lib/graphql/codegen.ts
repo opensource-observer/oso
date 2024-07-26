34dd44f43aed491518058a7b23d766828d39a50a
Raymond Cheng <me@raymondcheng.net>
2024-07-26 11:05:31 -0700

    feat: Apollo tweaks
    * Just checking in the parts we'll keep
    * Move API to /api/v1/
    * Move graphql codegen to lib/
    * Use dotenv-cli instead of the library

--- apps/frontend/lib/graphql/codegen.ts
+++ apps/frontend/lib/graphql/codegen.ts
@@ -1,11 +1,5 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
-import dotenv from "dotenv";
-const envPath = [__dirname, "./.env.local"].join("/");
-dotenv.config();
-dotenv.config({ path: envPath, override: true });
-
-const DB_GRAPHQL_URL = process.env.NEXT_PUBLIC_DB_GRAPHQL_URL!;
-const OSO_API_KEY = process.env.OSO_API_KEY;
+import { DB_GRAPHQL_URL, OSO_API_KEY } from "../config";
 console.log(DB_GRAPHQL_URL);
 
 const SCHEMA: Record<string, any> = {};
