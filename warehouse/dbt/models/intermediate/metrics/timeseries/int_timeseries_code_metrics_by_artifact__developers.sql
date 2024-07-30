71d79183165b9ac8d4f370f51268168f78141093
ccerv1 <carl.cervone@gmail.com>
2024-07-30 17:28:38 -0400

    fix: use rolling windows for ftdevs average in code metrics

--- warehouse/dbt/models/intermediate/metrics/timeseries/int_timeseries_code_metrics_by_artifact__developers.sql
+++ warehouse/dbt/models/intermediate/metrics/timeseries/int_timeseries_code_metrics_by_artifact__developers.sql
@@ -9,37 +9,37 @@
 with commits as (
   select
     from_artifact_id as developer_id,
-    project_id,
+    to_artifact_id,
     event_source,
     bucket_day,
     CAST(SUM(amount) > 0 as int64) as commit_count
-  from {{ ref('int_events_daily_to_project') }}
+  from {{ ref('int_events_daily_to_artifact') }}
   where event_type = 'COMMIT_CODE'
   group by
     from_artifact_id,
-    project_id,
+    to_artifact_id,
     event_source,
     bucket_day
 ),
 
-project_start_dates as (
+to_artifact_start_dates as (
   select
-    project_id,
+    to_artifact_id,
     event_source,
     MIN(bucket_day) as first_commit_date
   from commits
   group by
-    project_id,
+    to_artifact_id,
     event_source
 ),
 
 calendar as (
   select
-    project_id,
+    to_artifact_id,
     event_source,
     TIMESTAMP_ADD(first_commit_date, interval day_offset day) as bucket_day
   from
-    project_start_dates,
+    to_artifact_start_dates,
     UNNEST(
       GENERATE_ARRAY(
         0,
@@ -56,10 +56,10 @@ devs as (
   from commits
 ),
 
-developer_project_dates as (
+developer_to_artifact_dates as (
   select
     devs.developer_id,
-    calendar.project_id,
+    calendar.to_artifact_id,
     calendar.bucket_day,
     calendar.event_source
   from calendar
@@ -70,15 +70,15 @@ filled_data as (
   select
     dpd.bucket_day,
     dpd.developer_id,
-    dpd.project_id,
+    dpd.to_artifact_id,
     dpd.event_source,
     COALESCE(c.commit_count, 0) as commit_count
-  from developer_project_dates as dpd
+  from developer_to_artifact_dates as dpd
   left join commits as c
     on
       dpd.bucket_day = c.bucket_day
       and dpd.developer_id = c.developer_id
-      and dpd.project_id = c.project_id
+      and dpd.to_artifact_id = c.to_artifact_id
       and dpd.event_source = c.event_source
 ),
 
@@ -86,10 +86,10 @@ rolling_commit_days as (
   select
     bucket_day,
     developer_id,
-    project_id,
+    to_artifact_id,
     event_source,
     SUM(commit_count) over (
-      partition by developer_id, project_id, event_source
+      partition by developer_id, to_artifact_id, event_source
       order by bucket_day
       rows between 29 preceding and current row
     ) as num_commit_days
@@ -98,7 +98,7 @@ rolling_commit_days as (
 
 ft_devs as (
   select
-    project_id,
+    to_artifact_id,
     event_source,
     bucket_day,
     'fulltime_developers' as metric,
@@ -106,14 +106,14 @@ ft_devs as (
   from rolling_commit_days
   where num_commit_days >= {{ fulltime_dev_days }}
   group by
-    project_id,
+    to_artifact_id,
     event_source,
     bucket_day
 ),
 
 pt_devs as (
   select
-    project_id,
+    to_artifact_id,
     event_source,
     bucket_day,
     'parttime_developers' as metric,
@@ -121,14 +121,14 @@ pt_devs as (
   from rolling_commit_days
   where num_commit_days >= 1 and num_commit_days < {{ fulltime_dev_days }}
   group by
-    project_id,
+    to_artifact_id,
     event_source,
     bucket_day
 ),
 
 active_devs as (
   select
-    project_id,
+    to_artifact_id,
     event_source,
     bucket_day,
     'active_developers' as metric,
@@ -136,7 +136,7 @@ active_devs as (
   from rolling_commit_days
   where num_commit_days >= 1
   group by
-    project_id,
+    to_artifact_id,
     event_source,
     bucket_day
 )
