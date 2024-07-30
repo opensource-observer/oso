1eb93b01028e41fb21afeb59f8cc981b26e314b8
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2024-07-30 17:58:44 -0400

    fix: use rolling windows for ftdevs average in code metrics (#1889)

--- warehouse/dbt/models/intermediate/metrics/code/int_code_metric__fulltime_developers_average.sql
+++ warehouse/dbt/models/intermediate/metrics/code/int_code_metric__fulltime_developers_average.sql
@@ -1,51 +1,11 @@
-{% set fulltime_dev_days = 10 %}
-
-
-with developer_commit_days as (
-  select
-    project_id,
-    event_source,
-    from_artifact_id,
-    bucket_day
-  from {{ ref('int_events_daily_to_project') }}
-  where event_type = 'COMMIT_CODE'
-),
-
-rolling_commit_days as (
-  select
-    d1.project_id,
-    d1.event_source,
-    d1.from_artifact_id,
-    d1.bucket_day,
-    COUNT(distinct d2.bucket_day) as num_commit_days
-  from developer_commit_days as d1
-  inner join developer_commit_days as d2
-    on
-      d1.project_id = d2.project_id
-      and d1.from_artifact_id = d2.from_artifact_id
-      and (
-        d2.bucket_day between
-        DATE_SUB(d1.bucket_day, interval 30 day) and d1.bucket_day
-      )
-  group by
-    d1.project_id,
-    d1.event_source,
-    d1.from_artifact_id,
-    d1.bucket_day
-),
-
-ftdevs as (
+with ftdevs as (
   select
     project_id,
     event_source,
     bucket_day,
-    COUNT(distinct from_artifact_id) as amount
-  from rolling_commit_days
-  where num_commit_days >= {{ fulltime_dev_days }}
-  group by
-    project_id,
-    event_source,
-    bucket_day
+    amount
+  from {{ ref('int_timeseries_code_metrics__developers') }}
+  where metric = 'fulltime_developers'
 )
 
 select
--- warehouse/dbt/models/intermediate/metrics/code/int_code_metric_artifact__fulltime_developers_average.sql
+++ warehouse/dbt/models/intermediate/metrics/code/int_code_metric_artifact__fulltime_developers_average.sql
@@ -1,51 +1,11 @@
-{% set fulltime_dev_days = 10 %}
-
-
-with developer_commit_days as (
-  select
-    to_artifact_id,
-    event_source,
-    from_artifact_id,
-    bucket_day
-  from {{ ref('int_events_daily_to_artifact') }}
-  where event_type = 'COMMIT_CODE'
-),
-
-rolling_commit_days as (
-  select
-    d1.to_artifact_id,
-    d1.event_source,
-    d1.from_artifact_id,
-    d1.bucket_day,
-    COUNT(distinct d2.bucket_day) as num_commit_days
-  from developer_commit_days as d1
-  inner join developer_commit_days as d2
-    on
-      d1.to_artifact_id = d2.to_artifact_id
-      and d1.from_artifact_id = d2.from_artifact_id
-      and (
-        d2.bucket_day between
-        DATE_SUB(d1.bucket_day, interval 30 day) and d1.bucket_day
-      )
-  group by
-    d1.to_artifact_id,
-    d1.event_source,
-    d1.from_artifact_id,
-    d1.bucket_day
-),
-
-ftdevs as (
+with ftdevs as (
   select
     to_artifact_id,
     event_source,
     bucket_day,
-    COUNT(distinct from_artifact_id) as amount
-  from rolling_commit_days
-  where num_commit_days >= {{ fulltime_dev_days }}
-  group by
-    to_artifact_id,
-    event_source,
-    bucket_day
+    amount
+  from {{ ref('int_timeseries_code_metrics_by_artifact__developers') }}
+  where metric = 'fulltime_developers'
 )
 
 select
