14e857ac8ac157fdf909371f3649762795155788
Reuven Gonzales <reuven@karibalabs.co>
2024-07-31 09:55:25 -0700

    Reduce dbt materializations (#1894)

--- warehouse/oso_dagster/schedules.py
+++ warehouse/oso_dagster/schedules.py
@@ -8,9 +8,20 @@ materialize_all_assets = define_asset_job(
     "materialize_all_assets_job", AssetSelection.all()
 )
 
+materialize_source_assets = define_asset_job(
+    "materialize_source_assets_job",
+    AssetSelection.tag("opensource.observer/type", "source"),
+)
+
 schedules = [
+    # Run everything once a week on sunday at midnight
     ScheduleDefinition(
         job=materialize_all_assets,
-        cron_schedule="@daily",
-    )
+        cron_schedule="0 0 * * 0",
+    ),
+    # Run only source data every day (exclude sunday as it's already in the schedule above)
+    ScheduleDefinition(
+        job=materialize_source_assets,
+        cron_schedule="0 0 * * 1-6",
+    ),
 ]
