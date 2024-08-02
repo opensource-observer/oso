0d26256db995515caac1c859e23921d0f85fd044
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-08-02 06:48:07 +0000

    Remove cloudquery

--- warehouse/cloudquery-dune-contract-usage/dune_contract_usage/serve.py
+++
@@ -1,8 +0,0 @@
-import sys
-from cloudquery.sdk import serve
-from .plugin import ContractUsagePlugin
-
-
-def run():
-    p = ContractUsagePlugin()
-    serve.PluginCommand(p).run(sys.argv[1:])
--- warehouse/cloudquery-example-plugin/example_plugin/serve.py
+++
@@ -1,8 +0,0 @@
-import sys
-from cloudquery.sdk import serve
-from .plugin import ExamplePlugin
-
-
-def run():
-    p = ExamplePlugin()
-    serve.PluginCommand(p).run(sys.argv[1:])
