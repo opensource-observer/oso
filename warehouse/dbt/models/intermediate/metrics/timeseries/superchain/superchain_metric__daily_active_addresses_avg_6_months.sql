cc4e8665fe71ba3503b31867734746b725ccd56e
ccerv1 <carl.cervone@gmail.com>
2024-08-05 08:25:06 -0400

    add: DAAs model

---
+++ warehouse/dbt/models/intermediate/metrics/timeseries/superchain/superchain_metric__daily_active_addresses_avg_6_months.sql
@@ -0,0 +1,60 @@
+{% set metric = {
+    "metric_name": "daily_active_addresses_avg_6_months",
+    "metric_unit": "addresses",
+    "event_sources": [
+        "OPTIMISM", 
+        "BASE", 
+        "FRAX",
+        "METAL",
+        "MODE",
+        "ZORA"
+    ],
+    "event_types": ["CONTRACT_INVOCATION_SUCCESS_DAILY_COUNT"],
+    "to_types": ["CONTRACT"],
+    "from_types": ["EOA"],
+    "window_interval": "DAY",
+    "window_size": 180,
+    "window_missing_dates": "fill_with_zero",
+    "sampling_interval": "daily"
+} %}
+
+{{ timeseries_events(metric) }},
+
+agg_events as (
+  select
+    events.sample_date,
+    events.event_source,
+    artifacts_by_project.project_id,
+    COUNT(distinct artifacts.artifact_name) as amount
+  from timeseries_events as events
+  inner join {{ ref('artifacts_by_project_v1') }} as artifacts_by_project
+    on events.to_artifact_id = artifacts_by_project.artifact_id
+  inner join {{ ref('artifacts_v1') }} as artifacts
+    on events.to_artifact_id = artifacts.artifact_id
+  group by
+    events.sample_date,
+    events.event_source,
+    artifacts_by_project.project_id
+),
+
+windowed_events as (
+  select
+    project_id,
+    sample_date,
+    event_source,
+    AVG(amount) over (
+      partition by project_id, event_source
+      order by sample_date
+      rows between {{ metric.window_size-1 }} preceding and current row
+    ) as amount
+  from agg_events
+)
+
+select
+  project_id,
+  sample_date,
+  event_source,
+  amount,
+  '{{ metric.metric_name }}' as metric,
+  '{{ metric.metric_unit }}' as unit
+from windowed_events
