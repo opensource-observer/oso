7e69abf050f760b6220949e278e5609d2d8662cb
ccerv1 <carl.cervone@gmail.com>
2024-08-07 08:32:01 -0400

    fix: to_project_id

--- warehouse/dbt/models/intermediate/metrics/funding/int_funding_metric__grants_received_usd.sql
+++ warehouse/dbt/models/intermediate/metrics/funding/int_funding_metric__grants_received_usd.sql
@@ -5,12 +5,12 @@
 }}
 
 select
-  project_id,
+  to_project_id as project_id,
   'grants_received_usd' as metric,
   amount as amount,
   'USD' as unit,
   TIMESTAMP_TRUNC(`time`, day) as sample_date
 from {{ ref('int_oss_funding_grants_to_project') }}
 where
-  project_id is not null
+  to_project_id is not null
   and amount > 0
