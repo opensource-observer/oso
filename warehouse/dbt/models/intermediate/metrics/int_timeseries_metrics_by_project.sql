2c67b82986068c3890a4582e2b9ac9771ed5930b
ccerv1 <carl.cervone@gmail.com>
2024-08-11 13:50:39 -0400

    feat: add superchain timeseries metrics to consolidated int models

--- warehouse/dbt/models/intermediate/metrics/int_timeseries_metrics_by_project.sql
+++ warehouse/dbt/models/intermediate/metrics/int_timeseries_metrics_by_project.sql
@@ -4,7 +4,6 @@
   )
 }}
 
-
 select
   {{ oso_id('"OSO"', '"oso"', 'metric') }} as metric_id,
   metrics.project_id as project_id,
@@ -20,3 +19,11 @@ select
   amount,
   unit
 from {{ ref('int_funding_metric__grants_received_usd') }}
+union all
+select
+  {{ oso_id('"OSO"', '"oso"', 'metric', 'event_source') }} as metric_id,
+  project_id,
+  sample_date,
+  amount,
+  unit
+from {{ ref('int_timeseries_superchain_metrics__all') }}
