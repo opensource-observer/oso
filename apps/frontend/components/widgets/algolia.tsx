d1145ec173fc7163be097dc13838d2b317319c60
Raymond Cheng <me@raymondcheng.net>
2024-08-23 11:14:31 -0700

    feat: Adding AlgoliaSearchList (#1983)
    * Moving away from modal-only search for Algolia, towards a generic
      infinite list we can use in various places
    * This removes the modal component until we implement a fully-Plasmic
      version of it.

--- apps/frontend/components/widgets/algolia.tsx
+++ apps/frontend/components/widgets/algolia.tsx
@@ -2,16 +2,10 @@
 
 import "instantsearch.css/themes/algolia.css";
 import algoliasearch from "algoliasearch/lite";
-import Link from "next/link";
 import React, { ReactElement } from "react";
-import { SearchBox, Highlight, useHits } from "react-instantsearch";
+import { SearchBox, useHits } from "react-instantsearch";
 import { InstantSearchNext } from "react-instantsearch-nextjs";
-import SearchIcon from "@mui/icons-material/Search";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import Modal from "@mui/material/Modal";
-import Paper from "@mui/material/Paper";
+import { DataProvider } from "@plasmicapp/loader-nextjs";
 import {
   ALGOLIA_APPLICATION_ID,
   ALGOLIA_API_KEY,
@@ -19,94 +13,59 @@ import {
 } from "../../lib/config";
 
 const searchClient = algoliasearch(ALGOLIA_APPLICATION_ID, ALGOLIA_API_KEY);
-const PROJECT_PREFIX = "/project";
-const createLink = (slug: string) => {
-  return `${PROJECT_PREFIX}/${slug}`;
+const PLASMIC_KEY = "searchItem";
+
+type AlgoliaSearchListProps = {
+  className?: string; // Plasmic CSS class
+  children?: ReactElement; // Show this per item
+  indexName?: string; // Choose a custom Algolia index
+  showResultsOnEmptyQuery?: boolean; // Show some results even if no query
 };
 
-type HitProps = {
-  hit: any;
-};
-
-function Hit({ hit }: HitProps) {
-  return (
-    <Link
-      href={createLink(hit.project_slug)}
-      style={{
-        width: "100%",
-      }}
-    >
-      <ListItem
-        style={{
-          width: "100%",
-          textAlign: "center",
-          padding: "1rem",
-        }}
-      >
-        <ListItemText>
-          <Highlight attribute="project_name" hit={hit} />
-        </ListItemText>
-      </ListItem>
-    </Link>
-  );
-}
-
-function HitsContainer() {
+function HitsContainer(props: AlgoliaSearchListProps) {
+  const { children } = props;
   const { hits, results } = useHits();
   //console.log(hits);
 
   return (
-    <List sx={{ width: "100%" }}>
+    <div>
       {results?.query &&
-        hits.map((hit) => <Hit key={hit.objectID} hit={hit} />)}
-    </List>
+        hits.map((hit) => (
+          <div key={hit.objectID}>
+            <DataProvider name={PLASMIC_KEY} data={hit}>
+              {children}
+            </DataProvider>
+          </div>
+        ))}
+    </div>
   );
 }
 
-export type AlgoliaSearchModalProps = {
-  className?: string; // Plasmic CSS class
-  children?: ReactElement; // Show this
-};
-
-function AlgoliaSearchModal(props: AlgoliaSearchModalProps) {
-  const { className, children: rawChildren } = props;
-  const children = rawChildren ?? <SearchIcon />;
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+/**
+ * Infinite scroll list for Algolia search
+ */
+function AlgoliaSearchList(props: AlgoliaSearchListProps) {
+  const {
+    className,
+    //children: rawChildren,
+    indexName: rawIndexName,
+    //showResultsOnEmptyQuery,
+  } = props;
+  //const children = rawChildren ?? <SearchIcon />;
+  const indexName = rawIndexName ?? ALGOLIA_INDEX;
 
   return (
-    <>
-      <div className={className} onClick={handleOpen}>
-        {children}
-      </div>
-      <Modal open={open} onClose={handleClose}>
-        <Paper
-          elevation={4}
-          sx={{
-            width: "50%",
-            height: "70%",
-            padding: "8px",
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            bgcolor: "background.paper",
-            overflow: "auto",
-          }}
-        >
-          <InstantSearchNext
-            searchClient={searchClient}
-            indexName={ALGOLIA_INDEX}
-            insights
-          >
-            <SearchBox placeholder={"Search projects..."} autoFocus={true} />
-            <HitsContainer />
-          </InstantSearchNext>
-        </Paper>
-      </Modal>
-    </>
+    <div className={className}>
+      <InstantSearchNext
+        searchClient={searchClient}
+        indexName={indexName}
+        insights
+      >
+        <SearchBox placeholder={"Search projects..."} autoFocus={true} />
+        <HitsContainer {...props} />
+      </InstantSearchNext>
+    </div>
   );
 }
 
-export { AlgoliaSearchModal };
+export { AlgoliaSearchList };
