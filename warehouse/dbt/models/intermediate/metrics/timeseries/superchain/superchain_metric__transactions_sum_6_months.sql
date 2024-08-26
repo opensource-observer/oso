bcf14f7c0b240fca9da4906649dd54cd084b9a04
ccerv1 <carl.cervone@gmail.com>
2024-08-26 10:15:48 -0400

    add: three more metric models

--- warehouse/dbt/models/intermediate/metrics/timeseries/superchain/superchain_metric__transactions_sum_6_months.sql
+++ warehouse/dbt/models/intermediate/metrics/timeseries/superchain/superchain_metric__transactions_sum_6_months.sql
@@ -4,59 +4,30 @@
   )
 #}
 
-{% set metric = {
-    "metric_name": "transactions_sum_6_months",
-    "metric_unit": "successful transactions",
-    "event_sources": [
-        "OPTIMISM", 
-        "BASE", 
-        "FRAX",
-        "METAL",
-        "MODE",
-        "ZORA"
-    ],
-    "event_types": ["CONTRACT_INVOCATION_SUCCESS_DAILY_COUNT"],
-    "to_artifact_types": ["CONTRACT"],
-    "from_artifact_types": ["EOA", "SAFE"],
-    "window_interval": "DAY",
-    "window_size": 180,
-    "window_missing_dates": "ignore",
-    "sampling_interval": "daily"
-} %}
-
-with events as (
-  {{ timeseries_events(
-      metric.event_sources,
-      metric.event_types,
-      metric.to_artifact_types,
-      metric.from_artifact_types,
-      time_interval=metric.window_interval,
-      missing_dates=metric.window_missing_dates
-  ) }}
-),
-
-agg_events as (
-  select
-    events.sample_date,
-    events.event_source,
-    events.project_id,
-    SUM(events.amount) as amount
-  from events
-  group by
-    events.sample_date,
-    events.event_source,
-    events.project_id
-),
-
-windowed_events as ({{
-  window_events('agg_events', 'SUM', metric.window_size, 'project')
-}})
-
-select
-  project_id,
-  sample_date,
-  event_source,
-  amount,
-  '{{ metric.metric_name }}' as metric,
-  '{{ metric.metric_unit }}' as unit
-from windowed_events
+{{ metric_from_events(
+    metric_name='transactions_sum_6_months',
+    metric_unit='successful transactions',
+    metric_sql_logic='
+      select
+        sample_date,
+        event_source,
+        to_group_id,
+        SUM(amount) as amount
+      from events
+      group by
+        sample_date,
+        event_source,
+        to_group_id
+    ',
+    metric_agg_func='SUM',
+    metric_time_interval_value=180,
+    metric_time_interval_unit='DAY',
+    event_model='int_events',
+    event_sources=['OPTIMISM', 'BASE', 'FRAX', 'METAL', 'MODE', 'ZORA'],
+    event_types=['CONTRACT_INVOCATION_SUCCESS_DAILY_COUNT'],
+    to_artifact_types=['CONTRACT'],
+    from_artifact_types=['EOA', 'SAFE'],
+    to_entity_type='project',
+    from_entity_type='artifact',
+    missing_dates='ignore'
+) }}
