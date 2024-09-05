c89f2449f50bb6a55e3a4c74868ecea6a63581cc
Reuven Gonzales <reuven@karibalabs.co>
2024-09-04 23:23:24 -0700

    Removes many timeseries code metrics (#2063)

--- warehouse/dbt/models/intermediate/metrics/int_metrics.sql
+++ warehouse/dbt/models/intermediate/metrics/int_metrics.sql
@@ -1,21 +1,9 @@
 {{
   config(
-    materialized='table'
+    materialized='table',
   )
 }}
 
-select distinct
-  {{ oso_id('"OSO"', '"oso"', 'metric') }} as metric_id,
-  "OSO" as metric_source,
-  "oso" as metric_namespace,
-  metrics.metric as metric_name,
-  metrics.metric as display_name,
-  "TODO" as description,
-  null as raw_definition,
-  "TODO" as definition_ref,
-  "UNKNOWN" as aggregation_function
-from {{ ref('int_timeseries_code_metrics_by_artifact__developers') }} as metrics
-union all
 select distinct
   {{ oso_id('"OSO"', '"oso"', 'metric') }} as metric_id,
   "OSO" as metric_source,
