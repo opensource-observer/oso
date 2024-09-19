ab61c549b9982ad728a7e120828d3534cd19c803
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-09-19 16:52:52 +0000

    Update batch size and storage format to different tree

--- warehouse/metrics_mesh/models/events_daily_to_artifact.sql
+++ warehouse/metrics_mesh/models/events_daily_to_artifact.sql
@@ -2,7 +2,7 @@ MODEL (
   name metrics.events_daily_to_artifact,
   kind INCREMENTAL_BY_TIME_RANGE (
     time_column bucket_day,
-    batch_size 30
+    batch_size 90
   ),
   start '2015-01-01',
   cron '@daily',
@@ -21,6 +21,8 @@ MODEL (
     event_type String,
     from_artifact_id String,
     to_artifact_id String,
+    _sign Int8,
+    _version UInt32,
     amount Float64
   ),
   physical_properties (
@@ -30,8 +32,9 @@ MODEL (
       from_artifact_id,
       to_artifact_id,
       bucket_day
-    )
-  )
+    ),
+  ),
+  storage_format "VersionedCollapsingMergeTree(_sign, _version)",
 );
 WITH events AS (
   SELECT DISTINCT from_artifact_id,
@@ -48,6 +51,8 @@ SELECT from_artifact_id,
   event_source,
   event_type,
   DATE_TRUNC('DAY', time::DATE) AS bucket_day,
+  1 as _sign,
+  @str_to_unix_timestamp(@execution_ds) as _version,
   SUM(amount) AS amount
 FROM events
 GROUP BY from_artifact_id,
