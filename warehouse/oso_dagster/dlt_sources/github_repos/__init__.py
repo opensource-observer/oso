2a507af76efec5540815fd2c9aa7442ed9667741
Javier R <javier.rios-urbano@epitech.eu>
2024-10-11 17:03:19 +0200

    Revert "add: `created_at` and `updated_at` properties to repository (#2333)"
    This reverts commit 28d29b37a4f58f1694d092b1ab87d82829423cd3.

--- warehouse/oso_dagster/dlt_sources/github_repos/__init__.py
+++ warehouse/oso_dagster/dlt_sources/github_repos/__init__.py
@@ -60,8 +60,6 @@ class Repository(BaseModel):
     license_spdx_id: str
     license_name: str
     language: str
-    created_at: Optional[datetime]
-    updated_at: Optional[datetime]
 
 
 class InvalidGithubURL(Exception):
@@ -131,8 +129,6 @@ def gh_repository_to_repository(
         url=repo.html_url,
         is_fork=repo.fork,
         language=repo.language or "",
-        created_at=repo.created_at or None,
-        updated_at=repo.updated_at or None,
     )
 
 
