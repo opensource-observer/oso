0dad4447882f2b1e43d2b325defc6d6cb3a69b6d
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-11-03 23:51:32 +0000

    some model fixes

--- warehouse/metrics_mesh/models/metrics_factories.py
+++ warehouse/metrics_mesh/models/metrics_factories.py
@@ -21,26 +21,13 @@ timeseries_metrics(
             ref="stars.sql",
             time_aggregations=["daily", "weekly", "monthly"],
         ),
-        # # This defines something with a rolling option that allows you to look back
-        # # to some arbitrary window. So you specify the window and specify the unit.
-        # # The unit and the window are used to pass in variables to the query. So it's
-        # # up to the query to actually query the correct window.
-        # # The resultant models are named as such
-        # # `metrics.timeseries_active_days_to_{entity_type}_over_{window}_{unit}`
-        # "developer_active_days": MetricQueryDef(
-        #     ref="active_days.sql",
-        #     vars={
-        #         "activity_event_types": ["COMMIT_CODE"],
-        #     },
-        #     rolling=RollingConfig(
-        #         windows=[30, 60, 90],
-        #         unit="day",
-        #         cron="@daily",  # This determines how often this is calculated
-        #     ),
-        #     entity_types=["artifact", "project", "collection"],
-        #     is_intermediate=True,
-        # ),
-        "developer_active_days_as_py": MetricQueryDef(
+        # This defines something with a rolling option that allows you to look back
+        # to some arbitrary window. So you specify the window and specify the unit.
+        # The unit and the window are used to pass in variables to the query. So it's
+        # up to the query to actually query the correct window.
+        # The resultant models are named as such
+        # `metrics.timeseries_active_days_to_{entity_type}_over_{window}_{unit}`
+        "developer_active_days": MetricQueryDef(
             ref="active_days.sql",
             vars={
                 "activity_event_types": ["COMMIT_CODE"],
@@ -50,67 +37,67 @@ timeseries_metrics(
                 unit="day",
                 cron="@daily",  # This determines how often this is calculated
             ),
-            entity_types=["artifact"],
-            use_python_model=True,
+            entity_types=["artifact", "project", "collection"],
+            is_intermediate=True,
         ),
-        # "developer_classifications": MetricQueryDef(
-        #     ref="developer_activity_classification.sql",
-        #     vars={
-        #         "full_time_ratio": 10 / 30,
-        #     },
-        #     rolling=RollingConfig(
-        #         windows=[30, 60, 90],
-        #         unit="day",
-        #         cron="@daily",
-        #     ),
-        # ),
-        # "contributor_classifications": MetricQueryDef(
-        #     ref="contributor_activity_classification.sql",
-        #     vars={"full_time_ratio": 10 / 30},
-        #     rolling=RollingConfig(
-        #         windows=[30, 60, 90],
-        #         unit="day",
-        #         cron="@daily",
-        #     ),
-        # ),
-        # "active_addresses": MetricQueryDef(
-        #     ref="active_addresses.sql",
-        #     time_aggregations=["daily", "weekly", "monthly"],
-        # ),
-        # "commits": MetricQueryDef(
-        #     ref="commits.sql",
-        #     time_aggregations=["daily", "weekly", "monthly"],
-        # ),
-        # "forks": MetricQueryDef(
-        #     ref="forks.sql",
-        #     time_aggregations=["daily", "weekly", "monthly"],
-        # ),
-        # "gas_fees": MetricQueryDef(
-        #     ref="gas_fees.sql",
-        #     time_aggregations=["daily", "weekly", "monthly"],
-        # ),
-        # "change_in_30_developer_activity": MetricQueryDef(
-        #     vars={
-        #         "comparison_interval": 30,
-        #     },
-        #     ref="change_in_developers.sql",
-        #     rolling=RollingConfig(
-        #         windows=[2],
-        #         unit="period",
-        #         cron="@daily",
-        #     ),
-        # ),
-        # "change_in_60_developer_activity": MetricQueryDef(
-        #     vars={
-        #         "comparison_interval": 60,
-        #     },
-        #     ref="change_in_developers.sql",
-        #     rolling=RollingConfig(
-        #         windows=[2],
-        #         unit="period",
-        #         cron="@daily",
-        #     ),
-        # ),
+        "developer_classifications": MetricQueryDef(
+            ref="developer_activity_classification.sql",
+            vars={
+                "full_time_ratio": 10 / 30,
+            },
+            rolling=RollingConfig(
+                windows=[30, 60, 90],
+                unit="day",
+                cron="@daily",
+            ),
+        ),
+        "contributor_classifications": MetricQueryDef(
+            ref="contributor_activity_classification.sql",
+            vars={"full_time_ratio": 10 / 30},
+            rolling=RollingConfig(
+                windows=[30, 60, 90],
+                unit="day",
+                cron="@daily",
+            ),
+        ),
+        "active_addresses": MetricQueryDef(
+            ref="active_addresses.sql",
+            time_aggregations=["daily", "weekly", "monthly"],
+        ),
+        "commits": MetricQueryDef(
+            ref="commits.sql",
+            time_aggregations=["daily", "weekly", "monthly"],
+        ),
+        "forks": MetricQueryDef(
+            ref="forks.sql",
+            time_aggregations=["daily", "weekly", "monthly"],
+        ),
+        "gas_fees": MetricQueryDef(
+            ref="gas_fees.sql",
+            time_aggregations=["daily", "weekly", "monthly"],
+        ),
+        "change_in_30_developer_activity": MetricQueryDef(
+            vars={
+                "comparison_interval": 30,
+            },
+            ref="change_in_developers.sql",
+            rolling=RollingConfig(
+                windows=[2],
+                unit="period",
+                cron="@daily",
+            ),
+        ),
+        "change_in_60_developer_activity": MetricQueryDef(
+            vars={
+                "comparison_interval": 60,
+            },
+            ref="change_in_developers.sql",
+            rolling=RollingConfig(
+                windows=[2],
+                unit="period",
+                cron="@daily",
+            ),
+        ),
     },
     default_dialect="clickhouse",
 )
