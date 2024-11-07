ab7d5c8faf2b12511dcf1a9185ddf5b1a279f25b
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-11-07 11:02:29 +0000

    test

--- warehouse/metrics_tools/compute/flight.py
+++ warehouse/metrics_tools/compute/flight.py
@@ -297,7 +297,7 @@ def main(
 ):
     # Start the cluster
     cluster_spec = make_new_cluster(
-        "ghcr.io/opensource-observer/dagster-dask:test-1",
+        "ghcr.io/opensource-observer/dagster-dask:test-2",
         "sqlmesh-flight",
         "sqlmesh-manual",
     )
