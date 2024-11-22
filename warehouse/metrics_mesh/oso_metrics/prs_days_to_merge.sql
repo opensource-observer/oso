c71f91b5ee91cbdca0a3cabd6e39b14ae1ae550e
Carl Cervone <carl.cervone@gmail.com>
2024-11-22 14:22:11 -0500

    fix: number to issue_number

--- warehouse/metrics_mesh/oso_metrics/prs_days_to_merge.sql
+++ warehouse/metrics_mesh/oso_metrics/prs_days_to_merge.sql
@@ -1,5 +1,5 @@
 select 
-  @metrics_sample_date(merged_events.time) as metrics_sample_date, -- we use PR merge event tiemstamp (note: we haven't bucketed by day in the events table)
+  @metrics_sample_date(merged_events.time) as metrics_sample_date, -- we use PR merge event timestamp (note: we haven't bucketed by day in the events table)
   merged_events.event_source,
   merged_events.to_artifact_id,
   '' as from_artifact_id,
@@ -7,7 +7,7 @@ select
   AVG(TIMESTAMP_DIFF(merged_events.time, pr_events.time, minute) / 60.0 / 24.0) as amount
 from (
   select
-    number,
+    issue_number,
     to_artifact_id,
     time,
     event_source
@@ -16,14 +16,14 @@ from (
 ) as pr_events
 inner join (
   select
-    number,
+    issue_number,
     to_artifact_id,
     time,
     event_source
   from metrics.timeseries_events_aux_issues_by_artifact_v0
   where event_type = 'PULL_REQUEST_MERGED'
 ) as merged_events
-on pr_events.number = merged_events.number
+on pr_events.issue_number = merged_events.issue_number
 and pr_events.to_artifact_id = merged_events.to_artifact_id
 where merged_events.time BETWEEN @metrics_start('DATE') AND @metrics_end('DATE')
 group by 
