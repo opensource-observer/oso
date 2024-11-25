c2584d250c8a25100f838106861955faa320e419
ccerv1 <carl.cervone@gmail.com>
2024-11-25 18:33:39 -0500

    fix: active_addresses follow developer metric naming conventions

--- warehouse/metrics_mesh/oso_metrics/active_addresses.sql
+++ warehouse/metrics_mesh/oso_metrics/active_addresses.sql
@@ -1,30 +1,52 @@
-select @metrics_sample_date(events.bucket_day) as metrics_sample_date,
-  events.event_source,
-  events.to_artifact_id,
+select daa.metrics_sample_date,
+  daa.event_source,
+  @metrics_entity_type_col(
+    'to_{entity_type}_id',
+    table_alias := daa,
+    include_column_alias := true,
+  ),
   '' as from_artifact_id,
   @metric_name('daily_active_addresses') as metric,
-  COUNT(distinct events.from_artifact_id) as amount
-from metrics.events_daily_to_artifact as events
-where event_type in ('CONTRACT_INVOCATION_SUCCESS_DAILY_COUNT')
-  and events.bucket_day = @metrics_sample_date('DATE')
-group by 1,
-  metric,
+  COUNT(distinct daa.from_artifact_id) as amount
+from @metrics_peer_ref(
+    events_daily_to_artifact,
+    window := @rolling_window,
+    unit := @rolling_unit
+  ) as daa
+where daa.event_type in ('CONTRACT_INVOCATION_SUCCESS_DAILY_COUNT')
+  and daa.metrics_sample_date = @metrics_end('DATE')
+group by metric,
   from_artifact_id,
-  to_artifact_id,
-  event_source
+  @metrics_entity_type_col(
+    'to_{entity_type}_id',
+    table_alias := daa,
+  ),
+  event_source,
+  metrics_sample_date
 union all
-select @metrics_sample_date(events.bucket_day) as metrics_sample_date,
-  events.event_source,
-  events.to_artifact_id,
+select maa.metrics_sample_date,
+  maa.event_source,
+  @metrics_entity_type_col(
+    'to_{entity_type}_id',
+    table_alias := maa,
+    include_column_alias := true,
+  ),
   '' as from_artifact_id,
   @metric_name('monthly_active_addresses') as metric,
-  COUNT(distinct events.from_artifact_id) as amount
-from metrics.events_daily_to_artifact as events
-where event_type in ('CONTRACT_INVOCATION_SUCCESS_DAILY_COUNT')
-  and events.bucket_day BETWEEN DATEADD(day, -29, @metrics_sample_date('DATE')) 
-    AND @metrics_sample_date('DATE') 
-group by 1,
-  metric,
+  COUNT(distinct maa.from_artifact_id) as amount
+from @metrics_peer_ref(
+    events_daily_to_artifact,
+    window := @rolling_window,
+    unit := @rolling_unit
+  ) as maa
+where maa.event_type in ('CONTRACT_INVOCATION_SUCCESS_DAILY_COUNT')
+  and maa.metrics_sample_date between DATEADD(day, -29, @metrics_end('DATE')) 
+    AND @metrics_end('DATE')
+group by metric,
   from_artifact_id,
-  to_artifact_id,
-  event_source
+  @metrics_entity_type_col(
+    'to_{entity_type}_id',
+    table_alias := maa,
+  ),
+  event_source,
+  metrics_sample_date
