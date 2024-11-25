79771f179a5c80091779dfdf1c447dc2c688a783
Reuven Gonzales <reuven@karibalabs.co>
2024-11-25 19:19:22 +0900

    Add additional aux data for github issues (#2516)
    * Add additional aux data for github issues
    
    * Update stg_github__comments.sql
    
    Fix copy paste issues
    
    * Update stg_github__issues.sql
    
    * fix dbt error
    
    * Parse dates
    
    * Add the comments column
    
    * fix
    
    * fix

--- warehouse/dbt/models/staging/github/stg_github__issues.sql
+++ warehouse/dbt/models/staging/github/stg_github__issues.sql
@@ -10,11 +10,27 @@ with issue_events as (
 
 select
   ie.id as id,
-  ie.created_at as created_at,
+  ie.created_at as event_time,
   ie.repo.id as repository_id,
   ie.repo.name as repository_name,
   ie.actor.id as actor_id,
   ie.actor.login as actor_login,
   CONCAT("ISSUE_", UPPER(JSON_VALUE(ie.payload, "$.action"))) as `type`,
-  JSON_VALUE(ie.payload, "$.issue.number") as `number`
+  JSON_VALUE(ie.payload, "$.issue.number") as `number`,
+  PARSE_TIMESTAMP(
+    "%Y-%m-%dT%H:%M:%E*SZ",
+    JSON_VALUE(ie.payload, "$.issue.created_at")
+  ) as created_at,
+  PARSE_TIMESTAMP(
+    "%Y-%m-%dT%H:%M:%E*SZ",
+    JSON_VALUE(ie.payload, "$.issue.closed_at")
+  ) as closed_at,
+  JSON_VALUE(
+    ie.payload,
+    "$.issue.state"
+  ) as `state`,
+  JSON_VALUE(
+    ie.payload,
+    "$.issue.comments"
+  ) as comments
 from issue_events as ie
