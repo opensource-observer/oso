5de9509c8d952ff202cb4c69964ab24f22397800
Carl Cervone <carl.cervone@gmail.com>
2024-11-25 06:19:13 -0500

    feat(sqlmesh): create daily and monthly active address models

--- warehouse/metrics_mesh/oso_metrics/active_addresses.sql
+++ warehouse/metrics_mesh/oso_metrics/active_addresses.sql
@@ -2,11 +2,27 @@ select @metrics_sample_date(events.bucket_day) as metrics_sample_date,
   events.event_source,
   events.to_artifact_id,
   '' as from_artifact_id,
-  @metric_name() as metric,
+  @metric_name('daily_active_addresses') as metric,
   COUNT(distinct events.from_artifact_id) as amount
 from metrics.events_daily_to_artifact as events
 where event_type in ('CONTRACT_INVOCATION_SUCCESS_DAILY_COUNT')
-  and events.bucket_day BETWEEN @metrics_start('DATE') AND @metrics_end('DATE')
+  and events.bucket_day = @metrics_sample_date('DATE')
+group by 1,
+  metric,
+  from_artifact_id,
+  to_artifact_id,
+  event_source
+union all
+select @metrics_sample_date(events.bucket_day) as metrics_sample_date,
+  events.event_source,
+  events.to_artifact_id,
+  '' as from_artifact_id,
+  @metric_name('monthly_active_addresses') as metric,
+  COUNT(distinct events.from_artifact_id) as amount
+from metrics.events_daily_to_artifact as events
+where event_type in ('CONTRACT_INVOCATION_SUCCESS_DAILY_COUNT')
+  and events.bucket_day BETWEEN DATEADD(day, -29, @metrics_sample_date('DATE')) 
+    AND @metrics_sample_date('DATE') 
 group by 1,
   metric,
   from_artifact_id,
