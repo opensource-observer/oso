bdfa64b53a7d8072b641547ab594362219059cd1
Javier <javi@karibalabs.co>
2024-11-26 18:40:24 +0000

    fix: update `npm` manifests staging columns

--- warehouse/dbt/models/staging/npm/stg_npm__manifests.sql
+++ warehouse/dbt/models/staging/npm/stg_npm__manifests.sql
@@ -1,13 +1,12 @@
 {% set columns = [
-    "name", "version", "description", "keywords", "homepage", "bugs", 
-    "license", "author", "contributors", "funding", "files", "exports", 
-    "main", "browser", "bin", "man", "directories", "repository", 
-    "scripts", "config", "dependencies", "dev_dependencies", 
-    "peer_dependencies", "peer_dependencies_meta", "bundle_dependencies", 
-    "optional_dependencies", "overrides", "engines", "os", "cpu", 
-    "dev_engines", "private", "publish_config", "workspaces", "bugs__url", 
-    "repository__url", "repository__type",  "author__url", "author__name",
-    "author__email"
+  "name", "version", "description", "keywords", "homepage", "bugs", 
+  "license", "author", "contributors", "funding", "files", "exports", 
+  "main", "browser", "bin", "man", "directories", "repository", 
+  "scripts", "config", "dependencies", "dev_dependencies", 
+  "peer_dependencies", "peer_dependencies_meta", "bundle_dependencies", 
+  "optional_dependencies", "overrides", "engines", "os", "cpu", 
+  "dev_engines", "private", "publish_config", "workspaces", 
+  "_dlt_load_id", "_dlt_id"
 ] %}
 
 with source as (
