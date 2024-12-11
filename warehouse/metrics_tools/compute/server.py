fc454d544a8b3126f20972b0a25ce7cc9f223a6f
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-12-11 19:12:09 +0000

    server clean up for mcs

--- warehouse/metrics_tools/compute/server.py
+++ warehouse/metrics_tools/compute/server.py
@@ -95,17 +95,17 @@ async def initialize_app(app: FastAPI):
     )
     try:
         yield {
-            "mca": mcs,
+            "mcs": mcs,
         }
     finally:
         await mcs.close()
 
 
 # Dependency to get the cluster manager
-def get_mca(request: Request) -> MetricsCalculationService:
-    mca = request.state.mca
-    assert mca is not None
-    return t.cast(MetricsCalculationService, mca)
+def get_mcs(request: Request) -> MetricsCalculationService:
+    mcs = request.state.mcs
+    assert mcs is not None
+    return t.cast(MetricsCalculationService, mcs)
 
 
 app = FastAPI(lifespan=initialize_app)
@@ -124,30 +124,27 @@ async def start_cluster(
     request: Request,
     start_request: ClusterStartRequest,
 ):
+    """Start a Dask cluster in an idempotent way.
+
+    If the cluster is already running, it will not be restarted.
     """
-    Start a Dask cluster in an idempotent way
-    """
-    state = get_mca(request)
+    state = get_mcs(request)
     manager = state.cluster_manager
     return await manager.start_cluster(start_request.min_size, start_request.max_size)
 
 
 @app.post("/cluster/stop")
 async def stop_cluster(request: Request):
-    """
-    Stop the Dask cluster
-    """
-    state = get_mca(request)
+    """Stop the Dask cluster"""
+    state = get_mcs(request)
     manager = state.cluster_manager
     return await manager.stop_cluster()
 
 
 @app.get("/cluster/status")
 async def get_cluster_status(request: Request):
-    """
-    Get the current Dask cluster status
-    """
-    state = get_mca(request)
+    """Get the current Dask cluster status"""
+    state = get_mcs(request)
     manager = state.cluster_manager
     return await manager.get_cluster_status()
 
@@ -157,10 +154,8 @@ async def submit_job(
     request: Request,
     input: QueryJobSubmitRequest,
 ):
-    """
-    Submits a Dask job for calculation
-    """
-    service = get_mca(request)
+    """Submits a Dask job for calculation"""
+    service = get_mcs(request)
     return await service.submit_job(input)
 
 
@@ -169,11 +164,9 @@ async def get_job_status(
     request: Request,
     job_id: str,
 ):
-    """
-    Get the status of a job
-    """
+    """Get the status of a job"""
     include_stats = request.query_params.get("include_stats", "false").lower() == "true"
-    service = get_mca(request)
+    service = get_mcs(request)
     return await service.get_job_status(job_id, include_stats=include_stats)
 
 
@@ -181,9 +174,7 @@ async def get_job_status(
 async def add_existing_exported_table_references(
     request: Request, input: ExportedTableLoadRequest
 ):
-    """
-    Add a table export to the cache
-    """
-    service = get_mca(request)
+    """Add a table export to the cache"""
+    service = get_mcs(request)
     await service.add_existing_exported_table_references(input.map)
     return EmptyResponse()
