fb26cbb7518862cc4bab3d1c7e3b36b7bf1b4b03
Carl Cervone <carl.cervone@gmail.com>
2024-12-12 08:52:59 -0500

    feat(dbt): parse package lineage and determine if historic owner is current owner

--- warehouse/dbt/models/intermediate/directory/int_packages.sql
+++ warehouse/dbt/models/intermediate/directory/int_packages.sql
@@ -1,4 +1,4 @@
-with deps_dev_packages as (
+with deps_dev as (
   select
     `Version` as package_version,
     upper(`System`) as package_artifact_source,
@@ -6,21 +6,42 @@ with deps_dev_packages as (
     lower(split(`ProjectName`, '/')[0]) as package_github_owner,
     lower(split(`ProjectName`, '/')[1]) as package_github_repo
   from {{ ref('stg_deps_dev__packages') }}
+),
+
+latest_versions as (
+  select
+    package_artifact_source,
+    package_artifact_name,
+    package_github_owner as current_owner,
+    package_github_repo as current_repo
+  from deps_dev
+  qualify row_number() over (
+    partition by package_artifact_name, package_artifact_source
+    order by package_version desc
+  ) = 1
 )
 
 select
-  package_artifact_source,
-  package_artifact_name,
-  package_version,
-  package_github_owner,
-  package_github_repo,
+  deps_dev.package_artifact_source,
+  deps_dev.package_artifact_name,
+  deps_dev.package_version,
+  deps_dev.package_github_owner,
+  deps_dev.package_github_repo,
   case
-    when package_artifact_source = 'CARGO' then 'RUST'
-    when package_artifact_source = 'NPM' then 'NPM'
-    when package_artifact_source = 'PYPI' then 'PIP'
-    when package_artifact_source = 'GO' then 'GO'
-    when package_artifact_source = 'MAVEN' then 'MAVEN'
-    when package_artifact_source = 'NUGET' then 'NUGET'
+    when deps_dev.package_artifact_source = 'CARGO' then 'RUST'
+    when deps_dev.package_artifact_source = 'NPM' then 'NPM'
+    when deps_dev.package_artifact_source = 'PYPI' then 'PIP'
+    when deps_dev.package_artifact_source = 'GO' then 'GO'
+    when deps_dev.package_artifact_source = 'MAVEN' then 'MAVEN'
+    when deps_dev.package_artifact_source = 'NUGET' then 'NUGET'
     else 'UNKNOWN'
-  end as sbom_artifact_source
-from deps_dev_packages
+  end as sbom_artifact_source,
+  (
+    deps_dev.package_github_owner = latest_versions.current_owner
+    and deps_dev.package_github_repo = latest_versions.current_repo)
+    as is_current_owner
+from deps_dev
+left join latest_versions
+  on
+    deps_dev.package_artifact_source = latest_versions.package_artifact_source
+    and deps_dev.package_artifact_name = latest_versions.package_artifact_name
