f7c15d8331890a2815c2dddcad83ebb79a37ec52
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2024-12-12 17:43:43 -0500

    feat(dbt): mart registry for dependency graph (#2623)
    * fix(dbt): point to better source for latest package owner in deps.dev
    
    * feat(dbt): staging model for latest package owner
    
    * feat(dbt): int model to see if we have a package's owner repo already
    
    * fix(dbt): add model to resolve deps.dev and sbom source names
    
    * feat(dbt): parse package lineage and determine if historic owner is current owner
    
    * fix(dbt): refactor to use int_packages
    
    * fix(dbt): use int_packages to join on sbom package sources
    
    * fix(dbt): lookup deps.dev package owner artifact_id if it exists
    
    * fix(dbt): rename field
    
    * feat(dbt): add sbom mart model
    
    * feat(dbt): add project id to int model
    
    * refactor(dbt): break sboms_v0 into two models

--- warehouse/dbt/models/intermediate/directory/int_sbom_artifacts.sql
+++ warehouse/dbt/models/intermediate/directory/int_sbom_artifacts.sql
@@ -31,12 +31,23 @@ sbom_artifacts as (
     snapshot_at
   from ranked_snapshots
   where row_num = 1
+),
+
+deps_dev_packages as (
+  select distinct
+    sbom_artifact_source,
+    package_artifact_name,
+    package_github_owner,
+    package_github_repo
+  from {{ ref('int_packages') }}
+  where is_current_owner = true
 )
 
 select
   {#
     Because we use repo.id as the artifact_source_id for github, we need to lookup the artifact_id for the SBOM repo. If the artifact is not found, this will return null.
   #}
+  all_repos.project_id,
   all_repos.artifact_id,
   sbom_artifacts.artifact_source,
   sbom_artifacts.artifact_namespace,
@@ -44,10 +55,17 @@ select
   {#
     Because we only index packages that are found in OSSD, most of the time this will return a null package_artifact_id.
   #}
+  all_packages.project_id as package_project_id,
   all_packages.artifact_id as package_artifact_id,
   sbom_artifacts.package_source as package_artifact_source,
+  '' as package_artifact_namespace,
   sbom_artifacts.package as package_artifact_name,
+  'GITHUB' as package_owner_source,
   sbom_artifacts.package_version as package_version,
+  deps_dev_packages.package_github_owner,
+  deps_dev_packages.package_github_repo,
+  deps_dev_repos.artifact_id as package_github_artifact_id,
+  deps_dev_repos.project_id as package_github_project_id,
   sbom_artifacts.snapshot_at
 from sbom_artifacts
 left outer join {{ ref('int_all_artifacts') }} as all_repos
@@ -58,3 +76,11 @@ left outer join {{ ref('int_all_artifacts') }} as all_packages
   on
     sbom_artifacts.package = all_packages.artifact_name
     and sbom_artifacts.package_source = all_packages.artifact_source
+left outer join deps_dev_packages
+  on
+    sbom_artifacts.package_source = deps_dev_packages.sbom_artifact_source
+    and sbom_artifacts.package = deps_dev_packages.package_artifact_name
+left outer join {{ ref('int_all_artifacts') }} as deps_dev_repos
+  on
+    deps_dev_packages.package_github_owner = deps_dev_repos.artifact_namespace
+    and deps_dev_packages.package_github_repo = deps_dev_repos.artifact_name
