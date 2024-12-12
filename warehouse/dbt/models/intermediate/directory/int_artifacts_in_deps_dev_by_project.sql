6caae64d91130d66b00a846e8dd1b148eeddc437
Carl Cervone <carl.cervone@gmail.com>
2024-12-12 09:05:21 -0500

    fix(dbt): refactor to use int_packages

--- warehouse/dbt/models/intermediate/directory/int_artifacts_in_deps_dev_by_project.sql
+++ warehouse/dbt/models/intermediate/directory/int_artifacts_in_deps_dev_by_project.sql
@@ -1,23 +1,11 @@
 with latest_package as (
-  select
-    lower(`Name`) as package_artifact_name,
-    lower(split(`ProjectName`, '/')[0]) as package_owner,
-    lower(split(`ProjectName`, '/')[1]) as package_repo,
-    upper(`System`) as package_artifact_source
-  from (
-    select
-      lower(`Name`) as `Name`,
-      lower(`ProjectName`) as `ProjectName`,
-      upper(`System`) as `System`,
-      row_number() over (
-        partition by
-          lower(`Name`),
-          upper(`System`)
-        order by `Version` desc
-      ) as row_num
-    from {{ ref('stg_deps_dev__packages') }}
-  )
-  where row_num = 1
+  select distinct
+    package_artifact_source,
+    package_artifact_name,
+    package_github_owner,
+    package_github_repo
+  from {{ ref('int_packages') }}
+  where is_current_owner = true
 )
 
 select
@@ -25,16 +13,16 @@ select
   github_repos.artifact_id,
   github_repos.artifact_source_id,
   github_repos.artifact_source,
-  latest_package.package_owner as artifact_namespace,
-  latest_package.package_repo as artifact_name,
+  latest_package.package_github_owner as artifact_namespace,
+  latest_package.package_github_repo as artifact_name,
   all_packages.artifact_id as package_artifact_id,
   latest_package.package_artifact_source as package_artifact_source,
   latest_package.package_artifact_name as package_artifact_name
 from latest_package
 left outer join {{ ref('int_all_artifacts') }} as github_repos
   on
-    latest_package.package_owner = github_repos.artifact_namespace
-    and latest_package.package_repo = github_repos.artifact_name
+    latest_package.package_github_owner = github_repos.artifact_namespace
+    and latest_package.package_github_repo = github_repos.artifact_name
     and github_repos.artifact_source = 'GITHUB'
 left outer join {{ ref('int_all_artifacts') }} as all_packages
   on
