0de53fd11119ca9d8874b9ad6f9269f3512c040a
Javier <javi@karibalabs.co>
2024-12-21 21:39:15 +0000

    add: `growthepie` fundamentals tvl source

---
+++ warehouse/oso_dagster/assets/growthepie.py
@@ -0,0 +1,26 @@
+from dlt.sources.rest_api.typing import RESTAPIConfig
+
+from ..factories.rest import create_rest_factory_asset
+
+config: RESTAPIConfig = {
+    "client": {
+        "base_url": "https://api.growthepie.xyz/v1",
+    },
+    "resources": [
+        {
+            "name": "fundamentals_full",
+            "endpoint": {
+                "path": "fundamentals_full.json",
+                "data_selector": "$",
+            },
+        }
+    ],
+}
+
+dlt_assets = create_rest_factory_asset(
+    config=config,
+)
+
+growthepie_assets = dlt_assets(
+    key_prefix="growthepie",
+)
