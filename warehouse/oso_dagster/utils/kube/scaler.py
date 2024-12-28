583d0a607f4e5378f37ad77efbd3b5a530cead49
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2024-12-28 05:32:37 +0000

    Fix dagster-sqlmesh resource and deploy

--- warehouse/oso_dagster/utils/kube/scaler.py
+++ warehouse/oso_dagster/utils/kube/scaler.py
@@ -1,5 +1,7 @@
 # We have some resources that should only be scaled up if they are in use by a
 # dagster job.
+from inspect import isawaitable
+
 from kr8s.objects import Deployment
 
 
@@ -7,11 +9,15 @@ async def ensure_scale_up(*, name: str, namespace: str, scale: int) -> None:
     # Check if the deployment is in use by a dagster job
     deployment = await Deployment.get(name=name, namespace=namespace)
     if deployment.replicas != scale:
-        await deployment.scale(scale)
+        result = deployment.scale(scale)
+        if isawaitable(result):
+            await result
 
 
 async def ensure_scale_down(*, name: str, namespace: str) -> None:
     # Check if the deployment is in use by a dagster job
     deployment = await Deployment.get(name=name, namespace=namespace)
     if deployment.replicas != 0:
-        await deployment.scale(0)
+        result = deployment.scale(0)
+        if isawaitable(result):
+            await result
