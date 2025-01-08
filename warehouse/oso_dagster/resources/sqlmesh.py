12a25ca6f60fc34b1f4f9957296c924845f67f42
Reuven Gonzales <reuven@karibalabs.co>
2025-01-07 22:08:15 -0800

    Fix problems with trino + clickhouse (#2711)

--- warehouse/oso_dagster/resources/sqlmesh.py
+++ warehouse/oso_dagster/resources/sqlmesh.py
@@ -85,7 +85,16 @@ class Trino2ClickhouseSQLMeshExporter(SQLMeshExporter):
             clickhouse: ClickhouseResource,
         ) -> None:
             table_name = key.path[-1]
-            async with trino.get_client() as connection:
+            async with trino.get_client(
+                session_properties={
+                    # Clickhouse doesn't support retries on trino so we _must_
+                    # disable it for insert queries to work
+                    "retry_policy": "None",
+                    # For whatever reason this is also necessary, writes didn't
+                    # work in manual testing without this set.
+                    "clickhouse.non_transactional_insert": "true",
+                }
+            ) as connection:
                 # Export the data from trino to clickhouse by creating a table
                 # that has a suffix
                 exported_table_name = f"{clickhouse_key.path[-1]}_{datetime.now().strftime('%Y%m%d%H%M%S')}"
@@ -96,8 +105,16 @@ class Trino2ClickhouseSQLMeshExporter(SQLMeshExporter):
                     self.trino_source_table(table_name),
                     self.trino_destination_table(exported_table_name),
                 )
-                context.log.info(f"executing sql: {create_query}")
-                cursor.execute(create_query)
+                context.log.info(f"executing sql: {create_query.sql(dialect='trino')}")
+                cursor.execute(create_query.sql(dialect="trino"))
+                # Insert the data into the new table
+                cursor.execute(
+                    self.generate_insert_query(
+                        model,
+                        self.trino_source_table(table_name),
+                        self.trino_destination_table(exported_table_name),
+                    ).sql(dialect="trino")
+                )
 
             with clickhouse.get_client() as clickhouse_client:
                 # Create the clickhouse table
@@ -117,16 +134,13 @@ class Trino2ClickhouseSQLMeshExporter(SQLMeshExporter):
         base_create_query = f"""
             CREATE table {destination_table} (
                 placeholder VARCHAR,
-            ) WITH (
-                engine = "MergeTree",
             )
-            AS SELECT * FROM {source_table}
         """
 
         columns = model.columns_to_types
         assert columns is not None, "columns must not be None"
 
-        create_query = parse_one(base_create_query)
+        create_query = parse_one(base_create_query, dialect="trino")
         create_query.this.set(
             "expressions",
             [
@@ -134,4 +148,43 @@ class Trino2ClickhouseSQLMeshExporter(SQLMeshExporter):
                 for column_name, column_type in columns.items()
             ],
         )
-        return create_query.sql(dialect="trino")
+
+        properties = [
+            # DO NOT USE exp.EngineProperty. This doesn't currently work
+            # properly for sqlglot in this context. Also don't parse a create
+            # query that has the WITH (engine = 'something') portion in it. It
+            # will parse as an exp.EngineProperty and cause problems when
+            # rerendering to sql
+            exp.Property(
+                this=exp.Var(this="engine"),
+                value=exp.Literal(this="MergeTree", is_string=True),
+            ),
+        ]
+        if model.time_column:
+            properties.append(
+                exp.Property(
+                    this=exp.Var(this="order_by"),
+                    value=exp.Literal(
+                        this=model.time_column.column.sql(dialect="trino"),
+                        is_string=True,
+                    ),
+                )
+            )
+        create_query.set(
+            "properties",
+            exp.Properties(
+                expressions=properties,
+            ),
+        )
+        return create_query
+
+    def generate_insert_query(
+        self, model: Model, source_table: str, destination_table: str
+    ) -> exp.Expression:
+        assert model.columns_to_types is not None, "columns must not be None"
+        select = exp.select(
+            *[column_name for column_name in model.columns_to_types.keys()]
+        )
+        insert = exp.Insert(this=exp.to_table(destination_table), expression=select)
+
+        return insert
