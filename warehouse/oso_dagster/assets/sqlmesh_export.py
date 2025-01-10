7069beafc812061ac9e6d57080ae62a4c4c83f05
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2025-01-10 17:05:20 +0000

    Use multiasset for clickhouse copy

--- warehouse/oso_dagster/assets/sqlmesh_export.py
+++ warehouse/oso_dagster/assets/sqlmesh_export.py
@@ -1,12 +1,13 @@
 import logging
 import typing as t
 
-from dagster import ResourceParam
+from dagster import AssetKey, ResourceParam
 from dagster_sqlmesh import (
     DagsterSQLMeshController,
     SQLMeshContextConfig,
     SQLMeshDagsterTranslator,
 )
+from sqlmesh.core.model import Model
 
 from ..factories import AssetFactoryResponse, early_resources_asset_factory
 from ..resources import SQLMeshExporter
@@ -28,18 +29,24 @@ def sqlmesh_export_factory(
 
     with controller.instance(environment) as mesh:
         models = mesh.models()
+        models_to_export: t.List[t.Tuple[Model, AssetKey]] = []
         for name, model in models.items():
             if "export" not in model.tags:
                 continue
-            # Create a export assets for this
-            for exporter in sqlmesh_exporters:
-                asset_def = exporter.create_export_asset(
-                    mesh,
-                    name,
+            models_to_export.append(
+                (
                     model,
                     sqlmesh_translator.get_asset_key_from_model(mesh.context, model),
                 )
-                assets.append(asset_def)
-                logger.debug(f"exporting for {name} to {asset_def.key.to_string()}")
+            )
+
+        # Create a export assets for this
+        for exporter in sqlmesh_exporters:
+            asset_def = exporter.create_export_asset(
+                mesh,
+                to_export=models_to_export,
+            )
+            assets.append(asset_def)
+            logger.debug(f"exporting for {exporter.__class__.__name__}")
 
     return AssetFactoryResponse(assets=assets)
