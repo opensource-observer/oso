0c854f2b716fc3a5a6df772976350e2580d8bfbe
Carl Cervone <carl.cervone@gmail.com>
2025-01-13 14:25:53 -0500

    fix(dbt): metric for total dependents

--- warehouse/dbt/models/marts/metrics/optimism/devtools/odt_metric__total_dependents.sql
+++ warehouse/dbt/models/marts/metrics/optimism/devtools/odt_metric__total_dependents.sql
@@ -1,6 +1,7 @@
 select
-  dependency_project_id as project_id,
+  devtool_project_id as project_id,
   'total_dependents' as metric,
-  count(distinct dependent_project_id) as amount
-from {{ ref('odt_int__code_dependencies') }}
-group by dependency_project_id
+  count(distinct onchain_project_id) as amount
+from {{ ref('odt_int__devtool_to_onchain_project_registry') }}
+where edge_type in ('npm_package', 'rust_package')
+group by devtool_project_id
