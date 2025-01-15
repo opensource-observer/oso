3a8895db2460d93e5d492c67e5e5339961171c80
Raymond Cheng <me@raymondcheng.net>
2025-01-14 18:00:47 -0800

    feat: pre-requisite intermediate models for DevRank (#2763)

---
+++ warehouse/metrics_mesh/models/intermediate/directory/int_artifacts.sql
@@ -0,0 +1,13 @@
+MODEL (
+  name metrics.int_artifacts,
+  description 'All artifacts',
+  kind FULL,
+);
+
+select distinct
+  artifact_id,
+  artifact_source_id,
+  artifact_source,
+  artifact_namespace,
+  artifact_name,
+from @oso_source('artifacts_by_project_v1')
