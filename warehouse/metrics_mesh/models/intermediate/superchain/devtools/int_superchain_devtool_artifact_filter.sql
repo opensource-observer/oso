3b1958885957ce36648c19b2128687a58ec5049c
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2025-01-16 21:54:15 +0000

    fix after rebase

--- warehouse/metrics_mesh/models/intermediate/superchain/devtools/int_superchain_devtool_artifact_filter.sql
+++ warehouse/metrics_mesh/models/intermediate/superchain/devtools/int_superchain_devtool_artifact_filter.sql
@@ -9,7 +9,7 @@ with eligible_projects as (
   select
     to_artifact_id as artifact_id,
     max(time) as last_release_date
-  from @oso_source('timeseries_events_by_artifact_v0') events
+  from @oso_source('bigquery.oso.timeseries_events_by_artifact_v0') events
   where event_type = 'RELEASE_PUBLISHED'
   group by to_artifact_id
 ),
@@ -21,7 +21,7 @@ union_all_artifacts as (
     date(last_release_date) >= (current_date() - interval @max_release_lookback_days day)
   union all
   select package_owner_artifact_id as artifact_id
-  from @oso_source('package_owners_v0')
+  from @oso_source('bigquery.oso.package_owners_v0')
 )
 
 select distinct artifact_id
