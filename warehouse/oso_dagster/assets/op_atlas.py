f1c9f2bce23af6472a1998d2c56552487a6f614c
Reuven Gonzales <reuven@karibalabs.co>
2025-01-24 11:20:16 -0800

    dlt configuration for op-atlas (#2843)

---
+++ warehouse/oso_dagster/assets/op_atlas.py
@@ -0,0 +1,105 @@
+from dlt.sources import incremental
+from oso_dagster.factories import sql_assets
+from oso_dagster.utils import SecretReference
+
+# The following tables should be included in the op_atlas asset:
+# Application
+# Badgeholder
+# Category
+# FundingReward (updatedAt)
+# FundingRound
+# GithubProximity
+# ImpactStatement
+# ImpactStatementAnswer
+# Organization (updatedAt)
+# OrganizationSnapshot (createdAt)
+# Project (updatedAt)
+# ProjectContract (updatedAt)
+# ProjectFunding (updatedAt)
+# ProjectLinks (updatedAt)
+# ProjectOrganization (updatedAt)
+# ProjectRepository (updatedAt)
+# ProjectSnapshot (createdAt)
+# RewardClaim (updatedAt)
+
+# These are available but maybe shouldn't be included:
+# User (updatedAt)
+# UserAddress (updatedAt)
+# UserEmail (no access)
+# UserInteraction
+# UserOrganization (updatedAt)
+# UserProjects
+
+
+op_atlas = sql_assets(
+    "op_atlas",
+    SecretReference(group_name="op_atlas", key="database"),
+    [
+        {
+            "table": "Application",
+            "incremental": incremental("updatedAt"),
+        },
+        {
+            "table": "Badgeholder",
+        },
+        {
+            "table": "Category",
+        },
+        {
+            "table": "FundingReward",
+            "incremental": incremental("updatedAt"),
+        },
+        {
+            "table": "FundingRound",
+        },
+        {
+            "table": "GithubProximity",
+        },
+        {
+            "table": "ImpactStatement",
+        },
+        {
+            "table": "ImpactStatementAnswer",
+        },
+        {
+            "table": "Organization",
+            "incremental": incremental("updatedAt"),
+        },
+        {
+            "table": "OrganizationSnapshot",
+            "incremental": incremental("createdAt"),
+        },
+        {
+            "table": "Project",
+            "incremental": incremental("updatedAt"),
+        },
+        {
+            "table": "ProjectContract",
+            "incremental": incremental("updatedAt"),
+        },
+        {
+            "table": "ProjectFunding",
+            "incremental": incremental("updatedAt"),
+        },
+        {
+            "table": "ProjectLinks",
+            "incremental": incremental("updatedAt"),
+        },
+        {
+            "table": "ProjectOrganization",
+            "incremental": incremental("updatedAt"),
+        },
+        {
+            "table": "ProjectRepository",
+            "incremental": incremental("updatedAt"),
+        },
+        {
+            "table": "ProjectSnapshot",
+            "incremental": incremental("createdAt"),
+        },
+        {
+            "table": "RewardClaim",
+            "incremental": incremental("updatedAt"),
+        },
+    ],
+)
