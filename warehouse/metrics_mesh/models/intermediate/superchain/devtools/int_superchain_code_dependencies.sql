dd07d98870bc1a9722a5d2bfb9ae862ae8895f15
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2025-01-25 16:13:00 -0500

    refactor(sqlmesh): bring more info into the code dependency model (#2864)
    * refactor(sqlmesh): bring more info into the code dependency model
    
    * fix(sqlmesh): use events_daily instead of bq table
    
    * fix(sqlmesh): change project id field

--- warehouse/metrics_mesh/models/intermediate/superchain/devtools/int_superchain_code_dependencies.sql
+++ warehouse/metrics_mesh/models/intermediate/superchain/devtools/int_superchain_code_dependencies.sql
@@ -3,31 +3,34 @@ MODEL (
   kind FULL,
 );
 
+@DEF(max_release_lookback_days, 180);
+
 with all_dependencies as (
   select distinct
-    sbom.from_project_id as dependent_project_id,
-    sbom.from_artifact_id as dependent_artifact_id,
-    owners.package_owner_project_id as dependency_project_id,
-    owners.package_owner_artifact_id as dependency_artifact_id,
-    sbom.to_package_artifact_name as dependency_name,
-    sbom.to_package_artifact_source as dependency_source
-  from metrics.sboms_v0 as sbom
-  inner join metrics.package_owners_v0 as owners
-    on
-      sbom.to_package_artifact_name = owners.package_artifact_name
-      and sbom.to_package_artifact_source = owners.package_artifact_source
+    sbom.project_id as dependent_project_id,
+    sbom.artifact_id as dependent_artifact_id,
+    sbom.package_github_project_id as dependency_project_id,
+    sbom.package_github_artifact_id as dependency_artifact_id,
+    sbom.package_artifact_source as dependency_source,
+    sbom.package_artifact_name as dependency_name,
+    releases.latest_package_release as dependency_release_date
+  from metrics.int_sbom_artifacts as sbom
+  inner join metrics.int_latest_release_by_repo as releases
+    on sbom.package_github_artifact_id = releases.artifact_id
 )
 
-select *
-from all_dependencies
-where
-  dependent_project_id in (
+select
+  all_dependencies.*,
+  case
+    when date(dependency_release_date) >= (current_date() - interval @max_release_lookback_days day)
+    then true
+    else false
+  end as devtool_has_recent_release,
+  case when dependent_project_id in (
     select distinct project_id
     from metrics.int_superchain_s7_onchain_builder_eligibility
     where is_eligible
-  )
-  and dependency_artifact_id in (
-    select artifact_id
-    from metrics.int_superchain_devtool_artifact_filter
-  )
-  and dependency_project_id != dependent_project_id
+  ) then true else false end as dependent_is_eligible
+from all_dependencies
+where
+  dependency_project_id != dependent_project_id
