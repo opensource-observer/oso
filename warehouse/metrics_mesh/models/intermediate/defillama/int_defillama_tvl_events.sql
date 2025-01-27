2e9148fc31d611268ffadfa6aebdf7dcacd04050
ccerv1 <carl.cervone@gmail.com>
2025-01-27 18:36:13 -0500

    fix: change artifact properties for defillama data

--- warehouse/metrics_mesh/models/intermediate/defillama/int_defillama_tvl_events.sql
+++ warehouse/metrics_mesh/models/intermediate/defillama/int_defillama_tvl_events.sql
@@ -1,13 +1,16 @@
 MODEL (
   name metrics.int_defillama_tvl_events,
-  description 'All tvl events from defi llama',
+  description 'All tvl events from DefiLlama',
   kind FULL,
 );
 
 
-@DEF(to_artifact_name, LOWER(all_tvl_events.slug));
 @DEF(to_artifact_namespace, LOWER(all_tvl_events.chain));
-@DEF(to_artifact_type, UPPER(all_tvl_events.token));
+@DEF(to_artifact_name, LOWER(all_tvl_events.slug));
+@DEF(to_artifact_type, "PROTOCOL");
+@DEF(from_artifact_namespace, LOWER(all_tvl_events.chain));
+@DEF(from_artifact_name, LOWER(all_tvl_events.token));
+@DEF(from_artifact_type, "TOKEN");
 
 with all_tvl_events as (
   @unioned_defillama_tvl_events()
@@ -20,18 +23,18 @@ SELECT
   UPPER('tvl') as event_type,
   @oso_id(
     all_tvl_events.time, 
-    all_tvl_events.slug, 
     all_tvl_events.chain, 
+    all_tvl_events.slug, 
     all_tvl_events.token
   ) as event_source_id,
   UPPER('defillama') as event_source,
   @to_artifact_name as to_artifact_name,
   @to_artifact_namespace as to_artifact_namespace,
   @to_artifact_type as to_artifact_type,
-  @oso_id(all_tvl_events.slug, all_tvl_events.chain, all_tvl_events.token) as to_artifact_source_id,
-  '' as from_artifact_name,
-  '' as from_artifact_namespace,
-  '' as from_artifact_type,
-  '' as from_artifact_source_id,
+  @oso_id(all_tvl_events.chain, all_tvl_events.slug) as to_artifact_source_id,
+  @from_artifact_name as from_artifact_name,
+  @from_artifact_namespace as from_artifact_namespace,
+  @from_artifact_type as from_artifact_type,
+  @oso_id(all_tvl_events.chain, all_tvl_events.token) as from_artifact_source_id,
   all_tvl_events.tvl::DOUBLE as amount
 FROM all_tvl_events as all_tvl_events
