238d5d521e13e17e2d4041bdef4b35431a53b37e
Carl Cervone <carl.cervone@gmail.com>
2025-01-28 04:53:11 -0500

    fix(sqlmesh): hardcodes artifact types in defillama int model

--- warehouse/metrics_mesh/models/intermediate/defillama/int_defillama_tvl_events.sql
+++ warehouse/metrics_mesh/models/intermediate/defillama/int_defillama_tvl_events.sql
@@ -7,10 +7,8 @@ MODEL (
 
 @DEF(to_artifact_namespace, LOWER(all_tvl_events.chain));
 @DEF(to_artifact_name, LOWER(all_tvl_events.slug));
-@DEF(to_artifact_type, "PROTOCOL");
 @DEF(from_artifact_namespace, LOWER(all_tvl_events.chain));
 @DEF(from_artifact_name, LOWER(all_tvl_events.token));
-@DEF(from_artifact_type, "TOKEN");
 
 with all_tvl_events as (
   @unioned_defillama_tvl_events()
@@ -18,8 +16,6 @@ with all_tvl_events as (
 
 SELECT
   @from_unix_timestamp(all_tvl_events.time) as "time",
-  @oso_id(@to_artifact_name, @to_artifact_namespace, @to_artifact_type) as to_artifact_id,
-  '' as from_artifact_id,
   UPPER('tvl') as event_type,
   @oso_id(
     all_tvl_events.time, 
@@ -30,11 +26,13 @@ SELECT
   UPPER('defillama') as event_source,
   @to_artifact_name as to_artifact_name,
   @to_artifact_namespace as to_artifact_namespace,
-  @to_artifact_type as to_artifact_type,
-  @oso_id(all_tvl_events.chain, all_tvl_events.slug) as to_artifact_source_id,
+  UPPER('protocol') as to_artifact_type,
+  @oso_id(@to_artifact_namespace, @to_artifact_name) as to_artifact_id,
+  @oso_id(@to_artifact_namespace, @to_artifact_name) as to_artifact_source_id,
   @from_artifact_name as from_artifact_name,
   @from_artifact_namespace as from_artifact_namespace,
-  @from_artifact_type as from_artifact_type,
-  @oso_id(all_tvl_events.chain, all_tvl_events.token) as from_artifact_source_id,
+  UPPER('token') as from_artifact_type,
+  @oso_id(@from_artifact_namespace, @from_artifact_name) as from_artifact_id,
+  @oso_id(@from_artifact_namespace, @from_artifact_name) as from_artifact_source_id,
   all_tvl_events.tvl::DOUBLE as amount
 FROM all_tvl_events as all_tvl_events
