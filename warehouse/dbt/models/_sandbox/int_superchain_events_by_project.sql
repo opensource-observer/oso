91a0dad54716f8cf071a6430a257ab24f0f6c46d
Carl Cervone <carl.cervone@gmail.com>
2025-01-28 06:01:56 -0500

    fix(dbt): update de-duping logic for trace/txs

--- warehouse/dbt/models/_sandbox/int_superchain_events_by_project.sql
+++ warehouse/dbt/models/_sandbox/int_superchain_events_by_project.sql
@@ -21,9 +21,6 @@ with events as (
     {{ oso_id("chain", "to_address_trace") }} as to_address_trace_id,
     {{ oso_id("chain", "to_address_tx") }} as to_address_tx_id
   from {{ ref('int_superchain_traces_txs_joined') }}
-  where
-    from_address_tx != from_address_trace
-    and to_address_tx != to_address_trace
 ),
 
 addresses_by_project as (
@@ -34,24 +31,8 @@ addresses_by_project as (
   from {{ ref('int_artifacts_by_project') }}
 ),
 
-filtered_traces as (
-  select
-    addresses_by_project.project_id as to_project_id,
-    events.block_timestamp,
-    events.chain,
-    events.transaction_hash,
-    events.from_address_tx_id as from_artifact_id,
-    events.to_address_trace_id as to_artifact_id,
-    events.gas_used_trace as gas_used,
-    events.gas_price_tx as gas_price,
-    'TRACE_EVENT' as event_type
-  from events
-  inner join addresses_by_project
-    on events.to_address_trace_id = addresses_by_project.artifact_id
-),
-
 filtered_txns as (
-  select
+  select distinct
     addresses_by_project.project_id as to_project_id,
     events.block_timestamp,
     events.chain,
@@ -66,10 +47,28 @@ filtered_txns as (
     on events.to_address_tx_id = addresses_by_project.artifact_id
 ),
 
+filtered_traces as (
+  select distinct
+    addresses_by_project.project_id as to_project_id,
+    events.block_timestamp,
+    events.chain,
+    events.transaction_hash,
+    events.from_address_tx_id as from_artifact_id,
+    events.to_address_trace_id as to_artifact_id,
+    events.gas_used_trace as gas_used,
+    events.gas_price_tx as gas_price,
+    'TRACE_EVENT' as event_type
+  from events
+  inner join addresses_by_project
+    on events.to_address_trace_id = addresses_by_project.artifact_id
+  where events.to_address_trace_id != events.to_address_tx_id
+),
+
+
 union_events as (
   select * from filtered_traces
   union all
   select * from filtered_txns
 )
 
-select distinct * from union_events
+select * from union_events
