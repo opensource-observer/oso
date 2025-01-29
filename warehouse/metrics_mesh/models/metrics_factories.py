79c216c52a6e046ad41207da741e7a8f040e9aeb
Javier <javi@karibalabs.co>
2025-01-29 22:14:54 +0000

    add: use new `over_all_time` option
    Co-authored-by: Reuven Gonzales <ravenac95@users.noreply.github.com>

--- warehouse/metrics_mesh/models/metrics_factories.py
+++ warehouse/metrics_mesh/models/metrics_factories.py
@@ -29,38 +29,65 @@ timeseries_metrics(
         "stars": MetricQueryDef(
             ref="stars.sql",
             time_aggregations=["daily", "weekly", "monthly"],
+            rolling=RollingConfig(
+                windows=[30, 90, 180],
+                unit="day",
+                cron="@daily",
+            ),
+            entity_types=["artifact", "project", "collection"],
+            over_all_time=True,
         ),
+        # "stars": MetricQueryDef(
+        #     ref="stars.sql",
+        #     time_aggregations=["daily", "weekly", "monthly"],
+        #     rolling_window={ ... rolling window config ... },
+        #     over_all_time=True/False,
+        # ),
         "commits": MetricQueryDef(
             ref="commits.sql",
             time_aggregations=["daily", "weekly", "monthly"],
+            rolling=RollingConfig(
+                windows=[10],
+                unit="day",
+                cron="@daily",
+                slots=8,
+            ),
+            over_all_time=True,
         ),
         "comments": MetricQueryDef(
             ref="comments.sql",
             time_aggregations=["daily", "weekly", "monthly"],
+            over_all_time=True,
         ),
         "releases": MetricQueryDef(
             ref="releases.sql",
             time_aggregations=["daily", "weekly", "monthly"],
+            over_all_time=True,
         ),
         "forks": MetricQueryDef(
             ref="forks.sql",
             time_aggregations=["daily", "weekly", "monthly"],
+            over_all_time=True,
         ),
         "repositories": MetricQueryDef(
             ref="repositories.sql",
             time_aggregations=["daily", "weekly", "monthly"],
+            over_all_time=True,
         ),
         "active_contracts": MetricQueryDef(
             ref="active_contracts.sql",
             time_aggregations=["daily", "weekly", "monthly"],
+            over_all_time=True,
         ),
         "contributors": MetricQueryDef(
             ref="contributors.sql",
             time_aggregations=["daily", "weekly", "monthly"],
+            over_all_time=True,
         ),
         "active_developers": MetricQueryDef(
             ref="active_developers.sql",
             time_aggregations=["daily", "weekly", "monthly"],
+            over_all_time=True,
         ),
         # This defines something with a rolling option that allows you to look back
         # to some arbitrary window. So you specify the window and specify the unit.
@@ -154,16 +181,6 @@ timeseries_metrics(
                 slots=32,
             ),
         ),
-        "commits_rolling": MetricQueryDef(
-            ref="commits.sql",
-            rolling=RollingConfig(
-                windows=[10],
-                unit="day",
-                cron="@daily",
-                slots=8,
-            ),
-            entity_types=["artifact", "project", "collection"],
-        ),
         "opened_pull_requests": MetricQueryDef(
             ref="prs_opened.sql",
             rolling=RollingConfig(
@@ -173,6 +190,7 @@ timeseries_metrics(
                 slots=8,
             ),
             entity_types=["artifact", "project", "collection"],
+            over_all_time=True,
         ),
         "merged_pull_requests": MetricQueryDef(
             ref="prs_merged.sql",
@@ -183,6 +201,7 @@ timeseries_metrics(
                 slots=8,
             ),
             entity_types=["artifact", "project", "collection"],
+            over_all_time=True,
         ),
         "opened_issues": MetricQueryDef(
             ref="issues_opened.sql",
@@ -193,6 +212,7 @@ timeseries_metrics(
                 slots=8,
             ),
             entity_types=["artifact", "project", "collection"],
+            over_all_time=True,
         ),
         "closed_issues": MetricQueryDef(
             ref="issues_closed.sql",
@@ -203,6 +223,7 @@ timeseries_metrics(
                 slots=8,
             ),
             entity_types=["artifact", "project", "collection"],
+            over_all_time=True,
         ),
         "avg_prs_time_to_merge": MetricQueryDef(
             ref="prs_time_to_merge.sql",
@@ -213,6 +234,7 @@ timeseries_metrics(
                 slots=8,
             ),
             entity_types=["artifact", "project", "collection"],
+            over_all_time=True,
         ),
         "avg_time_to_first_response": MetricQueryDef(
             ref="time_to_first_response.sql",
@@ -223,15 +245,9 @@ timeseries_metrics(
                 slots=8,
             ),
             entity_types=["artifact", "project", "collection"],
+            over_all_time=True,
         ),
         "active_addresses_aggregation": MetricQueryDef(
-            ref="active_addresses.sql",
-            vars={
-                "activity_event_types": ["CONTRACT_INVOCATION_SUCCESS_DAILY_COUNT"],
-            },
-            time_aggregations=["daily", "monthly"],
-        ),
-        "active_addresses_rolling": MetricQueryDef(
             ref="active_addresses.sql",
             vars={
                 "activity_event_types": ["CONTRACT_INVOCATION_SUCCESS_DAILY_COUNT"],
@@ -242,6 +258,8 @@ timeseries_metrics(
                 cron="@daily",
                 slots=32,
             ),
+            time_aggregations=["daily", "monthly"],
+            over_all_time=True,
         ),
         "gas_fees": MetricQueryDef(
             ref="gas_fees.sql",
@@ -252,6 +270,7 @@ timeseries_metrics(
                 slots=16,
             ),
             entity_types=["artifact", "project", "collection"],
+            over_all_time=True,
         ),
         "transactions": MetricQueryDef(
             ref="transactions.sql",
@@ -262,6 +281,7 @@ timeseries_metrics(
                 slots=32,
             ),
             entity_types=["artifact", "project", "collection"],
+            over_all_time=True,
         ),
         "contributors_lifecycle": MetricQueryDef(
             ref="lifecycle.sql",
@@ -290,6 +310,7 @@ timeseries_metrics(
                 slots=8,
             ),
             entity_types=["artifact", "project", "collection"],
+            over_all_time=True,
         ),
         "dependencies": MetricQueryDef(
             ref="dependencies.sql",
@@ -300,106 +321,7 @@ timeseries_metrics(
                 slots=16,
             ),
             entity_types=["artifact", "project", "collection"],
-        ),
-        "key_active_address_count": MetricQueryDef(
-            ref="key_active_address_count.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_active_contract_count": MetricQueryDef(
-            ref="key_active_contract_count.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_active_developer_count": MetricQueryDef(
-            ref="key_active_developer_count.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_comment_count": MetricQueryDef(
-            ref="key_comment_count.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_commit_count": MetricQueryDef(
-            ref="key_commit_count.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_contributor_active_day_count": MetricQueryDef(
-            ref="key_contributor_active_day_count.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_contributor_count": MetricQueryDef(
-            ref="key_contributor_count.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_dependencies_count": MetricQueryDef(
-            ref="key_dependencies_count.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_developer_active_day_count": MetricQueryDef(
-            ref="key_developer_active_day_count.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_developer_count": MetricQueryDef(
-            ref="key_developer_count.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_first_commit": MetricQueryDef(
-            ref="key_first_commit.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_fork_count": MetricQueryDef(
-            ref="key_fork_count.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_funding_received": MetricQueryDef(
-            ref="key_funding_received.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_gas_fees": MetricQueryDef(
-            ref="key_gas_fees.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_issue_count_closed": MetricQueryDef(
-            ref="key_issue_count_closed.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_issue_count_opened": MetricQueryDef(
-            ref="key_issue_count_opened.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_last_commit": MetricQueryDef(
-            ref="key_last_commit.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_pr_count_merged": MetricQueryDef(
-            ref="key_pr_count_merged.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_pr_count_opened": MetricQueryDef(
-            ref="key_pr_count_opened.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_pr_time_to_merge": MetricQueryDef(
-            ref="key_pr_time_to_merge.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_release_count": MetricQueryDef(
-            ref="key_release_count.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_repository_count": MetricQueryDef(
-            ref="key_repository_count.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_star_count": MetricQueryDef(
-            ref="key_star_count.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_time_to_first_response": MetricQueryDef(
-            ref="key_time_to_first_response.sql",
-            entity_types=["artifact", "project", "collection"],
-        ),
-        "key_transaction_count": MetricQueryDef(
-            ref="key_transaction_count.sql",
-            entity_types=["artifact", "project", "collection"],
+            over_all_time=True,
         ),
     },
     default_dialect="clickhouse",
