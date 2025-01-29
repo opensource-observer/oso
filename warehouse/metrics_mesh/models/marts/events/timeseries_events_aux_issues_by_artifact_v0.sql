76c175e389893fb0063f2fe458ec1291c9e8d832
Raymond Cheng <me@raymondcheng.net>
2025-01-29 13:01:21 -0800

    feat: migrate int_events__github to sqlmesh (#2885)
    * feat: migrate int_events__github to sqlmesh
    
    * Grabs all staging models from stg_github__events down
    
    * fix: sqlfluff bug in int_sbom_artifacts

---
+++ warehouse/metrics_mesh/models/marts/events/timeseries_events_aux_issues_by_artifact_v0.sql
@@ -0,0 +1,24 @@
+MODEL (
+  name metrics.timeseries_events_aux_issues_by_artifact_v0,
+  kind INCREMENTAL_BY_TIME_RANGE (
+    time_column time,
+  ),
+  start '2015-01-01',
+  cron '@daily',
+  grain (time, event_type, event_source, from_artifact_id, to_artifact_id)
+);
+
+select
+  time,
+  to_artifact_id,
+  from_artifact_id,
+  event_type,
+  event_source_id,
+  event_source,
+  issue_id,
+  issue_number,
+  created_at,
+  merged_at,
+  closed_at,
+  comments
+from metrics.int_events_aux_issues
