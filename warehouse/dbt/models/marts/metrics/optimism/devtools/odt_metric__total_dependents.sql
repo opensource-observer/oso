339430a47305534b4c8b3ba9236931c45f3af46a
Carl Cervone <carl.cervone@gmail.com>
2025-01-30 17:09:09 -0500

    chore(dbt): clear out early versions of models

--- warehouse/dbt/models/marts/metrics/optimism/devtools/odt_metric__total_dependents.sql
+++
@@ -1,7 +0,0 @@
-select
-  devtool_project_id as project_id,
-  'total_dependents' as metric,
-  count(distinct onchain_project_id) as amount
-from {{ ref('odt_int__devtool_to_onchain_project_registry') }}
-where edge_type in ('npm_package', 'rust_package')
-group by devtool_project_id
