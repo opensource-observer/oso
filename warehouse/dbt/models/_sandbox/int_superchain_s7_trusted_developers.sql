69ed6a5920a9d9dab1ef3968a76443557ef673b1
Carl Cervone <carl.cervone@gmail.com>
2025-01-30 07:01:10 -0500

    feat(dbt): dependency graph

--- warehouse/dbt/models/_sandbox/int_superchain_s7_trusted_developers.sql
+++ warehouse/dbt/models/_sandbox/int_superchain_s7_trusted_developers.sql
@@ -7,33 +7,10 @@
 {% set active_months_threshold = 3 %}
 {% set commits_threshold = 20 %}
 {% set last_commit_threshold_months = 12 %}
-{% set min_repo_stars = 5 %}
-{% set last_repo_update_date = '2024-07-01' %}
 
 
 with eligible_repos as (
-  select
-    projects.sample_date,
-    projects.project_id,
-    repositories.artifact_id as repo_artifact_id,
-    repositories.artifact_namespace as repo_owner,
-    repositories.artifact_name as repo_name,
-    repositories.language as repo_language,
-    repositories.created_at as repo_created_at,
-    repositories.updated_at as repo_updated_at,
-    repositories.star_count as repo_stars,
-    repositories.fork_count as repo_forks,
-    projects.transaction_count as project_transaction_count,
-    projects.gas_fees as project_gas_fees,
-    projects.user_count as project_user_count
-  from {{ ref('int_superchain_s7_onchain_builder_eligibility') }} as projects
-  inner join {{ ref('int_repositories') }} as repositories
-    on projects.project_id = repositories.project_id
-  where
-    projects.is_eligible
-    and repositories.language in ('TypeScript', 'Solidity', 'Rust')
-    and repositories.updated_at > '{{ last_repo_update_date }}'
-    and repositories.star_count > {{ min_repo_stars }}
+  select * from {{ ref('int_superchain_s7_onchain_builder_repositories') }}
 ),
 
 developers as (
@@ -63,7 +40,19 @@ developers as (
 
 eligible_developers_by_repo as (
   select
-    eligible_repos.*,
+    eligible_repos.sample_date,
+    eligible_repos.repo_artifact_id,
+    eligible_repos.repo_owner,
+    eligible_repos.repo_name,
+    eligible_repos.repo_language,
+    eligible_repos.repo_created_at,
+    eligible_repos.repo_updated_at,
+    eligible_repos.repo_stars,
+    eligible_repos.repo_forks,
+    eligible_repos.project_id,
+    eligible_repos.project_transaction_count,
+    eligible_repos.project_gas_fees,
+    eligible_repos.project_user_count,
     developers.developer_id,
     developers.developer_name,
     developers.total_commits,
