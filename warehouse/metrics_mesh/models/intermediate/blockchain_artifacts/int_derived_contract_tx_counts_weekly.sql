21f9b5e113dc73a48186aebc7f9fbcabac9f7451
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2025-02-03 23:24:45 +0000

    Fixes issues with run

--- warehouse/metrics_mesh/models/intermediate/blockchain_artifacts/int_derived_contract_tx_counts_weekly.sql
+++ warehouse/metrics_mesh/models/intermediate/blockchain_artifacts/int_derived_contract_tx_counts_weekly.sql
@@ -8,12 +8,13 @@
 -- used by another model downstream to calculate the _current_ sorting order
 
 MODEL (
-  name metrics.int_derived_contracts_transactions_weekly
+  name metrics.int_derived_contracts_transactions_weekly,
   kind INCREMENTAL_BY_TIME_RANGE (
-    time_column time,
+    time_column week,
     batch_size 365,
     batch_concurrency 1
   ),
+  partitioned_by DAY(week),
   -- We only need to start a year before from the time we start using this
   -- model. We are using this as a way to categorize model ordering.
   start '2024-02-03'
@@ -27,9 +28,9 @@ select
   count(*) as tx_count
 from @oso_source('bigquery.optimism_superchain_raw_onchain_data.traces') as traces
 inner join metrics.int_derived_contracts as derived_contracts
-  on traces.contract_address = derived_contracts.contract_address
+  on traces.to_address = derived_contracts.contract_address
 where 
-  network = 'mainnet'
+  traces.network = 'mainnet'
   and "status" = 1
   and dt between @start_date and @end_date
 group by 1, 2
