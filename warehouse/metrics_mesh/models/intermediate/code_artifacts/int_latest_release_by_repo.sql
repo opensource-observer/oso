5ee6c0c26d5fe304ded9e9eade9cacfcf9c85116
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2025-02-03 18:46:18 -0500

    feat(sqlmesh): devtooling metrics (excluding openrank variants) - testing version (#2953)
    * chore: bump poetry.lock
    
    * feat: macro for resolving chain names between superchain and OSO
    
    * fix: minor updates to joined txns + traces model
    
    * fix: macro and col name
    
    * feat: implement trace level event logic with project counts
    
    * refactor: onchain user labels
    
    * feat: initial metrics and eligibility models for onchain builders
    
    * refactor: move code_dependencies and repositories_enriched to directory-level models
    
    * refactor: move some github/dependency related into their own directory
    
    * fix: update latest repo release logic
    
    * feat: add developer activity by repo model
    
    * feat: created trusted developer model for s7
    
    * feat: implement the repo eligibility model
    
    * feat: implement the project : developer graph
    
    * fix: (hopefully) fixes some timestamp issues
    
    * feat: implement project to dependency graph
    
    * feat: implements graph- and repo-based devtooling metrics
    
    * fix partitioning definitions
    
    * fix oso_id usage
    
    * fix: date partition tuples
    
    * fix: move project amortization logic to the metrics model only
    
    * fix: trino execution fixes
    
    * chore: update poetry version
    
    * fix: google bq types changed
    
    ---------
    
    Co-authored-by: Reuven V. Gonzales <reuven.v.gonzales@gmail.com>

--- warehouse/metrics_mesh/models/intermediate/code_artifacts/int_latest_release_by_repo.sql
+++ warehouse/metrics_mesh/models/intermediate/code_artifacts/int_latest_release_by_repo.sql
@@ -6,8 +6,8 @@ MODEL (
 with repo_releases as (
   select
     to_artifact_id as artifact_id,
-    max(bucket_day) as latest_repo_release
-  from metrics.int_events_daily_to_project as events
+    max("time") as latest_repo_release
+  from metrics.int_events__github
   where event_type = 'RELEASE_PUBLISHED'
   group by to_artifact_id
 ),
@@ -20,10 +20,15 @@ package_releases as (
   group by package_github_artifact_id
 )
 
-select
+select distinct
   coalesce(r.artifact_id, p.artifact_id) as artifact_id,
-  r.latest_repo_release,
-  p.latest_package_release
+  coalesce(r.latest_repo_release, p.latest_package_release)
+    as last_release_published,
+  case
+    when r.latest_repo_release is not null then 'GITHUB_RELEASE'
+    else 'PACKAGE_SNAPSHOT'
+  end as last_release_source
 from repo_releases r
 full outer join package_releases p
   on r.artifact_id = p.artifact_id
+where coalesce(r.latest_repo_release, p.latest_package_release) is not null
