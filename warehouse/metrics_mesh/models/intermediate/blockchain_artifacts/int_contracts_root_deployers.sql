e1649d283cabf13446771b7b6e806a91b2d85ead
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2025-02-08 11:34:21 +0000

    Fix start time

--- warehouse/metrics_mesh/models/intermediate/blockchain_artifacts/int_contracts_root_deployers.sql
+++ warehouse/metrics_mesh/models/intermediate/blockchain_artifacts/int_contracts_root_deployers.sql
@@ -11,17 +11,15 @@ MODEL (
   name metrics.int_contracts_root_deployers,
   kind INCREMENTAL_BY_TIME_RANGE (
     time_column deployment_timestamp,
-    batch_size 30,
+    batch_size 90,
     batch_concurrency 1,
     lookback 30
     --forward_only true
   ),
+  start '2021-10-01',
   partitioned_by (DAY("deployment_timestamp"), "chain")
 );
 
--- Take all of the contracts deployed up till now and find the root deployer
--- recursively once we find an originating address that is not a contract
--- address we know we have found the root deployer
 with existing_contracts as (
   select * 
   from @this_model
@@ -41,6 +39,7 @@ with existing_contracts as (
     end as factory_address,
     create_type,
     case 
+      -- for create deployed factories we can just use the originating address
       when originating_address = factory_address then originating_address
       when is_proxy then originating_address
       else null
@@ -65,7 +64,10 @@ with existing_contracts as (
       when new.root_deployer_address is not null then new.root_deployer_address
       else all.originating_address
     end as root_deployer_address,
-    new.depth + 1 as depth
+    case
+      when all.depth is not null then all.depth + 1
+      else new.depth
+    end as depth
   from new_contracts as new
   left join all_contracts as all 
     on
