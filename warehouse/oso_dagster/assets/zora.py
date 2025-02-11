f70a79c18700f12d343cac02260002a68605ebd0
Reuven Gonzales <reuven@karibalabs.co>
2025-02-10 18:36:14 -0800

    Remove superchain datasets from dagster (#3021)
    remove superchain datasets from dagster

--- warehouse/oso_dagster/assets/zora.py
+++
@@ -1,8 +0,0 @@
-from ..factories.goldsky import goldsky_network_assets
-
-
-zora_network = goldsky_network_assets(
-    network_name="zora",
-    destination_dataset_name="superchain",
-    working_destination_dataset_name="oso_raw_sources",
-)
