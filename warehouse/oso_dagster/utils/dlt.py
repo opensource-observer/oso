6d5f6bbe0e6b7bac98194dc203415cebd7f745e3
Javier <javi@karibalabs.co>
2025-02-18 18:11:00 +0000

    add: dlt resilient data `chunk` mechanism

--- warehouse/oso_dagster/utils/dlt.py
+++ warehouse/oso_dagster/utils/dlt.py
@@ -1,12 +1,14 @@
 import asyncio
 import json
 import logging
+import uuid
 from dataclasses import dataclass
 from datetime import datetime
-from functools import cmp_to_key
+from functools import partial
 from itertools import batched
 from typing import (
     Any,
+    AsyncGenerator,
     Callable,
     Coroutine,
     Generator,
@@ -45,12 +47,8 @@ class ParallelizeConfig:
 
 def dlt_parallelize(config: ParallelizeConfig):
     """
-    Decorator for parallelizing a synchronous generator function that yields callables.
-
-    The decorated function is expected to be synchronous and yield callables that, when called,
-    return a coroutine (i.e. of type Callable[..., Coroutine[Any, Any, R]]). The decorator converts
-    these callables into coroutine objects, batches them using the specified configuration,
-    runs them concurrently using asyncio, and yields their results synchronously.
+    Decorator that parallelizes the execution of coroutine tasks. It processes
+    coroutine tasks in parallel and yields results.
 
     Args:
         config (ParallelizeConfig): Configuration object
@@ -58,17 +56,20 @@ def dlt_parallelize(config: ParallelizeConfig):
 
     def _decorator(
         fn: Callable[K, Generator[Callable[..., Coroutine[Any, Any, R]], None, None]]
-    ) -> Callable[K, Generator[R, None, None]]:
+    ) -> Callable[K, AsyncGenerator[R, None]]:
         """
-        Decorator function that wraps the original synchronous generator function.
+        Decorator function that wraps the original generator function.
 
         Args:
             fn (Callable): The original function that yields callables returning coroutine objects.
         """
 
-        def _wrapper(*args: K.args, **kwargs: K.kwargs) -> Generator[R, None, None]:
+        async def _wrapper(
+            *args: K.args,
+            **kwargs: K.kwargs,
+        ) -> AsyncGenerator[R, None]:
             """
-            Synchronous wrapper that processes coroutine tasks in parallel and yields results.
+            Asynchronous wrapper that processes coroutine tasks in parallel and yields results.
 
             Args:
                 *args: Positional arguments forwarded to the original function.
@@ -81,44 +82,40 @@ def dlt_parallelize(config: ParallelizeConfig):
 
             log = context.log if context else logger
 
-            async def process_batches() -> List[R]:
-                """
-                Asynchronously collects and processes coroutine tasks in batches.
+            tasks: List[Coroutine[Any, Any, R]] = [
+                task() for task in fn(*args, **kwargs)
+            ]
+            batches_list = list(batched(tasks, config.chunk_size))
 
-                This function:
-                  - Calls the original function to get a generator of callables.
-                  - Converts each callable into a coroutine object.
-                  - Splits the list of coroutines into batches based on `chunk_size`.
-                  - Processes these batches concurrently in groups based on `parallel_batches`.
-                  - Waits asynchronously between batch groups as defined by `wait_interval`.
-                """
+            for batch_group in batched(batches_list, config.parallel_batches):
+                log.info(
+                    f"DLTParallelize: Executing {len(batch_group)} batch(es) in parallel"
+                )
+                group_coroutines = [coro for batch in batch_group for coro in batch]
 
-                results: List[R] = []
-                tasks: List[Coroutine[Any, Any, R]] = [
-                    task() for task in fn(*args, **kwargs)
+                results = [
+                    await future for future in asyncio.as_completed(group_coroutines)
                 ]
-                batches_list = list(batched(tasks, config.chunk_size))
 
-                for batch_group in batched(batches_list, config.parallel_batches):
-                    log.info(
-                        f"DLTParallelize: Executing {len(batch_group)} batch(es) in parallel"
-                    )
+                for result in results:
+                    yield result
 
-                    group_coroutines = [coro for batch in batch_group for coro in batch]
+                log.info(
+                    f"DLTParallelize: Waiting for {config.wait_interval} seconds ..."
+                )
 
-                    for future in asyncio.as_completed(group_coroutines):
-                        res = await future
-                        results.append(res)
+                chunk_update_fn = kwargs.get("_chunk_resource_update")
+                if chunk_update_fn and isinstance(chunk_update_fn, Callable):
+                    chunk_update_fn(results)
+                else:
+                    log.warning("DLTParallelize: chunk_update_fn function not found")
 
-                    log.info(
-                        f"DLTParallelize: Waiting for {config.wait_interval} seconds ..."
-                    )
+                await asyncio.sleep(config.wait_interval)
 
-                    await asyncio.sleep(config.wait_interval)
-
-                return results
-
-            yield from asyncio.run(process_batches())
+            retrieve_failed_fn = kwargs.get("_chunk_retrieve_failed")
+            if retrieve_failed_fn and isinstance(retrieve_failed_fn, Callable):
+                for retrieved in retrieve_failed_fn():
+                    yield retrieved
 
         return _wrapper
 
@@ -129,24 +126,43 @@ T = TypeVar("T")
 
 
 class ChunkedResourceConfig(Generic[T]):
+    """
+    Configuration for the process_chunked_resource function. This must
+    be used in conjunction with the `dlt_parallelize` decorator.
+
+    Attributes:
+        fetch_data_fn (Callable): Function that fetches the data to be chunked.
+            It will be called only once if the state file does not exist.
+        resource (DltResource): DltResource class to be used for processing the data.
+        to_string_fn (Callable): Function that converts a single data unit
+            to a string representation. For Pydantic models, this can be the model's
+            `model_dump_json` method.
+        gcs_bucket_name (str): Google Cloud Storage bucket name.
+        gcs_prefix (str): Google Cloud Storage prefix for chunked data. Defaults
+            to "dlt_chunked_state".
+        max_manifest_age (int): Maximum age of the manifest file in seconds. If the
+            manifest file is older than this value, the manifest will be reset. This
+            means that the `fetch_data_fn` will be called again, re-fetching all the
+            data and starting from scratch. Defaults to 24 hours.
+        context (AssetExecutionContext): Dagster context object.
+    """
+
     def __init__(
         self,
         fetch_data_fn: Callable[[], List[T]],
         resource: DltResource,
-        chunk_size: int,
+        to_string_fn: Callable[..., str],
         gcs_bucket_name: str,
-        sort_fn: Callable[[T, T], int],
-        gcs_prefix: str = "_chunked_state",
-        max_data_age: int = 60 * 60 * 24,
+        gcs_prefix: str = "dlt_chunked_state",
+        max_manifest_age: int = 60 * 60 * 24,
         context: AssetExecutionContext | None = None,
     ):
         self.fetch_data_fn = fetch_data_fn
         self.resource = resource
-        self.chunk_size = chunk_size
+        self.to_string_fn = to_string_fn
         self.gcs_bucket_name = gcs_bucket_name
-        self.sort_fn = sort_fn
         self.gcs_prefix = gcs_prefix
-        self.max_data_age = max_data_age
+        self.max_manifest_age = max_manifest_age
         self.context = context
 
 
@@ -164,72 +180,101 @@ def process_chunked_resource(
 
     log.info(f"ChunkedResource: Checking state in {state_blob.name}")
 
+    def resource_update(elements: List[List]):
+        count = len(elements)
+        current_manifest = state_blob.download_as_string()
+        current_data = json.loads(current_manifest)
+
+        current_data["pending_data"] = current_data["pending_data"][count:]
+        current_data["updated_at"] = datetime.now().isoformat()
+
+        log.info(
+            f"ChunkedResource: Updating state with {len(current_data['pending_data'])} pending entries"
+        )
+
+        state_blob.upload_from_string(json.dumps(current_data))
+
+        if not current_data["pending_data"]:
+            log.info("ChunkedResource: No more pending data, deleting state")
+            state_blob.delete()
+
+        stringified_elems = [
+            config.to_string_fn(elem) for sublist in elements for elem in sublist
+        ]
+
+        new_blob = bucket.blob(
+            f"{config.gcs_prefix}/{config.resource.name}/chunk.{uuid.uuid4()}.json"
+        )
+
+        new_blob.upload_from_string(json.dumps(stringified_elems))
+
+        log.info(f"ChunkedResource: Uploaded {count} elements to {new_blob.name}")
+
+    def retrieve_failed(yield_elems: bool):
+        blobs = bucket.list_blobs(
+            prefix=f"{config.gcs_prefix}/{config.resource.name}/chunk."
+        )
+
+        if yield_elems:
+            for blob in blobs:
+                if blob.name.endswith(".json"):
+                    log.info(f"ChunkedResource: Retrieving chunk {blob.name}")
+                    yield json.loads(blob.download_as_string())
+
+        blobs = bucket.list_blobs(prefix=f"{config.gcs_prefix}/{config.resource.name}")
+
+        for blob in blobs:
+            if blob.name.endswith(".json"):
+                log.info(f"ChunkedResource: Deleting {blob.name}")
+                blob.delete()
+
+    kwargs["_chunk_retrieve_failed"] = partial(retrieve_failed, False)
+
     try:
+        if not state_blob.exists():
+            raise NotFound("State file does not exist")
+
         manifest = state_blob.download_as_string()
         manifest_data = json.loads(manifest)
 
         log.info("ChunkedResource: Found existing state manifest")
+        kwargs["_chunk_retrieve_failed"] = partial(retrieve_failed, True)
 
         if (
             datetime.now() - datetime.fromisoformat(manifest_data["updated_at"])
             or datetime.now() - datetime.fromisoformat(manifest_data["created_at"])
-        ).total_seconds() > config.max_data_age:
+        ).total_seconds() > config.max_manifest_age:
             log.info("ChunkedResource: State manifest is too old, resetting")
             manifest_data = None
+            kwargs["_chunk_retrieve_failed"] = partial(retrieve_failed, False)
 
     except (NotFound, json.JSONDecodeError):
         log.info("ChunkedResource: No existing state found, creating new manifest")
-        manifest_data = None
-
-    if manifest_data is None:
-        log.info("ChunkedResource: Processing input data")
-        data = config.fetch_data_fn()
-
-        log.info("ChunkedResource: Sorting and chunking input data")
-        sorted_data = sorted(data, key=cmp_to_key(config.sort_fn))
-        chunks = [list(batch) for batch in batched(sorted_data, config.chunk_size)]
-
-        log.info(
-            f"ChunkedResource: Created {len(chunks)} chunks of size {config.chunk_size}"
-        )
 
         manifest_data = {
             "updated_at": datetime.now().isoformat(),
             "created_at": datetime.now().isoformat(),
-            "processed_chunks": [],
-            "pending_chunks": chunks,
+            "pending_data": config.fetch_data_fn(),
+        }
+
+        state_blob.upload_from_string(json.dumps(manifest_data))
+
+    if manifest_data is None:
+        log.info("ChunkedResource: Processing input data")
+
+        manifest_data = {
+            "updated_at": datetime.now().isoformat(),
+            "created_at": datetime.now().isoformat(),
+            "pending_data": config.fetch_data_fn(),
         }
 
         log.info("ChunkedResource: Uploading initial manifest")
         state_blob.upload_from_string(json.dumps(manifest_data))
 
-    pending_chunks = list(manifest_data["pending_chunks"])
+    kwargs["_chunk_resource_update"] = resource_update
 
-    for chunk in pending_chunks:
-        log.info(f"ChunkedResource: Processing chunk with {len(chunk)} items")
-
-        try:
-            yield config.resource(chunk, *args, **kwargs)
-
-            manifest_data["processed_chunks"].append(chunk)
-            manifest_data["pending_chunks"].remove(chunk)
-            manifest_data["updated_at"] = datetime.now().isoformat()
-
-            log.info("ChunkedResource: Chunk processed successfully, updating manifest")
-            state_blob.upload_from_string(json.dumps(manifest_data))
-
-        except Exception as e:
-            log.error(f"ChunkedResource: Failed to process chunk: {e}")
-            raise
-
-    if manifest_data["pending_chunks"]:
-        log.error("ChunkedResource: Unprocessed chunks remain, exiting")
-        raise Exception("ChunkedResource: Unprocessed chunks remain, exiting")
-
-    log.info("ChunkedResource: All chunks processed, exiting")
-    state_blob.delete()
-
-    try:
-        bucket.delete_blob(f"{config.gcs_prefix}/{config.resource.name}")
-    except NotFound:
-        pass
+    yield config.resource(
+        manifest_data["pending_data"],
+        *args,
+        **kwargs,
+    )
