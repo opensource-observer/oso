0e64bb4c0fad20937b8821449de676706fafc17b
ccerv1 <carl.cervone@gmail.com>
2025-03-04 16:57:13 -0500

    fix: add op atlas sourced deployers

---
+++ warehouse/metrics_mesh/models/staging/op_atlas/stg_op_atlas_project_deployer.sql
@@ -0,0 +1,27 @@
+MODEL (
+  name metrics.stg_op_atlas_project_deployer,
+  dialect trino,
+  kind FULL,
+);
+
+with latest_deployers as (
+  select
+    *,
+    row_number() over (
+      partition by project_id, chain_id, deployer_address
+      order by updated_at desc
+    ) as rn
+  from @oso_source('bigquery.op_atlas.project_contract')
+)
+
+select
+  -- Translating op-atlas project_id to OSO project_id
+  @oso_id('OP_ATLAS', project_id) as project_id,
+  id as artifact_source_id,
+  @chain_id_to_chain_name(chain_id) as artifact_source,
+  NULL::TEXT as artifact_namespace,
+  deployer_address as artifact_name,
+  NULL::TEXT as artifact_url,
+  'DEPLOYER' as artifact_type
+from latest_deployers
+where rn = 1
