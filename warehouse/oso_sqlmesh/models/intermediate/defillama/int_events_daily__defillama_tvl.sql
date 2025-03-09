0f0f1c49f4d75727008544150948e6d3913bfb1d
ccerv1 <carl.cervone@gmail.com>
2025-03-08 22:30:20 -0500

    refactor: defillama tvl events

---
+++ warehouse/oso_sqlmesh/models/intermediate/defillama/int_events_daily__defillama_tvl.sql
@@ -0,0 +1,57 @@
+MODEL (
+  name oso.int_events_daily__defillama_tvl,
+  description 'Daily TVL events from DefiLlama',
+  kind INCREMENTAL_BY_TIME_RANGE (
+    time_column bucket_day,
+    batch_size 365,
+    batch_concurrency 1
+  ),
+  start '2021-10-01',
+  cron '@daily',
+  partitioned_by (DAY("bucket_day"), "event_type"),
+  grain (bucket_day, event_type, event_source, from_artifact_id, to_artifact_id)
+);
+
+@DEF(to_artifact_namespace, LOWER(all_tvl_events.chain));
+@DEF(to_artifact_name, LOWER(all_tvl_events.slug));
+@DEF(from_artifact_namespace, LOWER(all_tvl_events.chain));
+@DEF(from_artifact_name, LOWER(all_tvl_events.token));
+
+WITH all_tvl_events AS (
+  @unioned_defillama_tvl_events()
+),
+
+tvl_events_with_ids AS (
+  SELECT
+    all_tvl_events.time AS bucket_day,
+    'TVL' AS event_type,
+    'DEFILLAMA' AS event_source,
+    @oso_id(
+      all_tvl_events.time, @to_artifact_namespace, @to_artifact_name, @from_artifact_namespace, @from_artifact_name
+    ) AS event_source_id,
+    @oso_id(@to_artifact_namespace, @to_artifact_name) AS to_artifact_id,
+    @to_artifact_namespace AS to_artifact_namespace,
+    @to_artifact_name AS to_artifact_name,
+    @oso_id(@from_artifact_namespace, @from_artifact_name) AS from_artifact_id,
+    @from_artifact_namespace AS from_artifact_namespace,
+    @from_artifact_name AS from_artifact_name,
+    all_tvl_events.tvl::DOUBLE AS amount
+  FROM all_tvl_events
+)
+
+SELECT
+  abp.project_id,
+  tvl_events_with_ids.bucket_day,
+  tvl_events_with_ids.event_type,
+  tvl_events_with_ids.event_source,
+  tvl_events_with_ids.event_source_id,
+  tvl_events_with_ids.to_artifact_id,
+  tvl_events_with_ids.to_artifact_namespace,
+  tvl_events_with_ids.to_artifact_name,
+  tvl_events_with_ids.from_artifact_id,
+  tvl_events_with_ids.from_artifact_namespace,
+  tvl_events_with_ids.from_artifact_name,
+  tvl_events_with_ids.amount
+FROM tvl_events_with_ids
+LEFT OUTER JOIN oso.artifacts_by_projects_v1 AS abp
+  ON tvl_events_with_ids.to_artifact_id = abp.artifact_id
