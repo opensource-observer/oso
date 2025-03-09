5b11a1ccd28630b2e473ff1befd32e46f29d08ab
ccerv1 <carl.cervone@gmail.com>
2025-03-09 19:25:23 -0400

    fix(sqlmesh): small fix to applications staging model

--- warehouse/oso_sqlmesh/models/staging/op_atlas/stg_op_atlas_application.sql
+++ warehouse/oso_sqlmesh/models/staging/op_atlas/stg_op_atlas_application.sql
@@ -7,9 +7,10 @@ MODEL (
 
 SELECT
   @oso_id('OP_ATLAS', project_id) AS project_id, /* Translating op-atlas project_id to OSO project_id */
+  project_id::VARCHAR AS project_name,
   attestation_id::VARCHAR AS attestation_id,
   created_at::TIMESTAMP AS created_at,
   updated_at::TIMESTAMP AS updated_at,
   round_id::VARCHAR AS round_id,
   status::VARCHAR AS status
-FROM latest_contracts
+FROM @oso_source('bigquery.op_atlas.application')
