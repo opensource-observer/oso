4551a85a62bb862fce4cb7889927c3b24ec9ab73
Javier R <javier.rios-urbano@epitech.eu>
2025-03-10 16:49:52 +0100

    fix: raise error if no upstream `defillama` protocols exist

--- warehouse/oso_sqlmesh/macros/defillama/unioned_defillama_tvl_events.py
+++ warehouse/oso_sqlmesh/macros/defillama/unioned_defillama_tvl_events.py
@@ -1,7 +1,3 @@
-from oso_dagster.assets.defillama import (
-    DEFILLAMA_PROTOCOLS,
-    defillama_slug_to_name,
-)
 from sqlglot import exp
 from sqlmesh import macro
 from sqlmesh.core.macros import MacroEvaluator
@@ -10,6 +6,16 @@ from sqlmesh.core.macros import MacroEvaluator
 @macro()
 def unioned_defillama_tvl_events(evaluator: MacroEvaluator):
     """Unions all of the defi llama staging models for use in a cte"""
+    from oso_dagster.assets.defillama import (
+        DEFILLAMA_PROTOCOLS,
+        defillama_slug_to_name,
+    )
+
+    if not DEFILLAMA_PROTOCOLS:
+        raise ValueError(
+            "No upstream Defillama protocols exist, please materialize them first"
+        )
+
     return exp.union(
         *[
             exp.select("*").from_(
