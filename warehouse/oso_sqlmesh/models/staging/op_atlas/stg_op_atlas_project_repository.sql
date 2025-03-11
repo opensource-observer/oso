718c551fddcb611c3219c5c17d1118c6f925ad76
ccerv1 <carl.cervone@gmail.com>
2025-03-11 04:30:53 -0400

    fix(sqlmesh): ensure repo parts are lower cased

--- warehouse/oso_sqlmesh/models/staging/op_atlas/stg_op_atlas_project_repository.sql
+++ warehouse/oso_sqlmesh/models/staging/op_atlas/stg_op_atlas_project_repository.sql
@@ -16,8 +16,8 @@ SELECT
   @oso_id('OP_ATLAS', repos.project_id) AS project_id, /* Translating op-atlas project_id to OSO project_id */
   repos.id AS artifact_source_id,
   'GITHUB' AS artifact_source,
-  @url_parts(repos.url, 2) AS artifact_namespace,
-  @url_parts(repos.url, 3) AS artifact_name,
+  LOWER(@url_parts(repos.url, 2)) AS artifact_namespace,
+  LOWER(@url_parts(repos.url, 3)) AS artifact_name,
   repos.url AS artifact_url,
   'REPOSITORY' AS artifact_type
 /* repos.created_at, */ /* repos.updated_at, */ /* repos.verified as is_verified, */ /* repos.open_source as is_open_source, */ /* repos.contains_contracts, */ /* repos.crate as contains_crates, */ /* repos.npm_package as contains_npm */
