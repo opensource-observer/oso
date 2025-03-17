2bbe907384a369a582f56670f96a3c5787c82f0c
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2025-03-17 17:37:19 +0000

    fix(sqlmesh): get all main commits even for repos we haven't crawled

--- warehouse/oso_sqlmesh/models/staging/github/stg_github__distinct_main_commits.sql
+++ warehouse/oso_sqlmesh/models/staging/github/stg_github__distinct_main_commits.sql
@@ -1,7 +1,15 @@
 MODEL (
   name oso.stg_github__distinct_main_commits,
   description 'Gathers all github commits on the default branch of a repo that are distinct.',
-  kind FULL
+  kind INCREMENTAL_BY_TIME_RANGE (
+    time_column created_at,
+    batch_size 90,
+    batch_concurrency 3,
+    lookback 7
+  ),
+  start @github_incremental_start,
+  partitioned_by DAY(created_at),
+  dialect trino
 );
 
 /*
@@ -9,7 +17,13 @@ MODEL (
 
   We use the `MIN_BY` method here to grab the first occurrence of a given commit
   in the case of duplicated event counts (which does seem to happen with some
-  frequency)
+  frequency). We only look back 180 days to avoid pulling in too much data.
+
+  This model uses a heuristic to determine the default branch of a repository.
+  If the repository is not in the `stg_ossd__current_repositories` table, we
+  assume the default branch is either `main` or `master`. If the repository is
+  in the `stg_ossd__current_repositories` table, we use the branch specified
+  there.
 */
 SELECT
   ghc.repository_id,
@@ -25,10 +39,11 @@ SELECT
   ARG_MIN(ghc.is_distinct, ghc.created_at) AS is_distinct,
   ARG_MIN(ghc.api_url, ghc.created_at) AS api_url
 FROM oso.stg_github__commits AS ghc
-INNER JOIN oso.stg_ossd__current_repositories AS repos
+LEFT JOIN oso.stg_ossd__current_repositories AS repos
   ON ghc.repository_id = repos.id
-WHERE
-  ghc.ref = CONCAT('refs/heads/', repos.branch)
+WHERE ((repos.id IS NULL AND ghc.ref in ('refs/heads/main', 'refs/heads/master')) 
+    OR (ghc.ref = CONCAT('refs/heads/', repos.branch)))
+    AND ghc.created_at BETWEEN @start_dt - INTERVAL 180 DAY AND @end_dt
 GROUP BY
   ghc.repository_id,
   ghc.sha
