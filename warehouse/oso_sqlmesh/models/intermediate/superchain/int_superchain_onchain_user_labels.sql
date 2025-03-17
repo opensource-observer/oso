041e89592b22106a0dcc6eb42fb7b9da7001046f
ccerv1 <carl.cervone@gmail.com>
2025-03-17 11:32:37 -0400

    fix(sqlmesh): update user labels to only include farcaster

--- warehouse/oso_sqlmesh/models/intermediate/superchain/int_superchain_onchain_user_labels.sql
+++ warehouse/oso_sqlmesh/models/intermediate/superchain/int_superchain_onchain_user_labels.sql
@@ -1,23 +1,59 @@
-model(
-    name oso.int_superchain_onchain_user_labels,
-    description 'Onchain user labels (farcaster, bots)',
-    kind full,
-    enabled false,
+-- TODO: Currently only includes farcaster users.
+
+MODEL(
+  name oso.int_superchain_onchain_user_labels,
+  description 'Onchain user labels',
+  kind full
+);
+
+@DEF(chains, [
+  'base',
+  'bob',
+  'frax',
+  'ink',
+  'kroma',
+  'lisk',
+  'lyra',
+  'metal',
+  'mint',
+  'mode',
+  'optimism',
+  'orderly',
+  'polynomial',
+  'race',
+  'redstone',
+  'scroll',
+  'shape',
+  'soneium',
+  'swan',
+  'swell',
+  'unichain',
+  'worldchain',
+  'xterio',
+  'zora'
+]);
+
+WITH chains_struct AS (
+  SELECT UPPER(t.chain) as chain
+  FROM UNNEST(@chains) AS t(chain)
+),
+
+farcaster_addresses AS (
+  SELECT
+    artifacts.user_id,
+    artifacts.farcaster_id,
+    artifacts.address,
+    chains_struct.chain
+  FROM oso.int_artifacts_by_farcaster_user AS artifacts
+  CROSS JOIN chains_struct
 )
-;
 
-with
-    user_labels as (
-        select
-            artifacts.artifact_id,
-            artifacts.user_source = 'FARCASTER' as is_farcaster_user,
-            coalesce(bots.artifact_id is not null, false) as is_bot
-        from oso.int_artifacts_by_user as artifacts
-        left outer join
-            oso.int_superchain_potential_bots as bots
-            on artifacts.artifact_id = bots.artifact_id
-    )
-
-select *
-from user_labels
-where is_farcaster_user = true or is_bot = true
+SELECT
+  user_id,
+  farcaster_id,
+  chain AS artifact_source,
+  @oso_entity_id(chain, '', address) AS artifact_id,
+  '' AS artifact_namespace,
+  address AS artifact_name,
+  true AS is_farcaster_user
+FROM farcaster_addresses
