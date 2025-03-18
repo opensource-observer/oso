7c0ebd2cd6f7773d9518650b672f52e3288d624c
ccerv1 <carl.cervone@gmail.com>
2025-03-18 17:18:11 -0400

    fix(sqlmesh): resolve min_by commits

--- warehouse/oso_sqlmesh/models/staging/github/stg_github__distinct_main_commits.sql
+++ warehouse/oso_sqlmesh/models/staging/github/stg_github__distinct_main_commits.sql
@@ -17,10 +17,7 @@ MODEL (
 /*
   Gathers all github commits on the default branch of a repo that are distinct.
 
-  We use the `MIN_BY` method here to grab the first occurrence of a given commit
-  in the case of duplicated event counts (which does seem to happen with some
-  frequency). We only look back 180 days from the start date of the incremental
-  query to avoid pulling in too much data to compare for the deduplication.
+  We use ROW_NUMBER() to grab the first occurrence of a given commit in the case of duplicated event counts (which does seem to happen with some frequency). We only look back 180 days from the start date of the incremental query to avoid pulling in too much data to compare for the deduplication.
 
   This model uses a heuristic to determine the default branch of a repository.
   If the repository is not in the `stg_ossd__current_repositories` table, we
@@ -28,25 +25,46 @@ MODEL (
   in the `stg_ossd__current_repositories` table, we use the branch specified
   there.
 */
-SELECT
-  ghc.repository_id,
-  ghc.sha,
-  MIN(ghc.created_at) AS created_at,
-  ARG_MIN(ghc.repository_name, ghc.created_at) AS repository_name,
-  ARG_MIN(ghc.push_id, ghc.created_at) AS push_id,
-  ARG_MIN(ghc.ref, ghc.created_at) AS ref,
-  ARG_MIN(ghc.actor_id, ghc.created_at) AS actor_id,
-  ARG_MIN(ghc.actor_login, ghc.created_at) AS actor_login,
-  ARG_MIN(ghc.author_email, ghc.created_at) AS author_email,
-  ARG_MIN(ghc.author_name, ghc.created_at) AS author_name,
-  ARG_MIN(ghc.is_distinct, ghc.created_at) AS is_distinct,
-  ARG_MIN(ghc.api_url, ghc.created_at) AS api_url
-FROM oso.stg_github__commits AS ghc
-LEFT JOIN oso.stg_ossd__current_repositories AS repos
-  ON ghc.repository_id = repos.id
-WHERE ((repos.id IS NULL AND ghc.ref in ('refs/heads/main', 'refs/heads/master')) 
-    OR (ghc.ref = CONCAT('refs/heads/', repos.branch)))
+WITH deduped_commits AS (
+  SELECT
+    ghc.repository_id,
+    ghc.sha,
+    ghc.created_at,
+    ghc.repository_name,
+    ghc.push_id,
+    ghc.ref,
+    ghc.actor_id,
+    ghc.actor_login,
+    ghc.author_email,
+    ghc.author_name,
+    ghc.is_distinct,
+    ghc.api_url,
+    ROW_NUMBER() OVER (
+      /* TODO: consider using sha instead of push_id */
+      PARTITION BY ghc.repository_id, ghc.push_id
+      ORDER BY ghc.created_at ASC
+    ) AS rn
+  FROM oso.stg_github__commits AS ghc
+  LEFT JOIN oso.stg_ossd__current_repositories AS repos
+    ON ghc.repository_id = repos.id
+  WHERE (
+    (repos.id IS NULL AND ghc.ref IN ('refs/heads/main', 'refs/heads/master'))
+    OR (ghc.ref = CONCAT('refs/heads/', repos.branch))
+  )
     AND ghc.created_at BETWEEN @start_dt - INTERVAL @deduplication_window DAY AND @end_dt
-GROUP BY
-  ghc.repository_id,
-  ghc.sha
+)
+SELECT
+  repository_id,
+  sha,
+  created_at,
+  repository_name,
+  push_id,
+  ref,
+  actor_id,
+  actor_login,
+  author_email,
+  author_name,
+  is_distinct,
+  api_url
+FROM deduped_commits
+WHERE rn = 1
