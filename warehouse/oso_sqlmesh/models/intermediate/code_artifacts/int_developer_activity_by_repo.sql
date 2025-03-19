1c3ca0f3d40b1f1841f35875cb126b036065354f
ccerv1 <carl.cervone@gmail.com>
2025-03-19 05:43:42 -0400

    fix(sqlmesh): bring back developer_activity_by_repo model

--- warehouse/oso_sqlmesh/models/intermediate/code_artifacts/int_developer_activity_by_repo.sql
+++ warehouse/oso_sqlmesh/models/intermediate/code_artifacts/int_developer_activity_by_repo.sql
@@ -1,22 +1,22 @@
 MODEL (
   name oso.int_developer_activity_by_repo,
-  description 'Summarizes developer activity by repository',
+  description 'Summarizes developer activity by repository in OSSD',
   kind FULL,
-  enabled false,
 );
 
-WITH developers AS (
-  SELECT DISTINCT
+WITH deveopers AS (
+  SELECT
     users.user_id AS developer_id,
-    users.display_name AS developer_name,
+    users.github_username AS developer_name,
     events.to_artifact_id AS repo_artifact_id
-  FROM oso.int_events__github AS events
-  INNER JOIN oso.int_users AS users
+  FROM oso.int_events_filtered__github AS events
+  JOIN oso.int_github_users AS users
     ON events.from_artifact_id = users.user_id
   WHERE
-    events.event_type = 'COMMIT_CODE'
-    AND NOT REGEXP_MATCHES(LOWER(users.display_name), '(^|[^a-z0-9_])bot([^a-z0-9_]|$)|bot$')
+    NOT users.is_bot
+    AND events.event_type = 'COMMIT_CODE'
 )
+
 SELECT
   events.to_artifact_id AS repo_artifact_id,
   developers.developer_id,
@@ -24,8 +24,8 @@ SELECT
   events.event_type,
   MIN(events.time) AS first_event,
   MAX(events.time) AS last_event,
-  COUNT(DISTINCT events.time) AS total_events
-FROM oso.int_events__github AS events
+  SUM(events.amount) AS total_events
+FROM oso.int_events_filtered__github AS events
 INNER JOIN developers
   ON events.from_artifact_id = developers.developer_id
 GROUP BY
