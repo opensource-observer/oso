5f530283f43849362c82feb789e6f56c50411c8f
ccerv1 <carl.cervone@gmail.com>
2025-03-19 05:43:11 -0400

    feat(sqlmesh): add github user model

---
+++ warehouse/oso_sqlmesh/models/intermediate/users/int_github_users.sql
@@ -0,0 +1,24 @@
+MODEL (
+  name oso.int_github_users,
+  kind FULL,
+  description "GitHub users linked to repositories in OSSD"
+);
+
+@DEF(bot_regex, '(^|[^a-z0-9_])bot([^a-z0-9_]|$)|bot$');
+
+WITH users AS (
+  SELECT DISTINCT
+    event_source,
+    from_artifact_namespace AS github_namespace,
+    from_artifact_name AS github_username,
+    from_artifact_source_id AS github_id
+  FROM oso.int_events_filtered__github
+)
+
+SELECT
+  @oso_entity_id(event_source, github_namespace, github_username) AS user_id,
+  github_id,
+  github_username,
+  'https://github.com/' || github_username AS url,
+  REGEXP_MATCHES(github_username, @bot_regex) AS is_bot
+FROM users
