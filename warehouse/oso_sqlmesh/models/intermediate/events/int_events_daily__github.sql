dc39010e09ac45eaa2ea0e1b8866ad7acebeb0f8
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2025-03-19 10:09:35 -0400

    feat(sqlmesh): reintroduce enriched github int models (#3330)
    * feat(sqlmesh): int_events_daily__github model
    
    * fix(sqlmesh): add date filters to other int_events_daily models
    
    * feat(sqlmesh): add filtered github event model
    
    * feat(sqlmesh): add github user model
    
    * fix(sqlmesh): bring back developer_activity_by_repo model
    
    * chore: move int_users
    
    * fix: typo
    
    * chore(sqlmesh): re-enable releases and repo models

--- warehouse/oso_sqlmesh/models/intermediate/events/int_events_daily__github.sql
+++ warehouse/oso_sqlmesh/models/intermediate/events/int_events_daily__github.sql
@@ -1,14 +1,13 @@
--- 4337 events (currently only from the superchain dataset)
 MODEL (
-  name oso.int_events_daily__4337,
+  name oso.int_events_daily__github,
   kind INCREMENTAL_BY_TIME_RANGE (
     time_column bucket_day,
-    batch_size 180,
+    batch_size 365,
     batch_concurrency 1
   ),
-  start @blockchain_incremental_start,
+  start @github_incremental_start,
   cron '@daily',
-  partitioned_by (DAY("bucket_day"), "event_type", "event_source"),
+  partitioned_by (DAY("bucket_day"), "event_type"),
   grain (bucket_day, event_type, event_source, from_artifact_id, to_artifact_id)
 );
 
@@ -18,13 +17,12 @@ SELECT
   to_artifact_id::VARCHAR AS to_artifact_id,
   event_source::VARCHAR,
   event_type::VARCHAR,
-  SUM(userop_gas_used * userop_gas_price)::DOUBLE AS amount,
-  COUNT(*)::DOUBLE AS "count"
-FROM oso.int_events__4337 as events
+  SUM(amount::DOUBLE)::DOUBLE AS amount
+FROM oso.int_events__github as events
 WHERE time BETWEEN @start_dt AND @end_dt
 GROUP BY
+  DATE_TRUNC('DAY', time::DATE),
   from_artifact_id,
   to_artifact_id,
   event_source,
-  event_type,
-  DATE_TRUNC('DAY', time::DATE)
+  event_type
