998fe2b5b9c30b671626f9a9e5a28aba70c6d80e
ccerv1 <carl.cervone@gmail.com>
2025-03-19 05:01:01 -0400

    feat(sqlmesh): int_events_daily__github model

---
+++ warehouse/oso_sqlmesh/models/intermediate/events/int_events_daily__github.sql
@@ -0,0 +1,28 @@
+MODEL (
+  name oso.int_events_daily__github,
+  kind INCREMENTAL_BY_TIME_RANGE (
+    time_column bucket_day,
+    batch_size 365,
+    batch_concurrency 1
+  ),
+  start @github_incremental_start,
+  cron '@daily',
+  partitioned_by (DAY("bucket_day"), "event_type"),
+  grain (bucket_day, event_type, event_source, from_artifact_id, to_artifact_id)
+);
+
+SELECT
+  DATE_TRUNC('DAY', time::DATE) AS bucket_day,
+  from_artifact_id::VARCHAR AS from_artifact_id,
+  to_artifact_id::VARCHAR AS to_artifact_id,
+  event_source::VARCHAR,
+  event_type::VARCHAR,
+  SUM(amount::DOUBLE)::DOUBLE AS amount
+FROM oso.int_events__github as events
+WHERE time BETWEEN @start_dt AND @end_dt
+GROUP BY
+  DATE_TRUNC('DAY', time::DATE),
+  from_artifact_id,
+  to_artifact_id,
+  event_source,
+  event_type
