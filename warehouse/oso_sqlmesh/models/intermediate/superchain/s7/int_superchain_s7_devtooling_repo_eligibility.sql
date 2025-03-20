3c1896b4d9773fef034c092c080a1fc57df8c597
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2025-03-20 10:05:36 -0400

    chore(sqlmesh): delete old models now that job is finished (#3340)

--- warehouse/oso_sqlmesh/models/intermediate/superchain/s7/int_superchain_s7_devtooling_repo_eligibility.sql
+++
@@ -1,9 +0,0 @@
-MODEL (
-  name oso.int_superchain_s7_devtooling_repo_eligibility,
-  description 'Temp table - will delete after S7 migration',
-  dialect trino,
-  kind full,
-);
-
-SELECT *
-FROM oso.int_superchain_s7_devtooling_repositories
