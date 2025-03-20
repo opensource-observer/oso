bf84f6eda376300218e2fe88b0666c7311a88594
ccerv1 <carl.cervone@gmail.com>
2025-03-20 09:56:17 -0400

    chore(sqlmesh): delete old models now that job is finished

--- warehouse/oso_sqlmesh/models/intermediate/superchain/s7/int_superchain_s7_project_to_developer_graph.sql
+++
@@ -1,9 +0,0 @@
-MODEL (
-  name oso.int_superchain_s7_project_to_developer_graph,
-  description 'Temp table - will delete after S7 migration',
-  dialect trino,
-  kind full,
-);
-
-SELECT *
-FROM oso.int_superchain_s7_devtooling_devs_to_projects_graph
