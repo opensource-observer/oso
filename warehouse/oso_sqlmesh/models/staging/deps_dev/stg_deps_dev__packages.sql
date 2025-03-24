575da31cf014cf968fac2dfc1637b6aa294d76ff
ccerv1 <carl.cervone@gmail.com>
2025-03-23 20:59:52 -0400

    fix: create temp table until source is recognized

--- warehouse/oso_sqlmesh/models/staging/deps_dev/stg_deps_dev__packages.sql
+++ warehouse/oso_sqlmesh/models/staging/deps_dev/stg_deps_dev__packages.sql
@@ -10,13 +10,24 @@ MODEL (
   partitioned_by (DAY(snapshot_at), package_artifact_source),
 );
 
+-- SELECT
+--   "SnapshotAt"::TIMESTAMP as snapshot_at,
+--   UPPER("System")::VARCHAR as package_artifact_source,
+--   LOWER("ProjectName")::VARCHAR as package_github_owner_and_repo,
+--   LOWER("Name")::VARCHAR as package_name,
+--   LOWER("Version")::VARCHAR as package_version
+-- FROM @oso_source('bigquery.deps_dev_v1.package_version_to_project')
+-- WHERE "ProjectName" is not null
+--   AND "ProjectType" = 'GITHUB'
+--   AND "RelationType" = 'SOURCE_REPO_TYPE'
+
+
 SELECT
-  "SnapshotAt"::TIMESTAMP as snapshot_at,
-  UPPER("System")::VARCHAR as package_artifact_source,
-  LOWER("ProjectName")::VARCHAR as package_github_owner_and_repo,
-  LOWER("Name")::VARCHAR as package_name,
-  LOWER("Version")::VARCHAR as package_version
-FROM @oso_source('bigquery.deps_dev_v1.package_version_to_project')
-WHERE "ProjectName" is not null
-  AND "ProjectType" = 'GITHUB'
-  AND "RelationType" = 'SOURCE_REPO_TYPE'
+  DATE('2025-03-24') as snapshot_at,
+  package_artifact_source,
+  package_github_owner_and_repo,
+  package_name,
+  package_version
+FROM VALUES
+  ('NPM', 'opensource-observer/oso', '@example/oso', '0.0.16')
+  AS t(package_artifact_source, package_github_owner_and_repo, package_name, package_version)
