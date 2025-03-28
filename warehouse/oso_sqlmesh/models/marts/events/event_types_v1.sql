743697e34fe780d79f66c2b05bb1bce634a1b598
Reuven Gonzales <reuven@karibalabs.co>
2025-03-27 17:26:50 -0700

    fix: remove int_events (#3439)
    * fix: remove int_events
    
    * fix: remove all int_events references

--- warehouse/oso_sqlmesh/models/marts/events/event_types_v1.sql
+++ warehouse/oso_sqlmesh/models/marts/events/event_types_v1.sql
@@ -6,6 +6,24 @@ MODEL (
   )
 );
 
+WITH all_event_types AS (
+  SELECT
+    event_type
+  FROM oso.int_events_daily__github
+  UNION ALL
+  SELECT
+    event_type
+  FROM oso.int_events_daily__blockchain
+  UNION ALL
+  SELECT
+    event_type
+  FROM oso.int_events_daily__funding
+  UNION ALL
+  SELECT
+    event_type
+  FROM oso.int_events_daily__dependencies
+)
+
 SELECT DISTINCT
   events.event_type
-FROM oso.int_events AS events
+FROM oso.int_events_daily__github AS events
