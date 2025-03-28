c575e8502674e700bb186944b9bed8b65635a06d
Raymond Cheng <me@raymondcheng.net>
2025-03-28 12:57:05 -0700

    fix: revert the models_v0 column names (#3444)

--- warehouse/oso_sqlmesh/models/marts/sqlmesh/models_v0.sql
+++ warehouse/oso_sqlmesh/models/marts/sqlmesh/models_v0.sql
@@ -12,14 +12,14 @@ MODEL (
 WITH all_timeseries_metrics_by_project AS (
   SELECT
     @oso_id('OSO', 'sqlmesh', model_name) AS model_id,
-    model_name as name,
-    rendered_sql as sql,
+    model_name,
+    rendered_sql,
     rendered_at,
   FROM oso.stg_sqlmesh__rendered_models
 )
 SELECT
     model_id::TEXT,
-    name::TEXT,
-    sql::TEXT,
+    model_name::TEXT,
+    rendered_sql::TEXT,
     rendered_at::TIMESTAMP
 FROM all_timeseries_metrics_by_project
