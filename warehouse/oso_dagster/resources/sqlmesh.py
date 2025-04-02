6db6e492d51eb974f184367cea32a38ea610264c
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2025-04-02 19:58:23 +0000

    hotfix: sqlmesh tag parser typo

--- warehouse/oso_dagster/resources/sqlmesh.py
+++ warehouse/oso_dagster/resources/sqlmesh.py
@@ -61,7 +61,7 @@ class PrefixedSQLMeshTranslator(SQLMeshDagsterTranslator):
         """
         tags: t.Dict[str, str] = {}
         for tag in model.tags:
-            if ":" in tag:
+            if "=" in tag:
                 key, value = tag.split("=")
                 tags[key] = value
             else:
