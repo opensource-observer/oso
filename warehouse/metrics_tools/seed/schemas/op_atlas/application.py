f6759ae5a24a60138c4c3b424dbbdd233d6be052
Ícaro Guerra <icaro@plasmic.app>
2025-04-03 19:11:09 -0300

    feat(seed): start implementation of seed data in python (#3482)
    * feat(seed): start implementation of seed data in python
    
    * feat(seed): add projects seed data and make sql parser work with type references
    
    * feat(seed): stop using print and add some logs
    
    * feat(seed): add more python seeds
    
    * feat(seed): create Column type for less verbosity
    
    * fix(seed): correctly parse json columns
    
    * fix(seed): fix column_name not working when starting with underscore
    
    * feat(seed): add last tables in python seed
    
    * feat(seed): add cli command for running/seeding docker and testing sqlmesh
    
    * nit: remove comments
    
    * feat(seed): create SeedConfig file for better generalization
    
    * refactor(seed): use SeedConfig class for discovery
    
    * feat(seed): add missing seed data for bigquery.sqlmesh
    
    * refactor(cli): use python-on-whales to manage docker cli

---
+++ warehouse/metrics_tools/seed/schemas/op_atlas/application.py
@@ -0,0 +1,51 @@
+from datetime import datetime, timedelta
+
+from metrics_tools.seed.types import Column, SeedConfig
+from pydantic import BaseModel
+
+
+class Application(BaseModel):
+    id: str = Column("VARCHAR")
+    status: str = Column("VARCHAR")
+    attestation_id: str = Column("VARCHAR")
+    created_at: datetime = Column("TIMESTAMP(6) WITH TIME ZONE")
+    updated_at: datetime = Column("TIMESTAMP(6) WITH TIME ZONE")
+    round_id: str = Column("VARCHAR")
+    project_id: str = Column("VARCHAR")
+    category_id: str | None = Column("VARCHAR")
+    dlt_load_id: str = Column("VARCHAR", "_dlt_load_id")
+    dlt_id: str = Column("VARCHAR", "_dlt_id")
+
+
+seed = SeedConfig(
+    catalog="bigquery",
+    schema="op_atlas",
+    table="application",
+    base=Application,
+    rows=[
+        Application(
+            id="1",
+            status="active",
+            attestation_id="att1",
+            created_at=datetime.now() - timedelta(days=2),
+            updated_at=datetime.now() - timedelta(days=2),
+            round_id="round1",
+            project_id="proj1",
+            category_id="cat1",
+            dlt_load_id="load1",
+            dlt_id="dlt1",
+        ),
+        Application(
+            id="2",
+            status="inactive",
+            attestation_id="att2",
+            created_at=datetime.now() - timedelta(days=1),
+            updated_at=datetime.now() - timedelta(days=1),
+            round_id="round2",
+            project_id="proj2",
+            category_id="cat2",
+            dlt_load_id="load2",
+            dlt_id="dlt2",
+        ),
+    ],
+)
