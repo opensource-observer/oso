73916ec66aacae82fb2e22f0963bf3222d24e17e
Raymond Cheng <me@raymondcheng.net>
2025-04-10 19:11:24 -0700

    update staging model for deps_dev

--- warehouse/oso_sqlmesh/models/staging/deps-dev/stg_deps_dev__packages.sql
+++ warehouse/oso_sqlmesh/models/staging/deps-dev/stg_deps_dev__packages.sql
@@ -1,13 +1,13 @@
 MODEL (
   name oso.stg_deps_dev__packages,
   kind INCREMENTAL_BY_TIME_RANGE (
-    time_column SnapshotAt,
+    time_column snapshot_at,
     batch_size 90,
     batch_concurrency 3,
     lookback 7
   ),
   start @deps_dev_incremental_start,
-  partitioned_by DAY(SnapshotAt),
+  partitioned_by DAY(snapshot_at),
   dialect duckdb,
 );
 
@@ -15,19 +15,20 @@ MODEL (
 
 with base as (
   select
-    `SnapshotAt`,
-    `System`,
-    `ProjectName`,
-    `Name`,
-    `Version`,
-    `RelationType`
-  from bigquery_public_data.deps_dev_v1.PackageVersionToProject
+    "SnapshotAt" as snapshot_at,
+    "System" as system,
+    "ProjectName" as project_name,
+    "ProjectType" as project_type,
+    "Name" as name,
+    "Version" as version,
+    "RelationType" as relationship_type
+  from @oso_source('bigquery_public_data.deps_dev_v1.PackageVersionToProject')
   where
-    `ProjectName` is not null
-    and `ProjectType` = 'GITHUB'
-    and `SnapshotAt` >= @oldest_snapshot_date
-    --and `RelationType` = 'SOURCE_REPO_TYPE'
+    project_name is not null
+    and project_type = 'GITHUB'
+    and snapshot_at >= @oldest_snapshot_date
+    --and RelationType = 'SOURCE_REPO_TYPE'
 )
 
 select * from base
-where `SnapshotAt` between @start_dt and @end_dt
+where snapshot_at between @start_dt and @end_dt
