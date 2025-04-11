457c44616271e58666a357959c016034d18003b1
Raymond Cheng <me@raymondcheng.net>
2025-04-10 19:11:04 -0700

    feat: add seed data for deps_dev

---
+++ warehouse/metrics_tools/seed/schemas/deps_dev/PackageVersionToProject.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+
+from metrics_tools.seed.types import Column, SeedConfig
+from pydantic import BaseModel
+
+#select
+#    `SnapshotAt`,
+#    `System`,
+#    `ProjectName`,
+#    `Name`,
+#    `Version`,
+#    `RelationType`
+#  from bigquery_public_data.deps_dev_v1.PackageVersionToProject
+#  where
+#    `ProjectName` is not null
+#    and `ProjectType` = 'GITHUB'
+#    and `SnapshotAt` >= @oldest_snapshot_date
+#    --and `RelationType` = 'SOURCE_REPO_TYPE'
+#
+
+class PackageVersionToProject(BaseModel):
+    SnapshotAt: datetime | None = Column("TIMESTAMP")
+    System: str | None = Column("VARCHAR")
+    ProjectName: str | None = Column("VARCHAR")
+    ProjectType: str | None = Column("VARCHAR")
+    Name: str | None = Column("VARCHAR")
+    Version: str | None = Column("VARCHAR")
+    RelationType: str | None = Column("VARCHAR")
+
+seed = SeedConfig(
+    catalog="bigquery_public_data",
+    schema="deps_dev_v1",
+    table="PackageVersionToProject",
+    base=PackageVersionToProject,
+    rows=[
+        PackageVersionToProject(
+            SnapshotAt=datetime(2025, 3, 2),
+            System="NPM",
+            ProjectName="react",
+            ProjectType="GITHUB",
+            Name="react",
+            Version="18.0.1",
+            RelationType=None,
+        ),
+        PackageVersionToProject(
+            SnapshotAt=datetime(2025, 3, 2),
+            System="NPM",
+            ProjectName="ethers",
+            ProjectType="GITHUB",
+            Name="ethers",
+            Version="6.13.1",
+            RelationType=None,
+        ),
+        PackageVersionToProject(
+            SnapshotAt=datetime(2025, 3, 2),
+            System="NPM",
+            ProjectName="viem",
+            ProjectType="GITHUB",
+            Name="viem",
+            Version="2.26.1",
+            RelationType=None,
+        ),
+    ],
+)
+
