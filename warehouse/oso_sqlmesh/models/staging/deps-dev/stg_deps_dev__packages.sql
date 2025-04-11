9ba375a050fa3893138f07908760d0db108a31b4
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2025-04-12 02:21:56 +0900

    fix: stg_deps_dev__packages bad where clause

--- warehouse/oso_sqlmesh/models/staging/deps-dev/stg_deps_dev__packages.sql
+++ warehouse/oso_sqlmesh/models/staging/deps-dev/stg_deps_dev__packages.sql
@@ -8,10 +8,10 @@ MODEL (
   ),
   start @deps_dev_incremental_start,
   partitioned_by DAY(snapshot_at),
-  dialect duckdb,
+  dialect duckdb
 );
 
-@DEF(oldest_snapshot_date, '2025-03-01');
+@DEF(oldest_snapshot_date, DATE '2025-03-01');
 
 with base as (
   select
@@ -24,11 +24,19 @@ with base as (
     "RelationType" as relationship_type
   from @oso_source('bigquery_public_data.deps_dev_v1.PackageVersionToProject')
   where
-    and project_name is not null
+    project_name is not null
     and project_type = 'GITHUB'
     and snapshot_at >= @oldest_snapshot_date
     --and RelationType = 'SOURCE_REPO_TYPE'
 )
 
-select * from base
+select 
+  snapshot_at::TIMESTAMP,
+  system::TEXT,
+  project_name::TEXT,
+  project_type::TEXT,
+  name::TEXT,
+  version::TEXT,
+  relationship_type::TEXT
+from base
 where snapshot_at between @start_dt and @end_dt
