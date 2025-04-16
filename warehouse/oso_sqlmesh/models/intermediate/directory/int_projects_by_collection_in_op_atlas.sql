e31c9f600ff29909e65b7fd7f041669801311aeb
ccerv1 <carl.cervone@gmail.com>
2025-04-15 22:40:20 -0400

    fix(sqlmesh): define measurement periods explicitly

--- warehouse/oso_sqlmesh/models/intermediate/directory/int_projects_by_collection_in_op_atlas.sql
+++ warehouse/oso_sqlmesh/models/intermediate/directory/int_projects_by_collection_in_op_atlas.sql
@@ -10,19 +10,32 @@ MODEL (
 @DEF(collection_source, 'OP_ATLAS');
 @DEF(collection_namespace, 'retro-funding');
 @DEF(flagged_for_removal, [
-  '0x8bfdc42f26bf691d378d2073ae509c46b85c0eed8db8abc6987b6725dd0d056a'
+  '0x8bfdc42f26bf691d378d2073ae509c46b85c0eed8db8abc6987b6725dd0d056a',
+  '0x10da43868e7439419af2859f2539ed8b355a0f330bd05a6d028ddff8fd4a03d8'
 ]);
 
-WITH app AS (
+WITH measurement_periods AS (
+  SELECT
+    period_number,
+    cutoff_date
+  FROM (
+    VALUES
+      ('1', DATE '2025-03-11'),
+      ('2', DATE '2025-04-11')
+  ) AS t(period_number, cutoff_date)
+),
+
+app AS (
   SELECT DISTINCT
     project_id,
     project_name,
-    round_id
+    round_id,
+    created_at
   FROM oso.stg_op_atlas_application
   WHERE
     round_id IN ('7', '8')
     AND status = 'submitted'
-    AND created_at BETWEEN DATE '2025-02-01' AND DATE '2025-03-11'
+    AND created_at >= DATE '2025-02-01'
     AND project_name NOT IN (
       SELECT project_name
       FROM UNNEST(@flagged_for_removal) AS t(project_name)
@@ -33,7 +46,7 @@ projects_by_collection AS (
   SELECT DISTINCT
     @collection_source AS collection_source,
     @collection_namespace AS collection_namespace,
-    CONCAT(app.round_id, '-', '1') AS collection_name,
+    CONCAT(app.round_id, '-', mp.period_number) AS collection_name,
     CASE
       WHEN app.round_id = '7'
       THEN 'Retro Funding S7: Developer Tooling'
@@ -46,8 +59,10 @@ projects_by_collection AS (
     projects.project_name,
     app.project_id
   FROM app
+  CROSS JOIN measurement_periods mp
   JOIN oso.stg_op_atlas_project AS projects
     ON app.project_id = projects.project_id
+  WHERE app.created_at <= mp.cutoff_date
 )
 SELECT
   @oso_entity_id(collection_source, collection_namespace, collection_name)
