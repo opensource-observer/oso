7b1e1ea2bf2859311b78c4a7665f2a2819020ca0
ccerv1 <carl.cervone@gmail.com>
2025-04-24 11:59:26 -0400

    chore: add seed models

--- warehouse/oso_sqlmesh/models/staging/gitcoin/stg_gitcoin__all_matching.sql
+++ warehouse/oso_sqlmesh/models/staging/gitcoin/stg_gitcoin__all_matching.sql
@@ -1,29 +1,22 @@
 MODEL (
   name oso.stg_gitcoin__all_matching,
-  kind INCREMENTAL_BY_TIME_RANGE (
-    time_column bucket_day,
-    batch_size 365,
-    batch_concurrency 1
-  ),
-  start @github_incremental_start,
+  description "Staging table for Gitcoin matching data",
+  kind full,
   cron '@daily',
-  partitioned_by ("round_id"),
-  grain (bucket_day, project_recipient_address, gitcoin_project_id, gitcoin_round_id, chain_id)
+  dialect trino,
+  grain (sample_date, project_recipient_address, gitcoin_project_id, gitcoin_round_id, chain_id)
 );
 
 SELECT distinct
   'MatchFunding' as gitcoin_data_source,
-  DATE_TRUNC('DAY', timestamp::DATE) AS bucket_day,
+  timestamp::DATE AS sample_date,
   round_id::VARCHAR AS gitcoin_round_id,
   round_num::INTEGER AS round_number,
-  cast(null)::VARCHAR as round_name,
+  round_name::VARCHAR as round_name,
   chain_id::INTEGER AS chain_id,
   project_id::VARCHAR AS gitcoin_project_id,
   trim(title)::VARCHAR AS project_application_title,
   lower(recipient_address)::VARCHAR AS project_recipient_address,
-  cast(null as string) as donor_address,
-  cast(null as string) as transaction_hash,
-  match_amount_in_usd::FLOAT AS amount_in_usd
+  match_amount_in_usd::DOUBLE AS amount_in_usd
 FROM @oso_source('bigquery.gitcoin.all_matching')
-WHERE timestamp BETWEEN @start_dt AND @end_dt
-and match_amount_in_usd > 0
+WHERE match_amount_in_usd > 0
