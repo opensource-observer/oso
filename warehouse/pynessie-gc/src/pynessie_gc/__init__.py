5513b6988484228ce57394c98b44ac7f94181561
Ícaro Guerra <icaro@plasmic.app>
2025-04-30 14:31:44 -0300

    feat(pynessie-gc): create script to remove orphaned files from trino/nessie

---
+++ warehouse/pynessie-gc/src/pynessie_gc/__init__.py
@@ -0,0 +1,70 @@
+import asyncio
+import itertools
+import os
+
+import aiotrino
+import gcsfs
+
+GOOGLE_PROJECT = os.environ.get("GOOGLE_PROJECT", "opensource-observer")
+BUCKET = os.environ.get("BUCKET_NAME", "oso-iceberg-usc1")
+TRINO_HOST = os.environ.get("TRINO_HOST", "localhost")
+TRINO_PORT = os.environ.get("TRINO_PORT", "8080")
+TRINO_SCHEMA = os.environ.get("TRINO_SCHEMA", "sqlmesh__oso")
+
+
+def get_iceberg_table_name(path: str) -> str:
+    parts = path.split("/")
+    table_name_with_uuid = parts[-1]
+    parts = table_name_with_uuid.split("__")
+    if len(parts) > 1:  # If there are double underscores
+        last_part = parts[-1]
+        # Split the last part by the last single underscore
+        if "_" in last_part:
+            last_part_without_uuid = last_part.rsplit("_", 1)[0]
+            parts[-1] = last_part_without_uuid
+    table_name = "__".join(parts)
+    return table_name
+
+
+async def delete_iceberg_table(storage_client: gcsfs.GCSFileSystem, path: str):
+    folder_name = path.split("/")[-1]
+    print(f"Deleting {folder_name}")
+    await storage_client._rm(path, recursive=True)
+
+
+async def _main():
+    trino_client = aiotrino.dbapi.connect(
+        host=TRINO_HOST, port=TRINO_PORT, user="trino", request_timeout=180
+    )
+    storage_client = gcsfs.GCSFileSystem(project=GOOGLE_PROJECT, asynchronous=True)
+
+    try:
+        cur = await trino_client.cursor()
+        await cur.execute(f"SHOW TABLES FROM iceberg.{TRINO_SCHEMA}")
+        tables = await cur.fetchall()
+        tables_set = set([t[0] for t in tables])
+        await cur.close()
+
+        folder_paths = await storage_client._ls(f"{BUCKET}/warehouse/{TRINO_SCHEMA}/")
+        deleted_paths = [
+            path
+            for path in folder_paths
+            if isinstance(path, str) and get_iceberg_table_name(path) not in tables_set
+        ]
+
+        print(f"Found {len(deleted_paths)}/{len(folder_paths)} deleted paths")
+        await asyncio.gather(
+            *map(
+                delete_iceberg_table,
+                itertools.repeat(storage_client),
+                (p for p in deleted_paths),
+            )
+        )
+    finally:
+        await trino_client.close()
+        if storage_client.session:
+            await storage_client.session.close()
+
+
+def main() -> None:
+    asyncio.run(_main())
