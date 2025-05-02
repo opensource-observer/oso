2fdbbee7474585211587a85fb8149437dc786d42
Raymond Cheng <me@raymondcheng.net>
2025-05-01 21:25:38 -0700

    feat: track API calls by user and models (#3685)
    * add node sql parser dependency
    
    * add ts-jest dependency
    
    * Add jest config
    
    * add jest.config.js to tsconfig.json
    
    * add parsing logic to get table names from a SQL statement
    
    * add tests for sql parsing
    
    * Add an enum for posthog events
    
    * add withPostHog wrapper function
    
    * Use the event enum for supabase_write events
    
    * Instrument graphql endpoint to track api calls
    
    * Instrument /sql to track calls
    
    * distinguish AnonUser and AuthUser
    
    * save the api key name in the user details
    
    * graphql track reports api key name
    
    * /sql reports api key name

--- apps/frontend/app/api/v1/sql/route.ts
+++ apps/frontend/app/api/v1/sql/route.ts
@@ -1,7 +1,9 @@
 import { NextResponse, type NextRequest } from "next/server";
-
 import { getTrinoClient, TrinoError } from "../../../../lib/clients/trino";
 import type { QueryResult, Iterator } from "trino-client";
+import { withPostHog } from "../../../../lib/clients/posthog";
+import { getTableNamesFromSql } from "../../../../lib/parsing";
+import { AuthUser } from "../../../../lib/auth/auth";
 //import { logger } from "../../../lib/logger";
 
 // Next.js route control
@@ -50,6 +52,18 @@ export async function POST(request: NextRequest) {
     return makeErrorResponse("Please provide a 'query' parameter", 400);
   }
   try {
+    await withPostHog(async (posthog, user: AuthUser) => {
+      posthog.capture({
+        distinctId: user.userId,
+        event: "api_call",
+        properties: {
+          type: "sql",
+          models: getTableNamesFromSql(query),
+          query: query,
+          apiKeyName: user.keyName,
+        },
+      });
+    }, request);
     const [firstRow, rows] = await doQuery(query);
     const readableStream = mapToReadableStream(firstRow, rows, format);
     return new NextResponse(readableStream, {
