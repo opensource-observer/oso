ec3b0a9a40b22e457a2529f5acf019feb9aac535
ccerv1 <carl.cervone@gmail.com>
2025-05-03 09:34:17 -0400

    fix(sqlmesh): make funding awarded model full instead of incremental

--- warehouse/oso_sqlmesh/models/intermediate/events/int_events__funding_awarded.sql
+++ warehouse/oso_sqlmesh/models/intermediate/events/int_events__funding_awarded.sql
@@ -1,13 +1,8 @@
 MODEL (
   name oso.int_events__funding_awarded,
+  description 'Intermediate table for funding awarded events',
   dialect trino,
-  kind INCREMENTAL_BY_TIME_RANGE (
-    time_column time,
-    batch_size 90,
-    batch_concurrency 1
-  ),
-  start @funding_incremental_start,
-  cron '@daily',
+  kind full,
   partitioned_by ( DAY("time"), "event_type" ),
   grain ( time, event_source, from_artifact_name, to_artifact_name ),
   audits (
@@ -16,14 +11,16 @@ MODEL (
 );
 
 SELECT
-  f.funding_date AS time,
-  f.funding_source AS event_source,
+  funding_date AS time,
+  funding_source AS event_source,
   'FUNDING_AWARDED' AS event_type,
-  f.to_project_name AS to_artifact_name,
-  f.funding_namespace AS to_artifact_namespace,
-  @oso_entity_id(f.funding_source, f.funding_namespace, f.to_project_name) as to_artifact_id,
-  f.grant_pool_name AS from_artifact_name,
-  f.from_funder_name AS from_artifact_namespace,
-  @oso_entity_id(f.funding_source, f.from_funder_name, f.grant_pool_name) as from_artifact_id,
-  f.amount AS amount
-FROM oso.stg_ossd__current_funding AS f
+  to_project_name AS to_artifact_name,
+  funding_namespace AS to_artifact_namespace,
+  @oso_entity_id(funding_source, funding_namespace, to_project_name)
+    AS to_artifact_id,
+  grant_pool_name AS from_artifact_name,
+  from_funder_name AS from_artifact_namespace,
+  @oso_entity_id(funding_source, from_funder_name, grant_pool_name)
+    AS from_artifact_id,
+  amount AS amount
+FROM oso.stg_ossd__current_funding
