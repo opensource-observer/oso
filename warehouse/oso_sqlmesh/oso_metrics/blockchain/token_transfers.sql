5fbba2f652e072e0f15b24bf742543bedd9ce4a2
ccerv1 <carl.cervone@gmail.com>
2025-05-02 20:36:29 -0400

    refactor(sqlmesh): smol changes to token transfer events

--- warehouse/oso_sqlmesh/oso_metrics/blockchain/token_transfers.sql
+++ warehouse/oso_sqlmesh/oso_metrics/blockchain/token_transfers.sql
@@ -1,5 +1,3 @@
--- make a new int_events_daily__blockchain that will have transaction volume
-
 select
     @metrics_sample_date(events.bucket_day) as metrics_sample_date,
     events.event_source,
@@ -7,8 +5,8 @@ select
     '' as from_artifact_id,
     @metric_name() as metric,
     sum(events.amount) as amount
-from oso.int_events_daily__blockchain_transfers as events
+from oso.int_events_daily__blockchain_token_transfers as events
 where
-    event_type = 'BLOCKCHAIN_TRANSFER'
+    event_type = 'BLOCKCHAIN_TOKEN_TRANSFER'
     and events.bucket_day between @metrics_start('DATE') and @metrics_end('DATE')
 group by 1, metric, from_artifact_id, to_artifact_id, event_source
