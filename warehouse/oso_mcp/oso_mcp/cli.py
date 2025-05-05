8474d42c1845f886169df53f85045ce57fd22120
Reuven Gonzales <reuven@karibalabs.co>
2025-05-05 14:43:49 +0900

    feat: prepare oso_mcp for deployment (#3715)

---
+++ warehouse/oso_mcp/oso_mcp/cli.py
@@ -0,0 +1,37 @@
+import logging
+
+import click
+from dotenv import load_dotenv
+
+from .server.app import setup_mcp_app
+from .server.config import MCPConfig
+from .utils.log import setup_logging
+
+load_dotenv()
+logger = logging.getLogger("oso-mcp")
+
+pass_config = click.make_pass_decorator(MCPConfig, ensure=True)
+
+@click.group(context_settings={"help_option_names": ["-h", "--help"]})
+@click.option(
+    "--verbose",
+    "-v",
+    count=True,
+    help="Increase verbosity (can be used multiple times)",
+)
+@click.pass_context
+def cli(ctx, verbose):
+    setup_logging(verbose)
+
+    ctx.obj = MCPConfig()
+
+
+@cli.command()
+@pass_config
+def serve(config: MCPConfig):
+    """Start the OSO MCP server."""
+    logger.info("Starting OSO MCP server...")
+
+    mcp_app = setup_mcp_app(config)
+
+    mcp_app.run(transport=config.transport)
---
+++ warehouse/oso_mcp/oso_mcp/server/config.py
@@ -0,0 +1,34 @@
+import typing as t
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class MCPConfig(BaseSettings):
+    """Configuration for the agent and its components."""
+
+    host: str = Field(
+        default="127.0.0.1",
+        description="Host for the mcp server to run on",
+    )
+
+    port: int = Field(
+        default=8000, description="Port for the mcp server to run on"
+    )
+
+    transport: t.Literal['sse', 'stdio'] = Field(
+        default="sse",
+        description="The MCP transport.",
+    )
+
+    oso_api_key: str = Field(
+        default="OSO API KEY",
+        description="API key for OSO",
+    )
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_prefix="MCP_",
+        extra="ignore",
+    )
