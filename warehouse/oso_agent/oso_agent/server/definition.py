7d31036999a75f4b0db4b35722cf5765d763fd90
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2025-05-06 15:05:31 +0000

    feat: adds agent server

--- warehouse/oso_agent/oso_agent/server/definition.py
+++ warehouse/oso_agent/oso_agent/server/definition.py
@@ -1,9 +1,56 @@
+import typing as t
 
-from pydantic import Field
+from fastapi import FastAPI
+from llama_index.core.llms import ChatMessage
+from pydantic import BaseModel, Field
 
 from ..agent.config import AgentConfig
 from ..utils.config import agent_config_dict
 
+
+class ChatRequestMessageTextPart(BaseModel):
+    type: t.Literal["text"]
+    text: str
+
+class ChatRequestMessageStepStartPart(BaseModel):
+    type: t.Literal["step-start"]
+
+ChatRequestMessagePart = t.Union[
+    ChatRequestMessageTextPart,
+    ChatRequestMessageStepStartPart,
+]
+
+class ChatRequestMessage(BaseModel):
+    """Historical message for the agent."""
+
+    role: t.Literal["user", "assistant"]
+    content: str
+    parts: ChatRequestMessagePart = Field(discriminator="type")
+
+class ChatRequest(BaseModel):
+    """Request to chat with the agent."""
+
+    id: str
+
+    # The chat history
+    messages: list[ChatRequestMessage]
+
+    @property
+    def current_message(self) -> ChatRequestMessage:
+        """Get the current message in the chat history."""
+        if not len(self.messages) < 1:
+            raise ValueError("No messages in chat history")
+        return self.messages[-1]
+
+    def to_llama_index_chat_history(self) -> t.List[ChatMessage]:
+        """Convert to LlamaIndex chat history."""
+        return [
+            ChatMessage(
+                role=message.role,
+                content=message.content,
+            )
+            for message in self.messages
+        ]
 
 class AgentServerConfig(AgentConfig):
     """Configuration for the agent and its components."""
@@ -18,3 +65,9 @@ class AgentServerConfig(AgentConfig):
         default="127.0.1",
         description="Host for the server to run on",
     )
+
+AppLifespan = t.Callable[[FastAPI], t.Any]
+
+ConfigType = t.TypeVar("ConfigType")
+
+AppLifespanFactory = t.Callable[[ConfigType], AppLifespan]
