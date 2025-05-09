eb5d6ba7f64e5ac22c95233d92060f8018e5c502
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2025-05-09 05:35:33 +0000

    chore!: remove warehouse/dbt directory

--- warehouse/dbt/models/intermediate/directory/int_collections.sql
+++
@@ -1,8 +0,0 @@
-select
-  collections.collection_id,
-  collections.collection_source,
-  collections.collection_namespace,
-  collections.collection_name,
-  collections.display_name,
-  collections.description
-from {{ ref('stg_ossd__current_collections') }} as collections
