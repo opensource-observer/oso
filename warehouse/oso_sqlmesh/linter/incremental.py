6d37c4015bb238b25d9e2a2505f5b8c938a00b87
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2025-05-10 04:19:13 +0000

    chore: add new linter

--- warehouse/oso_sqlmesh/linter/incremental.py
+++ warehouse/oso_sqlmesh/linter/incremental.py
@@ -53,3 +53,44 @@ class IncrementalMustDefineNoGapsAudit(Rule):
             rule=self,
             violation_msg=violation_msg or "Incremental models must have a time partition.",
         )
+
+class IncrementalMustHaveLookback(Rule):
+    """Incremental models must have a lookback defined that covers at least 1 month (31 days to be certain)."""
+
+    def check_model(self, model: Model) -> t.Optional[RuleViolation]:
+        # Rule violated if the model's owner field (`model.owner`) is not specified
+        if not isinstance(model.kind, IncrementalByTimeRangeKind):
+            return None
+        
+        minimum_lookback_seconds = 31 * 24 * 60 * 60
+        interval_unit = model.interval_unit
+        interval_seconds = interval_unit.seconds
+
+        minimum_lookback = minimum_lookback_seconds / interval_seconds
+
+        if model.kind.lookback is None:
+            return self.violation(f"Incremental by time range models must have a lookback defined of at least 31 days. At current settings this should be {minimum_lookback}")
+
+        # Check if the lookback is at least 31 days (or 1 month)
+        if interval_unit.is_month:
+            if model.kind.lookback >= 1:
+                return None
+            return self.violation(
+                f"Incremental by time range models must have a lookback of at least 31 days (or 1 month). Current lookback is {model.kind.lookback} {interval_unit.value}(s)."
+            )
+
+        lookback_seconds = model.kind.lookback * interval_seconds
+        print(f"lookback_seconds: {lookback_seconds}, minimum_lookback_seconds: {minimum_lookback_seconds}")
+        if lookback_seconds >= minimum_lookback_seconds:
+            return None
+        
+        return self.violation(
+            f"Incremental by time range models must have a lookback of at least 30 days. Current lookback is {model.kind.lookback} {interval_unit.value}(s)."
+        )
+
+    def violation(self, violation_msg: t.Optional[str] = None) -> RuleViolation:
+        # Create a RuleViolation object with the specified violation message
+        return RuleViolation(
+            rule=self,
+            violation_msg=violation_msg or "Incremental models must have a time partition.",
+        )
