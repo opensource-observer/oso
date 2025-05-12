a9c0c497cca355bac12c471e191ddbfff52a02e4
ccerv1 <carl.cervone@gmail.com>
2025-05-12 08:52:35 -0400

    chore(sqlmesh): disable audits on failing models

--- warehouse/oso_sqlmesh/models/staging/superchain/stg_superchain__4337_traces.sql
+++ warehouse/oso_sqlmesh/models/staging/superchain/stg_superchain__4337_traces.sql
@@ -23,10 +23,9 @@ MODEL (
   ),
   audits (
     has_at_least_n_rows(threshold := 0),
-    no_gaps(
-      time_column := block_timestamp,
-      no_gap_date_part := 'day',
-    ),
+  ),
+  ignored_rules (
+    "incrementalmustdefinenogapsaudit",
   )
 );
 
