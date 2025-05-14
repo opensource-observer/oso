2904c1b44ee15753c683b0241b108f3ba7e160bc
Reuven Gonzales <reuven@karibalabs.co>
2025-05-14 13:06:08 -0700

    chore: set better no_gaps params for more blockchain models (#3806)
    * chore: set better no_gaps params for more blockchain models
    
    * Update warehouse/oso_sqlmesh/models/intermediate/superchain/int_worldchain_events_by_project.sql

--- warehouse/oso_sqlmesh/models/intermediate/superchain/int_superchain_traces_txs_joined.sql
+++ warehouse/oso_sqlmesh/models/intermediate/superchain/int_superchain_traces_txs_joined.sql
@@ -27,6 +27,8 @@ MODEL (
     no_gaps(
       time_column := block_timestamp,
       no_gap_date_part := 'day',
+      ignore_before := @superchain_audit_start,
+      missing_rate_min_threshold := 0.95,
     ),
   )
 );
