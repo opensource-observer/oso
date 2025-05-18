5fd983c262be6221f4ad95dc0e1813a1d16e23e1
Raymond Cheng <me@raymondcheng.net>
2025-05-18 12:41:32 -0700

    feat: Add snackbars to global action results

--- apps/frontend/components/dataprovider/oso-global-context.tsx
+++ apps/frontend/components/dataprovider/oso-global-context.tsx
@@ -1,10 +1,25 @@
 import React, { ReactNode } from "react";
 import { DataProvider, GlobalActionsProvider } from "@plasmicapp/loader-nextjs"; // or "@plasmicapp/loader-*""
+import Snackbar from "@mui/material/Snackbar";
+import Alert from "@mui/material/Alert";
+import { ADT } from "ts-adt";
 import * as config from "../../lib/config";
 import { OsoAppClient } from "../../lib/clients/oso-app";
+import { RegistrationProps } from "../../lib/types/plasmic";
 
 const PLASMIC_KEY = "globals";
 const PLASMIC_CONTEXT_NAME = "OsoGlobalContext";
+const AUTO_HIDE_DURATION = 5000; // in milliseconds
+const SUCCESS_MESSAGE = "Done!";
+
+type SnackbarState = ADT<{
+  closed: Record<string, unknown>;
+  success: Record<string, unknown>;
+  error: {
+    code: string;
+    message: string;
+  };
+}>;
 
 const OsoGlobalActions: any = {
   updateMyUserProfile: { parameters: [{ name: "args", type: "object" }] },
@@ -24,12 +39,26 @@ const OsoGlobalActions: any = {
 // Users will be able to set these props in Studio.
 interface OsoGlobalContextProps {
   children?: ReactNode;
+  errorCodeMap?: any; // Map of error codes to error messages
 }
 
+const OsoGlobalContextPropsRegistration: RegistrationProps<OsoGlobalContextProps> =
+  {
+    children: "slot",
+    errorCodeMap: {
+      type: "object",
+      defaultValue: {},
+      helpText: "Error code to message (e.g. {'23505': 'Duplicate username'})",
+    },
+  };
+
 function OsoGlobalContext(props: OsoGlobalContextProps) {
-  const { children } = props;
+  const { children, errorCodeMap } = props;
   const [actionResult, setResult] = React.useState<any>(null);
   const [actionError, setError] = React.useState<any>(null);
+  const [snackbarState, setSnackbarState] = React.useState<SnackbarState>({
+    _type: "closed",
+  });
   const osoClient = new OsoAppClient();
   const data = {
     config,
@@ -37,30 +66,59 @@ function OsoGlobalContext(props: OsoGlobalContextProps) {
     actionError,
   };
 
+  const handleSuccess = (result: any) => {
+    setResult(result);
+    setSnackbarState({ _type: "success" });
+  };
+  const handleError = (error: any) => {
+    setError(error);
+    setSnackbarState({
+      _type: "error",
+      ...error,
+    });
+  };
+  const handleClose = (
+    _event?: React.SyntheticEvent | Event,
+    reason?: string,
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setSnackbarState({ _type: "closed" });
+  };
   const actions = React.useMemo(
     () => ({
       updateMyUserProfile: (args: any) =>
-        osoClient.updateMyUserProfile(args).then(setResult).catch(setError),
+        osoClient
+          .updateMyUserProfile(args)
+          .then(handleSuccess)
+          .catch(handleError),
       createApiKey: (args: any) =>
-        osoClient.createApiKey(args).then(setResult).catch(setError),
+        osoClient.createApiKey(args).then(handleSuccess).catch(handleError),
       deleteApiKey: (args: any) =>
-        osoClient.deleteApiKey(args).then(setResult).catch(setError),
+        osoClient.deleteApiKey(args).then(handleSuccess).catch(handleError),
       createOrganization: (args: any) =>
-        osoClient.createOrganization(args).then(setResult).catch(setError),
+        osoClient
+          .createOrganization(args)
+          .then(handleSuccess)
+          .catch(handleError),
       addUserToOrganizationByEmail: (args: any) =>
         osoClient
           .addUserToOrganizationByEmail(args)
-          .then(setResult)
-          .catch(setError),
+          .then(handleSuccess)
+          .catch(handleError),
       changeUserRole: (args: any) =>
-        osoClient.changeUserRole(args).then(setResult).catch(setError),
+        osoClient.changeUserRole(args).then(handleSuccess).catch(handleError),
       removeUserFromOrganization: (args: any) =>
         osoClient
           .removeUserFromOrganization(args)
-          .then(setResult)
-          .catch(setError),
+          .then(handleSuccess)
+          .catch(handleError),
       deleteOrganization: (args: any) =>
-        osoClient.deleteOrganization(args).then(setResult).catch(setError),
+        osoClient
+          .deleteOrganization(args)
+          .then(handleSuccess)
+          .catch(handleError),
     }),
     [osoClient],
   );
@@ -70,8 +128,37 @@ function OsoGlobalContext(props: OsoGlobalContextProps) {
       <DataProvider name={PLASMIC_KEY} data={data}>
         {children}
       </DataProvider>
+      <Snackbar
+        open={snackbarState._type !== "closed"}
+        autoHideDuration={AUTO_HIDE_DURATION}
+        onClose={handleClose}
+      >
+        <Alert
+          onClose={handleClose}
+          severity={
+            snackbarState._type === "success"
+              ? "success"
+              : snackbarState._type === "error"
+                ? "error"
+                : "info"
+          }
+          variant="filled"
+          sx={{ width: "100%" }}
+        >
+          {snackbarState._type === "success"
+            ? SUCCESS_MESSAGE
+            : snackbarState._type === "error"
+              ? errorCodeMap[snackbarState.code] ??
+                `${snackbarState.code}: ${snackbarState.message}`
+              : ""}
+        </Alert>
+      </Snackbar>
     </GlobalActionsProvider>
   );
 }
 
-export { OsoGlobalContext, OsoGlobalActions };
+export {
+  OsoGlobalContext,
+  OsoGlobalActions,
+  OsoGlobalContextPropsRegistration,
+};
