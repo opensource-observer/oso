835bba64d365f1fe408cd8c2c756c7115608dc08
Raymond Cheng <me@raymondcheng.net>
2025-05-17 19:12:59 -0700

    feat: separate OsoAppProvider into OsoDataProvider and OsoGlobalContext
    * Previous approach wasn't working
    * This makes it cleaner, where write actions are exposed globally, but
      read data context is local
    * Also exposes config as a global context

---
+++ apps/frontend/components/dataprovider/oso-global-context.tsx
@@ -0,0 +1,70 @@
+import React, { ReactNode } from "react";
+import { DataProvider, GlobalActionsProvider } from "@plasmicapp/loader-nextjs"; // or "@plasmicapp/loader-*""
+import * as config from "../../lib/config";
+import { OsoAppClient } from "../../lib/clients/oso-app";
+
+const PLASMIC_KEY = "globals";
+const PLASMIC_CONTEXT_NAME = "OsoGlobalContext";
+
+const OsoGlobalActions: any = {
+  updateMyUserProfile: { parameters: [{ name: "args", type: "object" }] },
+  createApiKey: { parameters: [{ name: "args", type: "object" }] },
+  createOrganization: { parameters: [{ name: "args", type: "object" }] },
+  addUserToOrganizationByEmail: {
+    parameters: [{ name: "args", type: "object" }],
+  },
+  changeUserRole: { parameters: [{ name: "args", type: "object" }] },
+  removeUserFromOrganization: {
+    parameters: [{ name: "args", type: "object" }],
+  },
+  deleteOrganization: { parameters: [{ name: "args", type: "object" }] },
+};
+
+// Users will be able to set these props in Studio.
+interface OsoGlobalContextProps {
+  children?: ReactNode;
+}
+
+function OsoGlobalContext(props: OsoGlobalContextProps) {
+  const { children } = props;
+  const [actionResult, setResult] = React.useState<any>(null);
+  const osoClient = new OsoAppClient();
+  const data = {
+    config,
+    actionResult,
+  };
+
+  const actions = React.useMemo(
+    () => ({
+      updateMyUserProfile: (args: any) =>
+        osoClient.updateMyUserProfile(args).then((res) => setResult(res)),
+      createApiKey: (args: any) =>
+        osoClient.createApiKey(args).then((res) => setResult(res)),
+      createOrganization: (args: any) =>
+        osoClient.createOrganization(args).then((res) => setResult(res)),
+      addUserToOrganizationByEmail: (args: any) =>
+        osoClient
+          .addUserToOrganizationByEmail(args)
+          .then((res) => setResult(res)),
+      changeUserRole: (args: any) =>
+        osoClient.changeUserRole(args).then((res) => setResult(res)),
+      removeUserFromOrganization: (args: any) =>
+        osoClient
+          .removeUserFromOrganization(args)
+          .then((res) => setResult(res)),
+      deleteOrganization: (args: any) =>
+        osoClient.deleteOrganization(args).then((res) => setResult(res)),
+    }),
+    [osoClient],
+  );
+
+  return (
+    <GlobalActionsProvider contextName={PLASMIC_CONTEXT_NAME} actions={actions}>
+      <DataProvider name={PLASMIC_KEY} data={data}>
+        {children}
+      </DataProvider>
+    </GlobalActionsProvider>
+  );
+}
+
+export { OsoGlobalContext, OsoGlobalActions };
--- apps/frontend/lib/types/plasmic.ts
+++ apps/frontend/lib/types/plasmic.ts
@@ -1,7 +1,5 @@
-import { PropType, CodeComponentMeta } from "@plasmicapp/loader-nextjs";
+import { PropType } from "@plasmicapp/loader-nextjs";
 
 export type RegistrationProps<P> = {
   [prop: string]: PropType<P>;
 };
-
-export type RegistrationRefActions<P> = CodeComponentMeta<P>["refActions"];
