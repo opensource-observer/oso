a130a4bf571f9f922f8b035ccac701a057845b8e
Raymond Cheng <me@raymondcheng.net>
2025-05-26 20:02:37 -0700

    feat: more table and column descriptions for metric mart models (#3920)

--- warehouse/oso_sqlmesh/models/marts/events/event_types_v1.sql
+++ warehouse/oso_sqlmesh/models/marts/events/event_types_v1.sql
@@ -6,6 +6,10 @@ MODEL (
   ),
   audits (
     has_at_least_n_rows(threshold := 0)
+  ),
+  description 'The `event_types_v1` table contains distinct event types from various sources, such as GitHub, blockchain, and funding events. This table is used to enumerate different types of events that occur across the open source ecosystem.',
+  column_descriptions (
+    event_type = 'The unique identifier for the event type, such as "COMMIT_CODE", "PULL_REQUEST_MERGED", or "FUNDING".'
   )
 );
 
