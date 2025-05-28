d6f5810d29823e859c7d74d4221fbb57ce657783
Raymond Cheng <me@raymondcheng.net>
2025-05-28 13:43:48 -0700

    feat: dataset diff
    * Looks for new questions in the code examples, and uploads only those

--- warehouse/oso_agent/oso_agent/datasets/text2sql.py
+++ warehouse/oso_agent/oso_agent/datasets/text2sql.py
@@ -2,7 +2,6 @@ from typing import List
 
 from ..types.datasets import Example, create_example
 
-TEXT2SQL_DATASET_DESCRIPTION = "Text2SQL dataset for evaluating SQL query generation from natural language questions."
 TEXT2SQL_DATASET: List[Example] = [
     create_example(
         question="How many projects are in the 'optimism' collection?",
@@ -11,5 +10,13 @@ TEXT2SQL_DATASET: List[Example] = [
         difficulty="easy",
         query_type=["aggregation", "filter"],
         query_domain=["directory"]
+    ),
+    create_example(
+        question="How many issues were opened and closed by the project Hypercerts during the year 2023?",
+        answer="WITH ProjectFilter AS (SELECT project_id FROM projects_v1 WHERE project_name IN ('hypercerts')), MetricFilter AS (SELECT metric_id, metric_name, CASE WHEN metric_name = 'GITHUB_opened_issues_yearly' THEN 'Issues Opened' WHEN metric_name = 'GITHUB_closed_issues_yearly' THEN 'Issues Closed' ELSE 'Other' END AS issue_status FROM metrics_v0 WHERE metric_name IN ('GITHUB_opened_issues_yearly', 'GITHUB_closed_issues_yearly')) SELECT mf.issue_status, SUM(COALESCE(ts.amount, 0)) AS total_count FROM timeseries_metrics_by_project_v0 AS ts INNER JOIN ProjectFilter AS pf ON ts.project_id = pf.project_id INNER JOIN MetricFilter AS mf ON ts.metric_id = mf.metric_id WHERE EXTRACT(YEAR FROM ts.sample_date) = 2023 GROUP BY mf.issue_status",
+        priority="medium",
+        difficulty="medium",
+        query_type=["aggregation", "filter"],
+        query_domain=["directory", "metrics"]
     ),
 ]
