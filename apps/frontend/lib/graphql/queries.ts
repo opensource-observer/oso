d53ef8edda19e3b9ffe80ac94656b6e7a31e3eac
Raymond Cheng <me@raymondcheng.net>
2025-06-04 15:51:17 -0700

    fix: checking in generated graphql codegen files

--- apps/frontend/lib/graphql/queries.ts
+++ apps/frontend/lib/graphql/queries.ts
@@ -1,4 +1,4 @@
-import { gql } from "../__generated__/gql";
+import { gql } from "./generated/gql";
 
 // Max TTL on Hasura is 300 seconds
 // https://hasura.io/docs/latest/caching/caching-config/#controlling-cache-lifetime/
