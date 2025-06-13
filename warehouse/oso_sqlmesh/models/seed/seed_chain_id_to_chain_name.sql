bd53be24b3f030b71ed912f28133f34b1e7e2b46
ccerv1 <carl.cervone@gmail.com>
2025-06-13 14:40:31 -0400

    feat(sqlmesh): create seed data for storing chain_id to oso_chain_names

---
+++ warehouse/oso_sqlmesh/models/seed/seed_chain_id_to_chain_name.sql
@@ -0,0 +1,14 @@
+/* Purpose: Seed data for chain id to OSO chain name. To update this data, please */ /* update the CSV file at the path specified below. This can be used for any evm */ /* chain. */
+MODEL (
+  name oso.seed_chain_id_to_chain_name,
+  kind SEED (
+    path '../../seeds/chain_id_to_chain_name.csv'
+  ),
+  columns (
+    chain_id INT64,
+    chain_name TEXT
+  ),
+  audits (
+    has_at_least_n_rows(threshold := 0)
+  )
+)
