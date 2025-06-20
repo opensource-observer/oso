9c70fa2bc51dcd8685103160860347fc40e050a2
Ícaro Guerra <icaro@plasmic.app>
2025-06-20 12:59:45 -0300

    refactor(semantic): move semantic code to a new package (#4233)
    * refactor(semantic): move semantic code to a new package
    
    * fix(oso_agent): include oso_semantic in the oso_agent dependency

---
+++ warehouse/oso_semantic/README.md
@@ -0,0 +1,1 @@
+# OSO-Semantic Query Layer
---
+++ warehouse/oso_semantic/oso_semantic/__init__.py
@@ -0,0 +1,12 @@
+# ruff: noqa: F401 F403
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("pyoso")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
+from .definition import *
+from .errors import InvalidAttributeReferenceError
+from .query import QueryBuilder
