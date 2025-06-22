7395a3bed9711b4a9c5f4cf72181ab74ca11f233
ccerv1 <carl.cervone@gmail.com>
2025-06-22 16:06:54 -0400

    fix(sqlmesh): fix linting error with snapshot_day

--- warehouse/oso_sqlmesh/models/intermediate/entities/artifacts/packages/int_sbom__latest_snapshot.sql
+++ warehouse/oso_sqlmesh/models/intermediate/entities/artifacts/packages/int_sbom__latest_snapshot.sql
@@ -13,27 +13,22 @@ WITH latest_repo_snapshot AS (
     MAX(snapshot_day) AS snapshot_day
   FROM oso.int_sbom__snapshots
   GROUP BY 1
-),
-
-latest_sboms AS (
-  SELECT *
-  FROM oso.int_sbom__snapshots AS sbom
-  JOIN latest_repo_snapshot AS latest
-    ON sbom.dependent_repo_artifact_id = latest.dependent_repo_artifact_id
-    AND sbom.snapshot_day >= (latest.snapshot_day - INTERVAL '1 DAY')
 )
 
 SELECT DISTINCT
-  snapshot_day,
-  artifact_source,
-  artifact_namespace,
-  artifact_name,
-  package_source,
-  package,
-  package_version,
-  package_github_owner,
-  package_github_repo,
-  dependent_repo_artifact_id,
-  dependency_repo_artifact_id,
-  dependency_package_artifact_id
-FROM latest_sboms
+  sbom.snapshot_day,
+  sbom.artifact_source,
+  sbom.artifact_namespace,
+  sbom.artifact_name,
+  sbom.package_source,
+  sbom.package,
+  sbom.package_version,
+  sbom.package_github_owner,
+  sbom.package_github_repo,
+  sbom.dependent_repo_artifact_id,
+  sbom.dependency_repo_artifact_id,
+  sbom.dependency_package_artifact_id
+FROM oso.int_sbom__snapshots AS sbom
+JOIN latest_repo_snapshot AS latest
+  ON sbom.dependent_repo_artifact_id = latest.dependent_repo_artifact_id
+  AND sbom.snapshot_day >= (latest.snapshot_day - INTERVAL '1 DAY')
