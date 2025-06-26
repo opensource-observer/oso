9d4234cab6a58a7d322dff3404f31d4d6097cbe8
Evan Meyer <evanm6@illinois.edu>
2025-06-26 18:10:18 -0500

    feat: improved how entity search variants are generated

--- warehouse/oso_mcp/oso_mcp/server/app.py
+++ warehouse/oso_mcp/oso_mcp/server/app.py
@@ -15,9 +15,13 @@ from ..utils.entity_context import (
     call_llm_for_entity_variants,
     call_llm_for_final_selection,
 <<<<<<< HEAD
+<<<<<<< HEAD
     generate_entity_string_variants,
 =======
 >>>>>>> 584d2789 (feat: improved how entity search variants are generated)
+=======
+    generate_entity_string_variants,
+>>>>>>> 90a03fe4 (feat: improved how entity search variants are generated)
 )
 from .config import MCPConfig
 
@@ -430,11 +434,16 @@ def setup_mcp_app(config: MCPConfig):
         else:
             config = AgentConfig()
 <<<<<<< HEAD
+<<<<<<< HEAD
             llm_variants = await call_llm_for_entity_variants(config, entity, nl_query, entity_type)
             entity_variants = generate_entity_string_variants(llm_variants)
 =======
             entity_variants = await call_llm_for_entity_variants(config, entity, nl_query, entity_type)
 >>>>>>> 584d2789 (feat: improved how entity search variants are generated)
+=======
+            llm_variants = await call_llm_for_entity_variants(config, entity, nl_query, entity_type)
+            entity_variants = generate_entity_string_variants(llm_variants)
+>>>>>>> 90a03fe4 (feat: improved how entity search variants are generated)
             sql = build_fuzzy_entity_search_sql(entity_variants, table, columns)
             resp = await query_oso(sql, ctx, limit=20)
             if not (isinstance(resp, McpSuccessResponse) and resp.results):
