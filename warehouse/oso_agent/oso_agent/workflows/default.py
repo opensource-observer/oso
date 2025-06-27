2b76b1d231a2f7862b63597d1dc6ac20ec324af0
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2025-06-27 21:52:34 +0000

    fix: fixes default resource resolver

--- warehouse/oso_agent/oso_agent/workflows/default.py
+++ warehouse/oso_agent/oso_agent/workflows/default.py
@@ -3,11 +3,13 @@ Provides a default workflow registry that is configured using the AgentConfig.
 """
 import logging
 
+from oso_agent.tool.oso_mcp_client import OsoMcpClient
+from oso_agent.tool.query_engine_tool import create_default_query_engine_tool
 from oso_agent.tool.storage_context import setup_storage_context
+from oso_semantic.testing import setup_registry
 
 from ..tool.embedding import create_embedding
 from ..tool.llm import create_llm
-from ..tool.oso_text2sql import create_oso_query_engine
 from ..util.config import AgentConfig
 from .registry import ResolverFactory, ResourceResolver, WorkflowRegistry
 from .semantic.mixins import SemanticWorkflow
@@ -21,19 +23,20 @@ async def default_resolver_factory(config: AgentConfig):
     llm = create_llm(config)
     embedding = create_embedding(config)
     storage_context = setup_storage_context(config, embed_model=embedding)
-    query_engine_tool = await create_oso_query_engine(
+    query_engine_tool = await create_default_query_engine_tool(
         config,
-        storage_context,
-        llm,
-        embedding,
-        synthesize_response=False,
     )
+    oso_mcp_client = OsoMcpClient(config.oso_mcp_url)
+
+    registry = setup_registry()
 
     return ResourceResolver.from_resources(
         query_engine_tool=query_engine_tool,
         llm=llm,
         embedding=embedding,
         storage_context=storage_context,
+        oso_mcp_client=oso_mcp_client,
+        registry=registry,
     )
 
 async def setup_default_workflow_registry(config: AgentConfig, resolver_factory: ResolverFactory = default_resolver_factory) -> WorkflowRegistry:
@@ -41,7 +44,7 @@ async def setup_default_workflow_registry(config: AgentConfig, resolver_factory:
     registry = WorkflowRegistry(config, resolver_factory)
 
     registry.add_workflow("basic_text2sql", BasicText2SQL)
-    registry.add_workflow("semantic_query", SemanticWorkflow)
+    registry.add_workflow("semantic_text2sql", SemanticWorkflow)
 
     logger.info("Default agent registry setup complete.")
     return registry
