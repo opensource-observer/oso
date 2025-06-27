df483650931fed808d5cd19927dbfe6b9630e63d
ccerv1 <carl.cervone@gmail.com>
2025-06-27 15:20:27 -0400

    fix(sqlmesh): ignore no gaps audit

--- warehouse/oso_sqlmesh/models/staging/superchain/stg_superchain__7702_transactions.sql
+++ warehouse/oso_sqlmesh/models/staging/superchain/stg_superchain__7702_transactions.sql
@@ -14,18 +14,15 @@ MODEL (
   dialect duckdb,
   audits (
     has_at_least_n_rows(threshold := 0),
-    no_gaps(
-      time_column := block_timestamp,
-      no_gap_date_part := 'day',
-      ignore_before := @superchain_audit_start,
-      ignore_after := @superchain_audit_end,
-      missing_rate_min_threshold := 0.95,
-    ),
+  ),
+  ignored_rules (
+    "incrementalmustdefinenogapsaudit",
   ),
   tags (
     "superchain",
     "incremental",
   ),
+
 );
 
 SELECT
