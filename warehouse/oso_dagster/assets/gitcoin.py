666a4328e84cea90177fa3f252dae70197537872
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2025-07-07 18:37:24 -0400

    chore: remove some old dagster assets

--- warehouse/oso_dagster/assets/gitcoin.py
+++ warehouse/oso_dagster/assets/gitcoin.py
@@ -1,26 +1,6 @@
 
-from ..factories import IntervalGCSAsset, interval_gcs_import_asset
 from ..factories.sql import sql_assets
-from ..utils.common import SourceMode, TimeInterval
 from ..utils.secrets import SecretReference
-
-gitcoin_passport_scores = interval_gcs_import_asset(
-    IntervalGCSAsset(
-        key_prefix="gitcoin",
-        name="passport_scores",
-        project_id="opensource-observer",
-        bucket_name="oso-dataset-transfer-bucket",
-        path_base="passport",
-        file_match=r"(?P<interval_timestamp>\d\d\d\d-\d\d-\d\d)/scores.parquet",
-        destination_table="passport_scores",
-        raw_dataset_name="oso_raw_sources",
-        clean_dataset_name="gitcoin",
-        interval=TimeInterval.Daily,
-        mode=SourceMode.Overwrite,
-        retention_days=10,
-        format="PARQUET",
-    ),
-)
 
 regendata_xyz = sql_assets(
     "gitcoin",
