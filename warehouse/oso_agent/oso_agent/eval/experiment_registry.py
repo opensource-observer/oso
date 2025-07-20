a854a64ef271f61a3eb6e8570a4ec210f4756ebd
Javier R <javier.rios-urbano@epitech.eu>
2025-07-20 18:54:43 +0200

    add: `text2sql-semantic` experiment in registry

--- warehouse/oso_agent/oso_agent/eval/experiment_registry.py
+++ warehouse/oso_agent/oso_agent/eval/experiment_registry.py
@@ -2,21 +2,29 @@ import logging
 import typing as t
 from typing import Awaitable, Callable
 
-from phoenix.experiments.types import RanExperiment, Experiment
+from phoenix.experiments.types import Experiment, RanExperiment
 
 from ..agent.agent_registry import AgentRegistry
 from ..util.config import AgentConfig
-from .text2sql import text2sql_experiment
+from .text2sql import text2sql_experiment, text2sql_semantic_experiment
 
 # Setup logging
 logger = logging.getLogger(__name__)
 
 # Type alias for a dictionary of agents
-ExperimentDict = t.Dict[str, Callable[[AgentConfig, AgentRegistry, dict[str, t.Any ]], Awaitable[RanExperiment | Experiment]]]
+ExperimentDict = t.Dict[
+    str,
+    Callable[
+        [AgentConfig, AgentRegistry, dict[str, t.Any]],
+        Awaitable[RanExperiment | Experiment],
+    ],
+]
+
 
 def get_experiments() -> ExperimentDict:
-    """Create and configure the ReAct agent."""
+    """Create and configure all experiments."""
     registry: ExperimentDict = {}
     logger.info("Creating all experiments...")
     registry["text2sql"] = text2sql_experiment
+    registry["text2sql-semantic"] = text2sql_semantic_experiment
     return registry
