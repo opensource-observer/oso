8f1956690208a43f34117ba19fc8602fb40f3b04
Carl Cervone <carl.cervone@gmail.com>
2025-08-11 19:10:16 +0900

    fix(sqlmesh): move unknown project to constant def

--- warehouse/oso_sqlmesh/models/intermediate/entities/projects/openlabelsinitiative/int_artifacts_by_project_in_openlabelsinitiative.sql
+++ warehouse/oso_sqlmesh/models/intermediate/entities/projects/openlabelsinitiative/int_artifacts_by_project_in_openlabelsinitiative.sql
@@ -9,6 +9,8 @@ MODEL (
   ),
 );
 
+@DEF(missing_project_name, 'unknown');
+
 WITH addresses_by_project AS (
   SELECT
     chain AS artifact_source,
@@ -18,7 +20,7 @@ WITH addresses_by_project AS (
     parsed.artifact_url,
     'OPENLABELSINITIATIVE' AS project_source,
     'owner_project' AS project_namespace,
-    COALESCE(owner_project, 'unknown') AS project_name,
+    COALESCE(owner_project, @missing_project_name) AS project_name,
     usage_category
   FROM oso.int_addresses__openlabelsinitiative
   CROSS JOIN LATERAL @parse_blockchain_artifact(address) AS parsed
