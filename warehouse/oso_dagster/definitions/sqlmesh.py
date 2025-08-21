afbe0576ba0865a874d6d0ac122638e395c4ad4d
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2025-08-21 19:35:33 +0000

    fix: fixes code locations scheduling
    This is a temporary fix for now. We should address this with: https://github.com/opensource-observer/oso/issues/4840

--- warehouse/oso_dagster/definitions/sqlmesh.py
+++ warehouse/oso_dagster/definitions/sqlmesh.py
@@ -15,14 +15,17 @@ def sqlmesh_definitions(
 ) -> DefinitionsLoaderResponse:
     """This is the sqlmesh definitions for oso_dagster.
 
-    sqlmesh is kept separately because it takes more time to load and we want to
-    make it easier to develop things locally.
+    This is kept separate _specifically_ to help with development of assets that
+    are not sqlmesh. sqlmesh assets take a long time to load. In production this
+    code location is not used because of some existing issues with dagster
+    schedules and multiple code locations. Additionally, on production we are
+    able to build the sqlmesh assets cache in the docker container of every
+    deployment so loading takes significantly less time.
     """
-    from ..assets import sqlmesh as sqlmesh_assets
-    from ..factories import load_all_assets_from_package
+    from ..factories import load_all_assets_from_packages
 
-    asset_factories = load_all_assets_from_package(
-        sqlmesh_assets,
+    asset_factories = load_all_assets_from_packages(
+        ["oso_dagster.assets.sqlmesh"],
         resources,
     )
 
