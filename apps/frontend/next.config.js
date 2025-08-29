bba56f345d9567197f51a3cecfb1e0058ac3f068
Raymond Cheng <me@raymondcheng.net>
2025-08-29 16:04:08 -0700

    feat: configure uploading sourcemaps to PostHog

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,5 +1,6 @@
 // @ts-check
 import { withPostHogConfig } from "@posthog/nextjs-config";
+import { POSTHOG_HOST, POSTHOG_KEY, POSTHOG_PROJECT_ID } from "@/lib/config";
 
 /**
  * @type {import('next').NextConfig}
@@ -125,14 +126,14 @@ const nextConfig = {
 };
 
 module.exports = withPostHogConfig(nextConfig, {
-  personalApiKey: process.env.POSTHOG_API_KEY, // Personal API Key
-  envId: process.env.POSTHOG_ENV_ID, // Environment ID
-  host: process.env.NEXT_PUBLIC_POSTHOG_HOST, // (optional), defaults to https://us.posthog.com
+  personalApiKey: POSTHOG_KEY, // Personal API Key
+  envId: POSTHOG_PROJECT_ID, // Environment ID
+  host: POSTHOG_HOST, // (optional), defaults to https://us.posthog.com
   sourcemaps: {
     // (optional)
-    enabled: true, // (optional) Enable sourcemaps generation and upload, default to true on production builds
-    project: "my-application", // (optional) Project name, defaults to repository name
-    version: "1.0.0", // (optional) Release version, defaults to current git commit
-    deleteAfterUpload: true, // (optional) Delete sourcemaps after upload, defaults to true
+    //enabled: true, // (optional) Enable sourcemaps generation and upload, default to true on production builds
+    //project: "my-application", // (optional) Project name, defaults to repository name
+    //version: "1.0.0", // (optional) Release version, defaults to current git commit
+    //deleteAfterUpload: true, // (optional) Delete sourcemaps after upload, defaults to true
   },
 });
