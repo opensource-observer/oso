6c2c9f2a993e1f462ff0e14007bef2c5f2357402
Raymond Cheng <me@raymondcheng.net>
2025-08-29 12:34:10 -0700

    feat: add PostHog sourcemaps

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,4 +1,5 @@
 // @ts-check
+import { withPostHogConfig } from "@posthog/nextjs-config";
 
 /**
  * @type {import('next').NextConfig}
@@ -123,4 +124,15 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+module.exports = withPostHogConfig(nextConfig, {
+  personalApiKey: process.env.POSTHOG_API_KEY, // Personal API Key
+  envId: process.env.POSTHOG_ENV_ID, // Environment ID
+  host: process.env.NEXT_PUBLIC_POSTHOG_HOST, // (optional), defaults to https://us.posthog.com
+  sourcemaps: {
+    // (optional)
+    enabled: true, // (optional) Enable sourcemaps generation and upload, default to true on production builds
+    project: "my-application", // (optional) Project name, defaults to repository name
+    version: "1.0.0", // (optional) Release version, defaults to current git commit
+    deleteAfterUpload: true, // (optional) Delete sourcemaps after upload, defaults to true
+  },
+});
