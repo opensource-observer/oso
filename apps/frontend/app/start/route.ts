49ce5400c6f84d332f81935db56f3247f1829c2b
Raymond Cheng <me@raymondcheng.net>
2025-08-29 12:15:00 -0700

    feat: add /start redirect (#4904)
    * feat: add /start redirect
    
    * throw is orgId is undefined

---
+++ apps/frontend/app/start/route.ts
@@ -0,0 +1,17 @@
+import { NextRequest, NextResponse } from "next/server";
+import { logger } from "@/lib/logger";
+import { getUser } from "@/lib/auth/auth";
+
+export async function GET(req: NextRequest) {
+  const user = await getUser(req);
+
+  if (user.role === "anonymous") {
+    logger.log(`/start: User is anonymous`);
+    return NextResponse.redirect("/login");
+  }
+  const orgId = user.orgId;
+  if (!orgId) {
+    throw new Error("User has no orgId");
+  }
+  return NextResponse.redirect(`/orgs/${orgId}`);
+}
