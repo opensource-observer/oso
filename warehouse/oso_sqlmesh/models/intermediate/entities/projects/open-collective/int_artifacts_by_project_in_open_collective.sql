893fc0a5ebacb62986b425c89fd319e7717facd8
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2025-08-30 12:49:16 +0900

    hotfix(sqlmesh): revert back and disable opencollective project models (#4916)
    * hotfix(sqlmesh): try with json_extract
    
    * fix(sqlmesh): disable open-collective accounts model

--- warehouse/oso_sqlmesh/models/intermediate/entities/projects/open-collective/int_artifacts_by_project_in_open_collective.sql
+++ warehouse/oso_sqlmesh/models/intermediate/entities/projects/open-collective/int_artifacts_by_project_in_open_collective.sql
@@ -9,7 +9,8 @@ MODEL (
   audits (
     has_at_least_n_rows(threshold := 0),
     not_null(columns := (artifact_id, project_id))
-  )
+  ),
+  enabled false
 );
 
 WITH filtered_accounts AS (
@@ -23,8 +24,8 @@ WITH filtered_accounts AS (
   WHERE
     github_handle IS NOT NULL
     AND repository_url IS NOT NULL
-    AND total_amount_received_currency = 'USD'
-    AND total_amount_received_value >= 1000
+    AND stats.total_amount_received.currency = 'USD'
+    AND CAST(stats.total_amount_received.value AS DOUBLE) >= 1000
     AND type IN ('ORGANIZATION', 'INDIVIDUAL', 'COLLECTIVE')
 ),
 
