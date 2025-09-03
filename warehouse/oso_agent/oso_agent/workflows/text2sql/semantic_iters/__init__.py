7691f8cb54f67df1244d81462c83d663d53ad2a3
Javier R <javier.rios-urbano@epitech.eu>
2025-09-03 19:12:59 +0200

    fix: address `workflow` structure concerns

--- warehouse/oso_agent/oso_agent/workflows/text2sql/semantic_iters/__init__.py
+++
@@ -1,7 +0,0 @@
-from .v18_08_25.i0 import SemanticText2SQLWorkflow as SemanticText2SQLWorkflowV0
-from .v27_08_25.i1 import SemanticText2SQLWorkflow as SemanticText2SQLWorkflowV1
-
-__all__ = [
-    "SemanticText2SQLWorkflowV0",
-    "SemanticText2SQLWorkflowV1",
-]
