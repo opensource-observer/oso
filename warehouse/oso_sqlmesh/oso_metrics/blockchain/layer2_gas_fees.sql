09c57c25779386266d83aea5a640b2a4613dd47f
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2025-09-04 17:11:27 -0400

    feat(sqlmesh): add s8 timeseries metrics (#4969)
    * feat(sqlmesh): add s8 timeseries metrics
    
    * fix: disable some of the intervals
    
    * fix: filter addresses by length and prefix

---
+++ warehouse/oso_sqlmesh/oso_metrics/blockchain/layer2_gas_fees.sql
@@ -0,0 +1,11 @@
+select
+    @metrics_sample_date(events.bucket_day) as metrics_sample_date,
+    events.event_source,
+    events.to_artifact_id,
+    '' as from_artifact_id,
+    @metric_name('layer2_gas_fees') as metric,
+    sum(events.l2_gas_fee) as amount
+from oso.int_events_daily__l2_transactions as events
+where
+    events.bucket_day between @metrics_start('DATE') and @metrics_end('DATE')
+group by 1, metric, from_artifact_id, to_artifact_id, event_source
