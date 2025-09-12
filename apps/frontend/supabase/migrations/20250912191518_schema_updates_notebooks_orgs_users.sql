24e5da33810eed854806a48284faa5bd0fe2d0b7
Javier R <javier.rios-urbano@epitech.eu>
2025-09-12 22:52:57 +0200

    fix: add `rls` and rename to `notebook_name`

--- apps/frontend/supabase/migrations/20250912191518_schema_updates_notebooks_orgs_users.sql
+++ apps/frontend/supabase/migrations/20250912191518_schema_updates_notebooks_orgs_users.sql
@@ -13,9 +13,14 @@ CREATE TABLE reserved_names (
 COMMENT ON TABLE reserved_names IS 'Contains reserved names that cannot be used for organization names';
 COMMENT ON COLUMN reserved_names.name IS 'Reserved name that is not allowed as an organization name';
 
+ALTER TABLE public.reserved_names ENABLE ROW LEVEL SECURITY;
+
+CREATE POLICY "Reserved names are private" ON public.reserved_names
+    FOR SELECT USING (false);
+
 ALTER TABLE notebooks 
-RENAME COLUMN display_name TO name;
+RENAME COLUMN display_name TO notebook_name;
 
 ALTER TABLE notebooks 
 ADD CONSTRAINT notebooks_name_org_id_unique 
-UNIQUE (name, org_id);
+UNIQUE (notebook_name, org_id);
