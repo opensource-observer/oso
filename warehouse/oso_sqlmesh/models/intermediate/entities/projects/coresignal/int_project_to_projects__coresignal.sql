8183b5974a116184aa9c23c83694a2d1cc014dbe
Carl Cervone <carl.cervone@gmail.com>
2025-09-16 22:14:27 -0400

    refactor(sqlmesh): int_project_to_projects__coresignal

--- warehouse/oso_sqlmesh/models/intermediate/entities/projects/coresignal/int_project_to_projects__coresignal.sql
+++ warehouse/oso_sqlmesh/models/intermediate/entities/projects/coresignal/int_project_to_projects__coresignal.sql
@@ -1,6 +1,6 @@
 MODEL (
   name oso.int_project_to_projects__coresignal,
-  description 'Maps Coresignal companies to OSO projects',
+  description 'Maps Coresignal companies to projects in OSS Directory',
   dialect trino,
   kind FULL,
   audits (
@@ -10,10 +10,9 @@ MODEL (
 
 WITH social AS (
   SELECT DISTINCT
-    @oso_entity_id('CORESIGNAL', '', CAST(id AS VARCHAR)) AS id,
-    id as source_id,
-    name,
-    url_type as artifact_source,
+    id AS coresignal_company_id,
+    name AS coresignal_company_name,
+    url_type AS artifact_type,
     CASE 
       WHEN url_type = 'TWITTER' THEN REGEXP_EXTRACT(url, '(?:twitter|x)\\.com/([^/?#]+)', 1)
       WHEN url_type = 'GITHUB' THEN REGEXP_EXTRACT(url, 'github\\.com/([^/?#]+)', 1)
@@ -23,9 +22,9 @@ WITH social AS (
 ),
 abp AS (
    SELECT DISTINCT
-     project_id,
-     project_name,
-     project_source,
+     project_id AS oso_project_id,
+     project_name AS oso_project_name,
+     project_source AS oso_project_source,
      artifact_source,
      CASE
        WHEN artifact_source = 'GITHUB' THEN artifact_namespace
@@ -34,15 +33,19 @@ abp AS (
    FROM oso.artifacts_by_project_v1
    WHERE
      artifact_source IN ('GITHUB', 'TWITTER')
-     AND project_source IN ('OSS_DIRECTORY', 'CRYPTO_ECOSYSTEMS')
-     AND project_namespace IN ('oso', 'eco')
+     AND project_source = 'OSS_DIRECTORY'
+     AND project_namespace = 'oso'
 )
 
 SELECT
-  social.*,
-  abp.project_id as oso_project_id,
-  abp.project_name as oso_project_name,
-  abp.project_source as oso_project_source
+  social.coresignal_company_id,
+  social.coresignal_company_name,
+  social.artifact_type,
+  social.artifact_owner,
+  social.artifact_url,
+  abp.oso_project_id,
+  abp.oso_project_name,
+  abp.oso_project_source
 FROM social
 LEFT JOIN abp
 ON
