700e575ef90b58a1fbb73ce6c723630621b712ae
Raymond Cheng <me@raymondcheng.net>
2025-09-24 04:30:50 -0700

    feat: /start should redirect to the onboarding wizard if you have no notebooks

--- apps/frontend/app/start/route.ts
+++ apps/frontend/app/start/route.ts
@@ -1,11 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 import { logger } from "@/lib/logger";
 import { getOrgUser } from "@/lib/auth/auth";
+import { createServerClient } from "@/lib/supabase/server";
 import { withPostHogTracking } from "@/lib/clients/posthog";
 
 export const dynamic = "force-dynamic";
 
 export const GET = withPostHogTracking(async (req: NextRequest) => {
+  const supabaseClient = await createServerClient();
   const user = await getOrgUser(req);
 
   if (user.role === "anonymous") {
@@ -13,5 +15,16 @@ export const GET = withPostHogTracking(async (req: NextRequest) => {
     return NextResponse.redirect(new URL("/login", req.url));
   }
   const orgName = user.orgName;
+  // Look for any existing notebooks
+  const { data, error } = await supabaseClient
+    .from("notebooks")
+    .select("*,organizations!inner(org_name)")
+    .eq("organizations.org_name", orgName)
+    .is("deleted_at", null);
+  if (error) {
+    throw error;
+  } else if (!data || data.length <= 0) {
+    return NextResponse.redirect(new URL("/get-started", req.url));
+  }
   return NextResponse.redirect(new URL(`/${orgName}`, req.url));
 });
