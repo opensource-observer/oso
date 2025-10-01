3aa150c2e60bf5b51ab06ce1df767682a6b9ae06
ccerv1 <carl.cervone@gmail.com>
2025-10-01 16:45:34 -0400

    feat(sqlmesh): add staticcalls models

---
+++ warehouse/oso_sqlmesh/models/intermediate/events/blockchain/superchain/int_superchain_static_calls_with_depth.sql
@@ -0,0 +1,37 @@
+MODEL (
+  name oso.int_superchain_static_calls_with_depth,
+  description 'Static calls with trace depth',
+  kind INCREMENTAL_BY_TIME_RANGE (
+    time_column block_timestamp,
+    batch_size 90,
+    batch_concurrency 3,
+    lookback 31,
+    forward_only true,
+  ),
+  start @blockchain_incremental_start,
+  cron '@daily',
+  partitioned_by (DAY("block_timestamp"), "chain"),
+  grain (block_timestamp, chain, transaction_hash, from_address, to_address, depth),
+  audits (
+    has_at_least_n_rows(threshold := 0),
+    no_gaps(
+      time_column := block_timestamp,
+      no_gap_date_part := 'day',
+      ignore_before := @superchain_audit_start,
+      ignore_after := @superchain_audit_end,
+      missing_rate_min_threshold := 0.95,
+    ),
+  )
+);
+
+SELECT
+  block_timestamp,
+  chain,
+  transaction_hash,
+  from_address,
+  to_address,
+  trace_address,
+  gas_used,
+  cardinality(filter(split(regexp_replace(trace_address,'\\s',''),','),x->x<>'')) AS depth
+FROM oso.stg_superchain__static_calls
+WHERE block_timestamp BETWEEN @start_dt AND @end_dt
---
+++ warehouse/oso_sqlmesh/models/staging/superchain/stg_superchain__static_calls.sql
@@ -0,0 +1,47 @@
+MODEL (
+  name oso.stg_superchain__static_calls,
+  kind INCREMENTAL_BY_TIME_RANGE (
+    time_column block_timestamp,
+    batch_size 90,
+    batch_concurrency 2,
+    lookback 31,
+    forward_only true,
+  ),
+  start @blockchain_incremental_start,
+  cron '@daily',
+  partitioned_by (DAY("block_timestamp"), "chain"),
+  grain (block_timestamp, chain, transaction_hash, from_address, to_address),
+  dialect duckdb,
+  audits (
+    has_at_least_n_rows(threshold := 0),
+    no_gaps(
+      time_column := block_timestamp,
+      no_gap_date_part := 'day',
+      ignore_before := @superchain_audit_start,
+      ignore_after := @superchain_audit_end,
+      missing_rate_min_threshold := 0.95,
+    ),
+  ),
+  tags (
+    "superchain",
+    "incremental",
+  ),
+);
+
+SELECT
+  @from_unix_timestamp(block_timestamp) AS block_timestamp,
+  transaction_hash,
+  from_address,
+  to_address,
+  gas_used,
+  subtraces,
+  trace_address,
+  @chain_name(chain) AS chain
+FROM @oso_source('bigquery.optimism_superchain_raw_onchain_data.traces')
+WHERE
+  network = 'mainnet'
+  AND "status" = 1
+  AND trace_type = 'call'
+  AND call_type = 'staticcall'
+  AND gas_used > 0
+  AND /* Bigquery requires we specify partitions to filter for this data source */ dt BETWEEN @start_dt AND @end_dt
