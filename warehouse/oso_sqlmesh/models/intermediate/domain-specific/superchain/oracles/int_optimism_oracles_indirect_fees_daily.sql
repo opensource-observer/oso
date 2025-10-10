abb70797a2988b2139e184f800833e26bee30e4c
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2025-10-09 22:14:40 -0700

    fix: fix merge issues

--- warehouse/oso_sqlmesh/models/intermediate/domain-specific/superchain/oracles/int_optimism_oracles_indirect_fees_daily.sql
+++ warehouse/oso_sqlmesh/models/intermediate/domain-specific/superchain/oracles/int_optimism_oracles_indirect_fees_daily.sql
@@ -3,12 +3,12 @@ MODEL (
   description "Optimism oracles indirect fees daily",
   dialect trino,
   kind incremental_by_time_range(
-   time_column bucket_day,
-   batch_size 60,
-   batch_concurrency 1,
-   lookback 14,
-   forward_only true,
-   on_destructive_change warn,
+    time_column bucket_day,
+    batch_size 60,
+    batch_concurrency 1,
+    lookback 14,
+    forward_only true,
+    on_destructive_change warn,
   ),
   start @blockchain_incremental_start,
   cron '@daily',
@@ -20,6 +20,9 @@ MODEL (
     "incrementalmustdefinenogapsaudit",
     "incrementalmusthaveforwardonly",
   ),
+  tags (
+    "entity_category=project",
+  )
 );
 
 WITH events AS (
