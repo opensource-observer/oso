c209ec0df03a9b77a979401f2742adac7ee30c4b
Javier R <javier.rios-urbano@epitech.eu>
2025-10-24 22:07:09 +0200

    chore: remove unnecessary `env` variable

--- apps/frontend/lib/graphql/codegen.ts
+++ apps/frontend/lib/graphql/codegen.ts
@@ -1,10 +1,16 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
 import {
   DAGSTER_GRAPHQL_URL,
+  // DOMAIN,
   HASURA_URL,
   OSO_API_KEY,
-  // OSO_GRAPH_URL,
 } from "@/lib/config";
+
+// const PROTOCOL = DOMAIN.includes("localhost") ? "http" : "https";
+// const OSO_GRAPH_URL = new URL(
+//   "/api/v1/osograph",
+//   `${PROTOCOL}://${DOMAIN}`,
+// ).toString();
 
 const SCHEMA: Record<string, any> = {};
 SCHEMA[HASURA_URL] = {
@@ -13,7 +19,7 @@ SCHEMA[HASURA_URL] = {
   },
 };
 SCHEMA[DAGSTER_GRAPHQL_URL] = {};
-// TODO(jabolo): Enable this whene oso graph is in production
+// TODO(jabolo): Enable this when oso graph is in production
 // so introspection does not fail (the endpoint is not accessible
 // until we merge into main for the first time).
 // SCHEMA[OSO_GRAPH_URL] = {};
