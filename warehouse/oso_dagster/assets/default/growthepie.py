9a72497a4700b86009cff351023f36880331e6a9
ccerv1 <carl.cervone@gmail.com>
2025-11-03 18:03:24 -0500

    fix(dagster): enumerate specific fundamentals metrics to go beyond 90D preview

--- warehouse/oso_dagster/assets/default/growthepie.py
+++ warehouse/oso_dagster/assets/default/growthepie.py
@@ -1,6 +1,20 @@
 from dlt.sources.rest_api.typing import RESTAPIConfig
 from oso_dagster.factories.rest import create_rest_factory_asset
 
+metrics = [
+    "daa",  # Daily Active Addresses
+    "fdv",  # Fully Diluted Valuation
+    "fees",  # Fees Paid by Users
+    "market_cap",  # Market Cap
+    "profit",  # Onchain Profit
+    "rent_paid",  # Rent Paid to L1
+    "stables_mcap",  # Stablecoin Market Cap
+    "throughput",  # Throughput
+    "tvl",  # Total Value Secured
+    "txcosts",  # Median Transaction Costs
+    "txcount",  # Transaction Count
+]
+
 config: RESTAPIConfig = {
     "client": {
         "base_url": "https://api.growthepie.com/v1",
@@ -10,12 +24,13 @@ config: RESTAPIConfig = {
     },
     "resources": [
         {
-            "name": "fundamentals",
+            "name": metric,
             "endpoint": {
-                "path": "fundamentals.json",
+                "path": f"export/{metric}.json",
                 "data_selector": "$",
             },
         }
+        for metric in metrics
     ],
 }
 
