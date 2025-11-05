bf4d80490c768b17910b0dd8155de37f0b3b0ff4
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2025-11-05 16:57:00 -0500

    hotfix(sqlmesh): change batch settings to staticcalls by project model (#5616)

--- warehouse/oso_sqlmesh/models/intermediate/domain-specific/superchain/oracles/int_optimism_static_calls_to_oracles_daily_by_project.sql
+++ warehouse/oso_sqlmesh/models/intermediate/domain-specific/superchain/oracles/int_optimism_static_calls_to_oracles_daily_by_project.sql
@@ -4,8 +4,8 @@ MODEL (
   dialect trino,
   kind incremental_by_time_range(
     time_column bucket_day,
-    batch_size 120,
-    batch_concurrency 3,
+    batch_size 365,
+    batch_concurrency 1,
     lookback @default_daily_incremental_lookback,
     forward_only true,
     on_destructive_change warn,
