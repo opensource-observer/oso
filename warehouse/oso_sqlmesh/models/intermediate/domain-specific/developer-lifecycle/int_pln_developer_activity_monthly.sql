1f3bbf5897f15325afa0bf82c1d25b12a63ad04c
ccerv1 <carl.cervone@gmail.com>
2025-11-14 00:47:06 -0300

    feat(sqlmesh): adds first attempt at lifecycle metrics for PLN projects

--- warehouse/oso_sqlmesh/models/intermediate/domain-specific/developer-lifecycle/int_pln_developer_activity_monthly.sql
+++ warehouse/oso_sqlmesh/models/intermediate/domain-specific/developer-lifecycle/int_pln_developer_activity_monthly.sql
@@ -7,13 +7,14 @@ MODEL (
     batch_size 12,
     batch_concurrency 2,
     forward_only true,
+    on_destructive_change warn,
     lookback @default_daily_incremental_lookback,
     auto_restatement_cron @default_auto_restatement_cron
   ),
   start @github_incremental_start,
   cron '@monthly',
   partitioned_by MONTH("bucket_month"),
-  grain (bucket_month, from_artifact_id, to_project_id),
+  grain (bucket_month, developer_id, project_id),
   audits (
     has_at_least_n_rows(threshold := 0),
     no_gaps(
@@ -29,8 +30,8 @@ MODEL (
 
 SELECT
   DATE_TRUNC('MONTH', e.bucket_day::DATE) AS bucket_month,
-  e.from_artifact_id,
-  abp.project_id AS to_project_id,
+  e.from_artifact_id AS developer_id,
+  abp.project_id AS project_id,
   COUNT(DISTINCT e.bucket_day) AS days_active
 FROM oso.int_events_daily__github AS e
 JOIN oso.artifacts_by_project_v1 AS abp
