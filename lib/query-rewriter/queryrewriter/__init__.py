9e9769351776649dea748e60bf216aa4dd04a555
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2025-11-18 14:17:05 -0300

    fix

--- lib/query-rewriter/queryrewriter/__init__.py
+++ lib/query-rewriter/queryrewriter/__init__.py
@@ -2,4 +2,16 @@
 from .dialect import extend_sqlglot
 from .rewrite import rewrite_query
 
-extend_sqlglot()
+extension_run = False
+
+if not extension_run:
+    from sqlglot import parse_one
+    from sqlglot.errors import ParseError
+
+    try:
+        mf = parse_one("@fake_macro('what')")
+        if mf.__class__.__name__ != "MacroFunc":
+            extend_sqlglot()
+    except ParseError:
+        extend_sqlglot()
+    extension_run = True
