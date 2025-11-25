1b6202c8a5ea35ed2159199dad2fbe2848239bd6
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2025-11-25 14:33:17 -0500

    feat(sqlmesh): create an opendevdata version of artifacts by project to evaluate coverage (#5740)

--- warehouse/oso_sqlmesh/models/intermediate/domain-specific/ddp/discovery/int_ddp_pgf_repos.sql
+++ warehouse/oso_sqlmesh/models/intermediate/domain-specific/ddp/discovery/int_ddp_pgf_repos.sql
@@ -14,7 +14,10 @@ MODEL (
 );
 
 WITH funded_projects AS (
-  SELECT DISTINCT project_id
+  SELECT
+    project_id,
+    ARRAY_AGG(DISTINCT metric_event_source) AS project_funding_sources,
+    SUM(amount) AS project_total_funding
   FROM oso.timeseries_metrics_by_project_v0
   JOIN oso.projects_v1 USING project_id
   JOIN oso.metrics_v0 USING metric_id
@@ -31,49 +34,29 @@ WITH funded_projects AS (
       'GITCOIN_MATCHING',
       'GITCOIN_DONATIONS'
     )
-    AND amount >= 5000
   GROUP BY 1
 ),
 project_repos AS (
   SELECT
     artifact_id,
-    project_id
+    artifact_namespace,
+    artifact_name,
+    'https://github.com/' || artifact_namespace || '/' || artifact_name AS artifact_url,
+    project_id,
+    project_name
   FROM oso.artifacts_by_project_v1
   JOIN funded_projects USING project_id
   WHERE artifact_source = 'GITHUB'
-),
-selected_repos AS (
-  SELECT
-    artifact_id,
-    last_artifact_id,
-    project_id
-  FROM oso.int_ddp_repo_lineage
-  JOIN project_repos USING artifact_id
-),
-all_repos AS (
-  SELECT
-    artifact_id,
-    project_id
-  FROM selected_repos
-  UNION ALL
-  SELECT
-    last_artifact_id AS artifact_id,
-    project_id
-  FROM selected_repos
-  WHERE last_artifact_id IS NOT NULL
-),
-unique_repos AS (
-  SELECT DISTINCT
-    artifact_id,
-    project_id
-  FROM all_repos
 )
+
 SELECT
   artifact_id,
   artifact_namespace,
   artifact_name,
-  'https://github.com/' || artifact_namespace || '/' || artifact_name AS artifact_url,
-  is_current_url,
-  project_id
-FROM unique_repos
-JOIN oso.int_ddp_repo_lineage USING artifact_id
+  artifact_url,
+  project_id,
+  project_name,
+  project_funding_sources,
+  project_total_funding
+FROM project_repos
+JOIN funded_projects USING project_id
