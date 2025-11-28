9fe8654035b64a0f103a1f3082e3bfadee4f7697
Reuven Gonzales <reuven@karibalabs.co>
2025-11-27 22:35:32 -0800

    fix: test reading from vercel fs (#5764)

--- lib/pyodide-node-toolkit/src/load.ts
+++ lib/pyodide-node-toolkit/src/load.ts
@@ -47,6 +47,11 @@ export async function loadPyodideFromDirectory(
   const workDirAbsPath = path.resolve(workDir);
   logger.info(`Extracting pyodide environment into ${workDirAbsPath}`);
 
+  // Verify the contents of fake-import.js
+  const fakeImportPath = path.join(workDirAbsPath, "fake-import.js");
+  const fakeImportContents = await fsPromises.readFile(fakeImportPath, "utf-8");
+  logger.info(`Contents of fake-import.js: \n\n${fakeImportContents}`);
+
   // TEST IMPORTING THE fake-import.js FILE
   const fake = await (import(
     path.join(workDirAbsPath, "fake-import.js")
