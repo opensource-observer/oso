0182d54690b3a26073ea68824d7bf20b1d18b679
Ícaro Guerra <icaro@plasmic.app>
2025-12-01 12:44:46 -0300

    feat(run-request): add graphql mutation for creating run requests

--- apps/frontend/app/api/v1/osograph/schema/resolvers/index.ts
+++ apps/frontend/app/api/v1/osograph/schema/resolvers/index.ts
@@ -29,6 +29,7 @@ export const resolvers: GraphQLResolverMap<GraphQLContext> = {
     ...notebookResolvers.Mutation,
     ...datasetResolvers.Mutation,
     ...dataModelResolvers.Mutation,
+    ...schedulerResolvers.Mutation,
   },
 
   Viewer: viewerResolvers.Viewer,
