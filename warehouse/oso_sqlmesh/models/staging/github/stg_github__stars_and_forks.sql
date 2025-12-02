229717d7c44cda7b690b527964d48f7a7b679352
Ícaro Guerra <icaro@plasmic.app>
2025-12-02 15:11:48 -0300

    fix(github): for fork events, try to get id and full_name from payload

--- warehouse/oso_sqlmesh/models/staging/github/stg_github__stars_and_forks.sql
+++ warehouse/oso_sqlmesh/models/staging/github/stg_github__stars_and_forks.sql
@@ -33,8 +33,8 @@ WITH watch_events AS (
 SELECT
   we.id AS id,
   we.created_at AS created_at,
-  we.repo.id AS repository_id,
-  we.repo.name AS repository_name,
+  COALESCE(we.repo.id, CAST(we.payload ->> '$.forkee.id' AS BIGINT)) AS repository_id,
+  COALESCE(we.repo.name, we.payload ->> '$.forkee.full_name') AS repository_name,
   we.actor.id AS actor_id,
   we.actor.login AS actor_login,
   CASE we.type WHEN 'WatchEvent' THEN 'STARRED' WHEN 'ForkEvent' THEN 'FORKED' END AS "type"
