07ff96f8da1c06250099ca00b1460f59456c7e2f
Javier R <javier.rios-urbano@epitech.eu>
2025-12-10 17:33:30 +0100

    fix: add `import` alias to `jest`

--- apps/frontend/jest.config.ts
+++ apps/frontend/jest.config.ts
@@ -9,6 +9,9 @@ const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "jsdom",
   globalSetup: "./jest.setup.ts",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+  },
 };
 
 export default createJestConfig(config);
