17771219f9e7de41bf952335d143a7699f1c0b3d
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2025-12-10 08:53:58 +0000

    fix: add asynctools and improved CliApp

---
+++ lib/oso-core/oso_core/asynctools/__init__.py
@@ -0,0 +1,10 @@
+import asyncio
+
+
+def safe_run_until_complete(*args, **kwargs):
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+    return loop.run_until_complete(*args, **kwargs)
