d9ecabc8a4ee8f86bd8f4b5069ad999f063bb968
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2025-12-12 14:03:04 +0000

    wip fixes

--- warehouse/scheduler/scheduler/mq/handlers/data_model.py
+++ warehouse/scheduler/scheduler/mq/handlers/data_model.py
@@ -1,10 +1,15 @@
+import uuid
+
 import structlog
 from osoprotobufs.data_model_pb2 import DataModelRunRequest
 from scheduler.graphql_client.client import Client
+from scheduler.graphql_client.enums import RunStatus, StepStatus
 from scheduler.graphql_client.get_data_models import (
     GetDataModelsDatasetsEdgesNodeTypeDefinitionDataModelDefinition,
 )
+from scheduler.graphql_client.input_types import DataModelColumnInput
 from scheduler.types import MessageHandler
+from scheduler.utils import convert_uuid_bytes_to_str
 
 logger = structlog.getLogger(__name__)
 
@@ -51,6 +56,46 @@ class DataModelRunRequestHandler(MessageHandler[DataModelRunRequest]):
 
         print(
             f"Selected {len(selected_models)} models to run for dataset {message.dataset_id}"
+        )
+
+        run_id_str = convert_uuid_bytes_to_str(message.run_id)
+
+        print(f"Starting run with ID: {run_id_str}")
+
+        # Fake
+        await oso_client.start_run(run_id=run_id_str)
+
+        # Start a step
+        step = await oso_client.start_step(
+            run_id=run_id_str,
+            name=f"fake_step_{str(uuid.uuid4())}",
+            display_name="Fake step",
+        )
+        step_id = step.start_step.step.id
+
+        # Add a materialization
+        schema: list[DataModelColumnInput] = [
+            DataModelColumnInput(name="col1", type="STRING"),
+        ]
+        await oso_client.create_materialization(
+            step_id=step_id,
+            table_id="fake_table_id",
+            warehouse_fqn="fake.warehouse.fqn",
+            schema=schema,
+        )
+
+        # Finish the step
+        await oso_client.finish_step(
+            step_id=step_id,
+            status=StepStatus.SUCCESS,
+            logs_url="http://example.com/logs",
+        )
+
+        # Finish the run
+        await oso_client.finish_run(
+            run_id=run_id_str,
+            status=RunStatus.SUCCESS,
+            logs_url="http://example.com/run_logs",
         )
 
         # For each selected model, run the evaluation
