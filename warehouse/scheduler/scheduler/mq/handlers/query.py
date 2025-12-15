86f4dea558af99567bdc26f1ffced267d9c43e4f
Ícaro Guerra <icaro@plasmic.app>
2025-12-15 18:29:36 -0300

    feat(query-run): add query_run_requests topic and handler

---
+++ warehouse/scheduler/scheduler/mq/handlers/query.py
@@ -0,0 +1,69 @@
+import csv
+import typing as t
+
+import aiotrino
+import aiotrino.utils
+import structlog
+from oso_dagster.resources import GCSFileResource, TrinoK8sResource
+from osoprotobufs.query_pb2 import QueryRunRequest
+from queryrewriter import rewrite_query
+from queryrewriter.types import TableResolver
+from scheduler.graphql_client.client import Client
+from scheduler.mq.common import RunHandler
+from scheduler.types import HandlerResponse, RunContext, SuccessResponse
+from scheduler.utils import OSOClientTableResolver
+
+if t.TYPE_CHECKING:
+    from scheduler.config import CommonSettings
+
+logger = structlog.getLogger(__name__)
+
+
+class QueryRunRequestHandler(RunHandler[QueryRunRequest]):
+    topic = "query_run_requests"
+    message_type = QueryRunRequest
+    schema_file_name = "query.proto"
+
+    async def handle_run_message(
+        self,
+        common_settings: "CommonSettings",
+        context: RunContext,
+        message: QueryRunRequest,
+        consumer_trino: TrinoK8sResource,
+        gcs: GCSFileResource,
+        oso_client: Client,
+    ) -> HandlerResponse:
+        # Process the QueryRunRequest message
+        context.log.info(f"Handling QueryRunRequest with ID: {message.run_id}")
+
+        table_resolvers: list[TableResolver] = [
+            OSOClientTableResolver(oso_client=oso_client)
+        ]
+
+        query = rewrite_query(message.query, table_resolvers)
+
+        context.log.info(f"Rewritten query: {query}")
+
+        storage_client = gcs.get_client(asynchronous=False)
+
+        async with consumer_trino.async_get_client(jwt_token=message.jwt) as client:
+            cursor = await client.cursor()
+            cursor = await cursor.execute(query)
+            columns = await cursor.get_description()
+            with storage_client.open(
+                f"gs://{common_settings.query_bucket}/{message.run_id}.csv.gz",
+                "w",
+                encoding="utf-8",
+                compression="gzip",
+                fixed_key_metadata={"content_encoding": "gzip"},
+            ) as f:
+                writer = csv.writer(f)
+                writer.writerow(columns)
+                async for row in aiotrino.utils.aiter(cursor.fetchone, None):
+                    if row is None:
+                        continue
+                    writer.writerow(row)
+
+        return SuccessResponse(
+            message=f"Processed DataModelRunRequest with ID: {message.run_id}"
+        )
