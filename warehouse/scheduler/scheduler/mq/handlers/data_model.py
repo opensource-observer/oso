506df1f172f74209cede4eea21590759352d7493
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2026-01-26 13:56:47 -0800

    fix: address issues with not updating tables properly
    This is a hotfix it's probably not very performant?

--- warehouse/scheduler/scheduler/mq/handlers/data_model.py
+++ warehouse/scheduler/scheduler/mq/handlers/data_model.py
@@ -252,20 +252,25 @@ class DataModelRunRequestHandler(RunHandler[DataModelRunRequest]):
             ignore_if_exists=True,
         )
 
-        resolved_query = await model.resolve_query(table_resolvers=table_resolvers)
+                    resolved_query = await model.resolve_query(
+                        table_resolvers=table_resolvers
+                    )
 
-        create_query = ctas_query(resolved_query)
+                    with adapter.transaction():
+                        # Delete existing table if it exists
+                        adapter.drop_table(table_name=target_table)
 
-        adapter.ctas(
-            table_name=target_table,
-            query_or_df=create_query,
-            exists=True,
-        )
+                        create_query = ctas_query(resolved_query)
 
-        adapter.replace_query(
-            table_name=target_table,
-            query_or_df=resolved_query,
-        )
+                        adapter.ctas(
+                            table_name=target_table,
+                            query_or_df=create_query,
+                            exists=True,
+                        )
+                        adapter.replace_query(
+                            table_name=target_table,
+                            query_or_df=resolved_query,
+                        )
 
         columns = adapter.columns(table_name=target_table)
 
