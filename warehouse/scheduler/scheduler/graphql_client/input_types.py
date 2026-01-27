a376f51eb7a7fa551b3c8667830740b71a40ab31
Ícaro Guerra <icaro@karibalabs.co>
2026-01-27 11:43:48 -0300

    refactor(scheduler): change DATA_CONNECTOR to DATA_CONNECTION

--- warehouse/scheduler/scheduler/graphql_client/input_types.py
+++ warehouse/scheduler/scheduler/graphql_client/input_types.py
@@ -129,6 +129,20 @@ class RevokeInvitationInput(BaseModel):
     invitation_id: str = Field(alias="invitationId")
 
 
+class ModelColumnContextInput(BaseModel):
+    name: str
+    context: str
+
+
+class UpdateModelContextInput(BaseModel):
+    dataset_id: str = Field(alias="datasetId")
+    model_id: str = Field(alias="modelId")
+    context: Optional[str] = None
+    column_context: Optional[List["ModelColumnContextInput"]] = Field(
+        alias="columnContext", default=None
+    )
+
+
 class CreateNotebookInput(BaseModel):
     org_id: str = Field(alias="orgId")
     name: str
@@ -172,7 +186,7 @@ class CreateDataIngestionRunRequestInput(BaseModel):
     dataset_id: str = Field(alias="datasetId")
 
 
-class CreateDataConnectorRunRequestInput(BaseModel):
+class CreateDataConnectionRunRequestInput(BaseModel):
     dataset_id: str = Field(alias="datasetId")
 
 
@@ -243,6 +257,7 @@ class SavePublishedNotebookHtmlInput(BaseModel):
 
 
 CreateDataModelRevisionInput.model_rebuild()
+UpdateModelContextInput.model_rebuild()
 UpdateRunMetadataInput.model_rebuild()
 FinishRunInput.model_rebuild()
 CreateMaterializationInput.model_rebuild()
