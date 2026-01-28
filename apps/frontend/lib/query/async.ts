7f4b12568754c8d31b456ed233de7a7f5d98fd8c
Ícaro Guerra <icaro@plasmic.app>
2026-01-28 12:49:10 -0300

    Revert "fix: revert user changes (#6421)" (#6423)
    This reverts commit 4583b40a53628e1b780d6d0a20fcc05726542b41.

--- apps/frontend/lib/query/async.ts
+++ apps/frontend/lib/query/async.ts
@@ -182,7 +182,7 @@ export async function makeAsyncSqlQuery({
   const message: QueryRunRequest = {
     runId: new Uint8Array(Buffer.from(run.id.replace(/-/g, ""), "hex")),
     query: query,
-    user: `jwt-${user.orgName.trim().toLowerCase()}`,
+    user: `ro-${user.orgName.trim().toLowerCase()}-${user.orgId.trim().replace(/-/g, "").toLowerCase()}`,
     metadataJson: metadataJson,
   };
 
