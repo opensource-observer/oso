334a0ef4ccc89e8cc295827520266ef4596b844c
Reuven V. Gonzales <reuven.v.gonzales@gmail.com>
2026-02-02 12:20:39 -0800

    fix: cast to double

--- warehouse/oso_sqlmesh/models/intermediate/domain-specific/superchain/oracles/int_optimism_oracles_indirect_fees_daily.sql
+++ warehouse/oso_sqlmesh/models/intermediate/domain-specific/superchain/oracles/int_optimism_oracles_indirect_fees_daily.sql
@@ -41,7 +41,7 @@ WITH events AS (
 oracles_per_txn AS (
   SELECT
     transaction_hash,
-    COUNT(DISTINCT oracle_name) AS num_oracles,
+    COUNT(DISTINCT oracle_name)::DOUBLE AS num_oracles,
     COUNT(*) AS num_traces_per_txn
   FROM events
   GROUP BY 1
@@ -52,7 +52,7 @@ amortized_events AS (
     oracle_name,
     to_address,
     SUM(l2_tx_fees / num_traces_per_txn / num_oracles) AS l2_tx_fees,
-    SUM(1.0 / num_oracles) AS tx_count
+    SUM(1.0 / num_oracles)::DOUBLE AS tx_count
   FROM events
   JOIN oracles_per_txn
     ON events.transaction_hash = oracles_per_txn.transaction_hash
@@ -75,7 +75,7 @@ SELECT
   ae.to_address,
   apps.to_project_name,
   ae.l2_tx_fees,
-  ae.tx_count
+  ae.tx_count::DOUBLE AS tx_count
 FROM amortized_events AS ae
 LEFT JOIN apps
   ON ae.to_address = apps.artifact_name
