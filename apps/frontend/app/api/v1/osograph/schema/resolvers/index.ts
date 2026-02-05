8cf9b6d36872ed68f9c60805b8d31e5f80737a92
Javier R <javier.rios-urbano@epitech.eu>
2026-02-05 02:50:15 +0100

    chore: use `new` resolvers

--- apps/frontend/app/api/v1/osograph/schema/resolvers/index.ts
+++ apps/frontend/app/api/v1/osograph/schema/resolvers/index.ts
@@ -1,71 +1,52 @@
 import { DateTimeISOResolver, GraphQLJSON } from "graphql-scalars";
 import type { GraphQLResolverMap } from "@apollo/subgraph/dist/schema-helper/resolverMap";
 import type { GraphQLContext } from "@/app/api/v1/osograph/types/context";
-import { viewerResolvers } from "@/app/api/v1/osograph/schema/resolvers/viewer";
-import { userResolvers } from "@/app/api/v1/osograph/schema/resolvers/user";
-import { organizationResolvers } from "@/app/api/v1/osograph/schema/resolvers/organization";
-import { invitationResolvers } from "@/app/api/v1/osograph/schema/resolvers/invitation";
-import { notebookResolvers } from "@/app/api/v1/osograph/schema/resolvers/notebook";
-import { datasetResolvers } from "@/app/api/v1/osograph/schema/resolvers/dataset";
-import { dataModelResolvers } from "@/app/api/v1/osograph/schema/resolvers/data-model";
-import { schedulerResolvers } from "@/app/api/v1/osograph/schema/resolvers/scheduler";
-import { systemResolvers } from "@/app/api/v1/osograph/schema/resolvers/system";
-import { staticModelResolvers } from "@/app/api/v1/osograph/schema/resolvers/static-model";
-import { dataIngestionResolvers } from "@/app/api/v1/osograph/schema/resolvers/data-ingestion";
-import { modelContextResolvers } from "@/app/api/v1/osograph/schema/resolvers/model-context";
-import { dataConnectionResolvers } from "@/app/api/v1/osograph/schema/resolvers/data-connection";
+
+import { systemResolvers } from "@/app/api/v1/osograph/schema/resolvers/system/index";
+import { organizationResolvers } from "@/app/api/v1/osograph/schema/resolvers/organization/index";
+import { resourceResolvers } from "@/app/api/v1/osograph/schema/resolvers/resource/index";
+import { userResolvers } from "@/app/api/v1/osograph/schema/resolvers/user/index";
 
 export const resolvers: GraphQLResolverMap<GraphQLContext> = {
   DateTime: DateTimeISOResolver,
   JSON: GraphQLJSON,
 
   Query: {
-    ...viewerResolvers.Query,
+    ...userResolvers.Query,
     ...organizationResolvers.Query,
-    ...invitationResolvers.Query,
-    ...notebookResolvers.Query,
-    ...datasetResolvers.Query,
-    ...dataModelResolvers.Query,
-    ...staticModelResolvers.Query,
-    ...schedulerResolvers.Query,
+    ...resourceResolvers.Query,
     ...systemResolvers.Query,
-    ...dataConnectionResolvers.Query,
   },
 
   Mutation: {
+    ...userResolvers.Mutation,
     ...organizationResolvers.Mutation,
-    ...invitationResolvers.Mutation,
-    ...notebookResolvers.Mutation,
-    ...datasetResolvers.Mutation,
-    ...dataModelResolvers.Mutation,
-    ...staticModelResolvers.Mutation,
-    ...schedulerResolvers.Mutation,
+    ...resourceResolvers.Mutation,
     ...systemResolvers.Mutation,
-    ...dataIngestionResolvers.Mutation,
-    ...modelContextResolvers.Mutation,
-    ...dataConnectionResolvers.Mutation,
   },
 
-  Viewer: viewerResolvers.Viewer,
+  Viewer: userResolvers.Viewer,
   User: userResolvers.User,
   Organization: organizationResolvers.Organization,
   OrganizationMember: organizationResolvers.OrganizationMember,
-  Invitation: invitationResolvers.Invitation,
-  Notebook: notebookResolvers.Notebook,
-  Dataset: datasetResolvers.Dataset,
-  DataModelDefinition: datasetResolvers.DataModelDefinition,
-  StaticModelDefinition: datasetResolvers.StaticModelDefinition,
-  DataIngestionDefinition: datasetResolvers.DataIngestionDefinition,
-  DataIngestion: dataIngestionResolvers.DataIngestion,
-  DataModel: dataModelResolvers.DataModel,
-  DataModelRevision: dataModelResolvers.DataModelRevision,
-  DataModelRelease: dataModelResolvers.DataModelRelease,
-  StaticModel: staticModelResolvers.StaticModel,
-  DataConnection: dataConnectionResolvers.DataConnection,
-  DataConnectionAlias: dataConnectionResolvers.DataConnectionAlias,
-  Run: schedulerResolvers.Run,
-  ModelContext: modelContextResolvers.ModelContext,
-  Step: schedulerResolvers.Step,
-  Materialization: schedulerResolvers.Materialization,
+  Invitation: userResolvers.Invitation,
+
+  Notebook: resourceResolvers.Notebook,
+  Dataset: resourceResolvers.Dataset,
+  DataModelDefinition: resourceResolvers.DataModelDefinition,
+  StaticModelDefinition: resourceResolvers.StaticModelDefinition,
+  DataIngestionDefinition: resourceResolvers.DataIngestionDefinition,
+  DataIngestion: resourceResolvers.DataIngestion,
+  DataModel: resourceResolvers.DataModel,
+  DataModelRevision: resourceResolvers.DataModelRevision,
+  DataModelRelease: resourceResolvers.DataModelRelease,
+  StaticModel: resourceResolvers.StaticModel,
+  DataConnection: resourceResolvers.DataConnection,
+  DataConnectionAlias: resourceResolvers.DataConnectionAlias,
+  ModelContext: resourceResolvers.ModelContext,
+
+  Run: systemResolvers.Run,
+  Step: systemResolvers.Step,
+  Materialization: systemResolvers.Materialization,
   System: systemResolvers.System,
 };
